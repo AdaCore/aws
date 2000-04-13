ec451384a5c2f4e8d715a16e73778fd18e60bb18
Pascal Obry <obry@adacore.com>
2000-04-13 20:27:12 +0000

    Complete rewrite. more efficent implementation.

--- src/aws-server.adb
+++ src/aws-server.adb
@@ -2,7 +2,7 @@
 --                              Ada Web Server                              --
 --                                                                          --
 --                            Copyright (C) 2000                            --
---                               Pascal Obry                                --
+--                      Dmitriy Anisimkov & Pascal Obry                     --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --
@@ -28,23 +28,29 @@
 
 --  $Id$
 
-with Sockets;
+with Ada.Calendar;
+with Ada.Exceptions;
+with Ada.Text_IO;
 
-with AWS.Connection;
+with Sockets;
 
 package body AWS.Server is
 
-   Accepting_Socket : Sockets.Socket_FD;
-   Incoming_Socket  : Sockets.Socket_FD;
+   use Ada;
 
-   ----------------
-   -- Web_Server --
-   ----------------
+   procedure Protocol_Handler (Sock    : in Sockets.Socket_FD;
+                               Handler : in Response.Callback;
+                               Slots   : in Slots_Access;
+                               Index   : in Positive);
+   --  handle the line, this is where the HTTP protocol is defined.
 
-   task body HTTP is
+   -----------
+   -- Start --
+   -----------
+
+   procedure Start (Web_Server : in out HTTP) is
+      Accepting_Socket : Sockets.Socket_FD;
    begin
-      Connection.Create_Slots (Max_Connection);
-
       Sockets.Socket (Accepting_Socket,
                       Sockets.AF_INET,
                       Sockets.SOCK_STREAM);
@@ -54,14 +60,215 @@ package body AWS.Server is
                           Sockets.SO_REUSEADDR,
                           1);
 
-      Sockets.Bind (Accepting_Socket, Port);
+      Sockets.Bind (Accepting_Socket, Web_Server.Port);
 
       Sockets.Listen (Accepting_Socket);
 
-      loop
-         Sockets.Accept_Socket (Accepting_Socket, Incoming_Socket);
-         Connection.Get_Free_Slot.Start (Incoming_Socket, CB);
+      Web_Server.Sock := Accepting_Socket;
+
+      --  initialize each connection lines.
+
+      for I in 1 .. Web_Server.Max_Connection loop
+         Web_Server.Lines (I).Start (Web_Server, I);
       end loop;
-   end HTTP;
+
+   end Start;
+
+   -----------
+   -- Slots --
+   -----------
+
+   protected body Slots is
+
+      -------------------
+      -- Set_Abortable --
+      -------------------
+
+      procedure Set_Abortable (Index : in Positive; Flag : in Boolean) is
+      begin
+         Set (Index).Abortable := Flag;
+      end Set_Abortable;
+
+      ------------------------
+      -- Mark_Activity_Time --
+      ------------------------
+
+      procedure Mark_Activity_Time (Index : in Positive) is
+      begin
+         Set (Index).Activity_Time_Stamp := Ada.Calendar.Clock;
+      end Mark_Activity_Time;
+
+      ------------------
+      -- Abort_Oldest --
+      ------------------
+
+      procedure Abort_Oldest (Force : in Boolean) is
+         use type Calendar.Time;
+         To_Be_Closed        : Natural := 0;
+         Time_Stamp          : Calendar.Time := Calendar.Clock;
+         Activity_Time_Stamp : Calendar.Time;
+      begin
+
+         --  look for the oldest abortable slot
+
+         for S in Set'Range loop
+            Activity_Time_Stamp := Set (S).Activity_Time_Stamp;
+
+            if Set (S).Abortable and then Activity_Time_Stamp < Time_Stamp then
+               To_Be_Closed := S;
+               Time_Stamp   := Activity_Time_Stamp;
+            end if;
+
+         end loop;
+
+         --  if one Slot has and abortable state, and we are in Force mode or
+         --  slot is open since more than Keep_Open_Duration we close it.
+
+         if To_Be_Closed /= 0
+           and then
+           (Force or else (Calendar.Clock - Time_Stamp) > Keep_Open_Duration)
+         then
+            Sockets.Shutdown (Set (To_Be_Closed).Sock);
+            Set (To_Be_Closed).Opened := False;
+         end if;
+      end Abort_Oldest;
+
+      ---------
+      -- Get --
+      ---------
+
+      procedure Get (FD : in Sockets.Socket_FD; Index : in Positive) is
+      begin
+         Set (Index).Sock   := FD;
+         Set (Index).Opened := True;
+         Count := Count - 1;
+         if Count = 0 then
+            Abort_Oldest (True);
+         end if;
+      end Get;
+
+      -------------
+      -- Release --
+      -------------
+
+      procedure Release (Index : in Positive) is
+      begin
+         Count := Count + 1;
+         if Set (Index).Opened then
+            Sockets.Shutdown (Set (Index).Sock);
+            Set (Index).Opened := False;
+         end if;
+      end Release;
+
+      ----------
+      -- Free --
+      ----------
+
+      function Free return Boolean is
+      begin
+         return Count > 0;
+      end Free;
+
+   end Slots;
+
+
+   ------------------
+   -- Line_Cleaner --
+   ------------------
+
+   task body Line_Cleaner is
+      Is_Force : Boolean;
+   begin
+      loop
+         select
+            accept Force do
+               Is_Force := True;
+            end Force;
+         or
+            delay 30.0;
+            Is_Force := False;
+         end select;
+
+         Server.Slots.Abort_Oldest (Is_Force);
+      end loop;
+   end Line_Cleaner;
+
+   ----------------------
+   -- Protocol_Handler --
+   ----------------------
+
+   procedure Protocol_Handler (Sock    : in Sockets.Socket_FD;
+                               Handler : in Response.Callback;
+                               Slots   : in Slots_Access;
+                               Index   : in Positive) is separate;
+
+   ----------
+   -- Line --
+   ----------
+
+   task body Line is
+
+      Server_Sock : Sockets.Socket_FD;
+      Sock        : Sockets.Socket_FD;
+      CB          : Response.Callback;
+      Slots       : Slots_Access;
+      Slot_Index  : Positive;
+
+   begin
+
+      select
+         accept Start (Server : HTTP;
+                       Index  : Positive)
+         do
+            Slots       := Server.Slots;
+            Server_Sock := Server.Sock;
+            Slot_Index  := Index;
+            CB          := Server.CB;
+         end Start;
+      or
+         terminate;
+      end select;
+
+      loop
+
+         --  wait for an incoming connection.
+
+         Sockets.Accept_Socket (Server_Sock, Sock);
+
+         begin
+            Slots.Get (Sock, Slot_Index);
+            Slots.Set_Abortable (Slot_Index, True);
+
+            Protocol_Handler (Sock, CB, Slots, Slot_Index);
+
+         exception
+
+            --  we must never exit from the outer loop as a Line task is
+            --  supposed to live forever. We have here a pool of Line and each
+            --  line is recycled when needed.
+
+            when Sockets.Connection_Closed =>
+               Text_IO.Put_Line ("Connection time-out, close it.");
+
+            when E : others =>
+               Text_IO.Put_Line ("A problem has been detected!");
+               Text_IO.Put_Line ("Connection will be closed...");
+               Text_IO.New_Line;
+               Text_IO.Put_Line (Ada.Exceptions.Exception_Information (E));
+
+         end;
+
+         Slots.Set_Abortable (Slot_Index, False);
+         Slots.Release (Slot_Index);
+
+      end loop;
+
+   exception
+
+      when E : others =>
+         Text_IO.Put_Line ("Slot problem has been detected!");
+         Text_IO.Put_Line (Ada.Exceptions.Exception_Information (E));
+
+   end Line;
 
 end AWS.Server;
--- src/aws-server.ads
+++ src/aws-server.ads
@@ -2,7 +2,7 @@
 --                              Ada Web Server                              --
 --                                                                          --
 --                            Copyright (C) 2000                            --
---                               Pascal Obry                                --
+--                      Dmitriy Anisimkov & Pascal Obry                     --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --
@@ -28,15 +28,98 @@
 
 --  $Id$
 
+with Ada.Calendar;
+
+with Sockets;
 with AWS.Response;
 
 package AWS.Server is
 
-   task type HTTP
-     (Max_Connection : Positive          := 10;
-      Port           : Positive          := 8080;
-      CB             : Response.Callback := Response.Default_Handler);
+   Default_Connection : constant := 10;
+   Default_Port       : constant := 8080;
 
-   --  Declare a new Web_Server listening at the specified port.
+   type HTTP
+     (Max_Connection : Positive          := Default_Connection;
+      Port           : Positive          := Default_Port;
+      CB             : Response.Callback := Response.Default_Handler) is
+   limited private;
+
+   procedure Start (Web_Server : in out HTTP);
+   --  Start the Web server. It initialize the connections lines.
+
+private
+
+   Keep_Open_Duration : constant Duration := 120.0;
+
+   type HTTP_Access is access all HTTP;
+
+   type Slot is record
+      Sock                : Sockets.Socket_FD;
+      Opened              : Boolean := False;
+      Abortable           : Boolean := False;
+      Quit                : Boolean := False;
+      Activity_Time_Stamp : Ada.Calendar.Time;
+   end record;
+
+   --  Abortable is set to true when the line can be aborted by closing the
+   --  associated socket. Activity_Time_Stamp is the last time the line has
+   --  been used. The line with the oldest activity could be closed.
+   --  Also a line is closed after Keep_Open_Duration seconds of inactivity.
+
+   type Slot_Set is array (Positive range <>) of Slot;
+
+   protected type Slots (N : Positive) is
+
+      procedure Set_Abortable (Index : in Positive; Flag : in Boolean);
+      --  set Abortable field to Flag for the Line number Index. This flag is
+      --  used by the Line_Cleaner to know if a line can be aborted safely.
+
+      procedure Mark_Activity_Time (Index : in Positive);
+      --  set Activity_Time_Stamp which is the last time where the line number
+      --  Index as been used.
+
+      procedure Abort_Oldest  (Force : in Boolean);
+      --  abort oldest line (the line with the oldest activity time stamp) if
+      --  force is True. Otherwise the Line must be in an abortable state.
+
+      procedure Get (FD : in Sockets.Socket_FD; Index : in Positive);
+      --  get the slot number Index. Opened status is set to True.
+
+      procedure Release  (Index : in Positive);
+      --  release slot number Index. Opened status us set to False.
+
+      function Free return Boolean;
+      --  returns True if there is some free slots available.
+
+   private
+      Set   : Slot_Set (1 .. N);
+      Count : Natural := N;
+   end Slots;
+
+   type Slots_Access is access all Slots;
+
+   task type Line is
+      entry Start (Server : in HTTP; Index : in Positive);
+   end Line;
+
+   type Line_Set is array (Positive range <>) of Line;
+
+   task type Line_Cleaner (Server : HTTP_Access) is
+     entry Force;
+   end Line_Cleaner;
+   --  run through the slots and see if some of them could be closed.
+
+   type HTTP
+     (Max_Connection : Positive          := Default_Connection;
+      Port           : Positive          := Default_Port;
+      CB             : Response.Callback := Response.Default_Handler) is
+   limited record
+      Sock    : Sockets.Socket_FD;
+      --  this is the server socket for incoming connection.
+
+      Lines   : Line_Set (1 .. Max_Connection);
+      Slots   : Slots_Access := new Server.Slots (Max_Connection);
+      Cleaner : Line_Cleaner (HTTP'Unchecked_Access);
+   end record;
 
 end AWS.Server;
