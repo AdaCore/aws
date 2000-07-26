74a15bac0600f32d0cb8ed69cd8e68ad9a40fbdc
Pascal Obry <obry@adacore.com>
2000-07-26 20:32:40 +0000

    New AWS version (0.6).

--- src/aws.ads
+++ src/aws.ads
@@ -35,7 +35,7 @@ package AWS is
    Internal_Error   : exception;
    Connection_Error : exception;
 
-   Version        : constant String := "0.5";
+   Version        : constant String := "0.6";
 
    HTTP_Version   : constant String := "HTTP/1.1";
 
