e558802e9a2b39d7a2936b8c25999cb09896a8d3
Pascal Obry <obry@adacore.com>
2000-09-07 18:58:09 +0000

    Minor reformating. Add/Fix header format.

--- include/aws-os_lib-win32.adb
+++ include/aws-os_lib-win32.adb
@@ -1,11 +1,35 @@
---
---  $id$
---
---  Use direct calls to win32 system routines instead of the Posix library
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2000                            --
+--                Dmitriy Anisimkov, Sune Falck, Pascal Obry                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+--  Use direct calls to Win32 system routines instead of the POSIX library
 --  and get the current UTC/GMT time from the system.
---
---  2000-04-19 Sune Falck
---
 
 with Interfaces.C;
 
@@ -227,6 +251,5 @@ package body AWS.OS_Lib is
       GetSystemTime (UTC);
       return Systime_To_Time (UTC);
    end OS_Clock;
-
 
 end AWS.OS_Lib;
