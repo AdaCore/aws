2bc902210fd6681b5d8defe9cc0aa22b7969c5ce
Pascal Obry <obry@adacore.com>
2000-09-07 18:58:51 +0000

    Fix copyright notice.

--- src/aws-os_lib.ads
+++ src/aws-os_lib.ads
@@ -2,7 +2,7 @@
 --                              Ada Web Server                              --
 --                                                                          --
 --                            Copyright (C) 2000                            --
---                          Sune Falck, Pascal Obry                         --
+--                Dmitriy Anisimkov, Sune Falck, Pascal Obry                --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --
