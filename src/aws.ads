fe0086e8688ae35ddabecd1285eda550de90be7c
Pascal Obry <obry@adacore.com>
2000-10-16 19:10:21 +0000

    (Version): set to 0.9.2.

--- src/aws.ads
+++ src/aws.ads
@@ -35,7 +35,7 @@ package AWS is
    Internal_Error   : exception;
    Connection_Error : exception;
 
-   Version        : constant String := "0.9";
+   Version        : constant String := "0.9.1";
 
    HTTP_Version   : constant String := "HTTP/1.1";
 
