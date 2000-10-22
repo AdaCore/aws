4dca6d3bbaa87e89be52c442268d6467b854ad94
Pascal Obry <obry@adacore.com>
2000-10-22 12:54:10 +0000

    Initial revision.

---
+++ demos/hotplug.adb
@@ -0,0 +1,97 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2000                            --
+--                      Dmitriy Anisimov - Pascal Obry                      --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+--  See main.adb on how to run this demo.
+
+with Ada.Command_Line;
+with Ada.Text_IO;
+
+with AWS.Communication.Client;
+with AWS.Response;
+with AWS.Server;
+with AWS.Utils;
+
+with Hotplug_CB;
+
+procedure Hotplug is
+
+   use Ada;
+
+   procedure Wait_Terminate;
+   --  Wait for module to terminate and unregister it.
+
+   Response : AWS.Response.Data;
+
+   --------------------
+   -- Wait_Terminate --
+   --------------------
+
+   procedure Wait_Terminate is
+      C : Character;
+   begin
+      loop
+         Text_IO.Get_Immediate (C);
+         exit when C = 'T';
+      end loop;
+
+      Response := AWS.Communication.Client.Send_Message
+        (AWS.Utils.Gethostname, 2222,
+         "UNREGISTER",
+         AWS.Communication.Parameters (".*AZERTY.*"));
+   end Wait_Terminate;
+
+   WS  : AWS.Server.HTTP
+     (3, 1235, False, Hotplug_CB.Hotplug'Access, False);
+
+begin
+   if Command_Line.Argument_Count /= 1 then
+      Text_IO.Put_Line ("Syntax: hotplug <main_server>");
+      Text_IO.New_Line;
+      return;
+   end if;
+
+   Text_IO.Put_Line ("AWS " & AWS.Version);
+   Text_IO.Put_Line ("Enter T to terminate...");
+   Text_IO.Put_Line ("Hotplug module linked to server " &
+                     Command_Line.Argument (1));
+
+   AWS.Server.Start (WS, "Hotplug", "/Admin-Page");
+
+   Response := AWS.Communication.Client.Send_Message
+     (Command_Line.Argument (1), 2222,
+      "REGISTER",
+      AWS.Communication.Parameters
+      (".*AZERTY.*",
+       "http://" & AWS.Utils.Gethostname & ":1235/"));
+
+   Wait_Terminate;
+
+   AWS.Server.Shutdown (WS);
+end Hotplug;
---
+++ demos/main.adb
@@ -0,0 +1,71 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2000                            --
+--                      Dmitriy Anisimov - Pascal Obry                      --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+--  How to run this demo ?
+--
+--  On the server side:
+--  $ main
+--
+--  On the same computer, or another one:
+--  $ hotplug
+--
+--  On the client side:
+--  * launch your Web Browser (this should work with any browser)
+--  * enter the URL : http://<servername>:1234/
+--
+--  You can ask for whatever URI
+--  http://<servername>:1234/abc
+--
+--  If you can ask for whatever URI containing string "AWS" the hotplug server
+--  will be triggered:
+--  http://<servername>:1234/aws_hotplug_is_great
+--
+
+with Ada.Text_IO;
+
+with AWS.Server.Hotplug;
+with AWS.Log;
+
+with Hotplug_CB;
+
+procedure Main is
+
+   use Ada;
+
+   WS  : aliased AWS.Server.HTTP
+     (3, 1234, False, Hotplug_CB.Main'Access, False);
+
+begin
+   Text_IO.Put_Line ("AWS " & AWS.Version);
+   Text_IO.Put_Line ("Kill me when you want me to stop...");
+   AWS.Server.Start (WS, "Runme", "/Admin-Page");
+   AWS.Server.Hotplug.Activate (WS'Unchecked_Access, 2222);
+   AWS.Log.Start;
+end Main;
