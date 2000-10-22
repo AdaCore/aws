f1cc599474a4d58635102f0a4820c18857de075e
Pascal Obry <obry@adacore.com>
2000-10-22 12:50:30 +0000

    Removes generic formal parameter Port and add a type T (and its  access type). This is to be able to pass a context to the  Callback generic formal function. (Start): new procedure.

--- src/aws-communication-server.ads
+++ src/aws-communication-server.ads
@@ -32,12 +32,13 @@ with AWS.Response;
 
 generic
 
-   Port : Positive;
-   --  server port
+   type T (<>) is limited private;
+   type T_Access is access T;
 
    with function Callback
      (Server     : in String;
       Name       : in String;
+      Context    : in T_Access;
       Parameters : in Parameter_Set := Null_Parameter_Set)
      return Response.Data;
 
@@ -45,8 +46,11 @@ package AWS.Communication.Server is
 
    --  Each instantiation of this package will create an HTTP server waiting
    --  for incoming request at the Port specified by the generic formal
-   --  parameter. This communication server can be stopped with the procedure
-   --  below.
+   --  parameter. This communication server must be started with the Start
+   --  procedure and can be stopped with the procedure Shutdown below.
+
+   procedure Start (Port : in Positive; Context : in T_Access);
+   --  Start communication HTTP server listening at the given port.
 
    procedure Shutdown;
    --  Shutdown the communication HTTP server.
