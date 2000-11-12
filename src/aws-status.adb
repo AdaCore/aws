13bee616faf832513acb08d4bb4a7f4c32e63645
Pascal Obry <obry@adacore.com>
2000-11-12 16:09:18 +0000

    Move all routines to set the status to aws-status-set.adb. Set routine in alpha order.

--- src/aws-status.adb
+++ src/aws-status.adb
@@ -28,15 +28,30 @@
 
 --  $Id$
 
-with Ada.Strings.Fixed;
+with Ada.Strings;
 with Ada.Characters.Handling;
-
-with AWS.Messages;
-with AWS.Translater;
 
 package body AWS.Status is
 
    use Ada.Strings;
+
+   ------------------------
+   -- Authorization_Name --
+   ------------------------
+
+   function Authorization_Name (D : in Data) return String is
+   begin
+      return To_String (D.Auth_Name);
+   end Authorization_Name;
+
+   ----------------------------
+   -- Authorization_Password --
+   ----------------------------
+
+   function Authorization_Password (D : in Data) return String is
+   begin
+      return To_String (D.Auth_Password);
+   end Authorization_Password;
 
    -----------------
    -- Binary_Data --
@@ -137,20 +152,6 @@ package body AWS.Status is
       return To_String (D.Boundary);
    end Multipart_Boundary;
 
-   -------------
-   -- Session --
-   -------------
-
-   function Session (D : in Data) return String is
-   begin
-      return To_String (D.Session_ID);
-   end Session;
-
-   function Session (D : in Data) return AWS.Session.ID is
-   begin
-      return AWS.Session.Value (Session (D));
-   end Session;
-
    ---------------
    -- Parameter --
    ---------------
@@ -208,154 +209,19 @@ package body AWS.Status is
       return Parameters.Get_Key (D.Parameters, N);
    end Parameter_Name;
 
-   -----------
-   -- Reset --
-   -----------
+   -------------
+   -- Session --
+   -------------
 
-   procedure Reset (D : in out Data) is
+   function Session (D : in Data) return String is
    begin
-      Parameters.Release (D.Parameters);
-      D := No_Data;
-   end Reset;
+      return To_String (D.Session_ID);
+   end Session;
 
-   --------------------
-   -- Set_Connection --
-   --------------------
-
-   procedure Set_Connection (D : in out Data; Connection : in String) is
+   function Session (D : in Data) return AWS.Session.ID is
    begin
-      D.Connection := To_Unbounded_String (Connection);
-   end Set_Connection;
-
-   ------------------------
-   -- Set_Content_Length --
-   ------------------------
-
-   procedure Set_Content_Length (D              : in out Data;
-                                 Content_Length : in     Natural) is
-   begin
-      D.Content_Length := Content_Length;
-   end Set_Content_Length;
-
-   ----------------------
-   -- Set_Content_Type --
-   ----------------------
-
-   procedure Set_Content_Type (D            : in out Data;
-                               Content_Type : in     String) is
-   begin
-      D.Content_Type := To_Unbounded_String (Content_Type);
-   end Set_Content_Type;
-
-   -------------------------
-   -- Set_File_Up_To_Date --
-   -------------------------
-
-   procedure Set_File_Up_To_Date (D               : in out Data;
-                                  File_Up_To_Date : in     Boolean) is
-   begin
-      D.File_Up_To_Date := File_Up_To_Date;
-   end Set_File_Up_To_Date;
-
-   --------------
-   -- Set_Host --
-   --------------
-
-   procedure Set_Host (D : in out Data; Host : in String) is
-   begin
-      D.Host := To_Unbounded_String (Host);
-   end Set_Host;
-
-   ---------------------------
-   -- Set_If_Modified_Since --
-   ---------------------------
-
-   procedure Set_If_Modified_Since (D                 : in out Data;
-                                    If_Modified_Since : in     String) is
-   begin
-      D.If_Modified_Since := To_Unbounded_String (If_Modified_Since);
-   end Set_If_Modified_Since;
-
-   ----------------------------
-   -- Set_Multipart_Boundary --
-   ----------------------------
-
-   procedure Set_Multipart_Boundary (D        : in out Data;
-                                     Boundary : in     String) is
-   begin
-      D.Boundary := To_Unbounded_String (Boundary);
-   end Set_Multipart_Boundary;
-
-   --------------------
-   -- Set_Parameters --
-   --------------------
-
-   procedure Set_Parameters (D : in out Data; Name, Value : in String) is
-   begin
-      AWS.Parameters.Add (D.Parameters, Name, Translater.Decode_URL (Value));
-   end Set_Parameters;
-
-   procedure Set_Parameters (D : in out Data; Parameters : in String) is
-      P : String renames Parameters;
-      C : Positive := P'First;
-      I : Natural;
-      S : Positive := P'First;
-      E : Natural;
-   begin
-      loop
-         I := Fixed.Index (P (C .. P'Last), "=");
-
-         exit when I = 0;
-
-         S := I + 1;
-
-         E := Fixed.Index (P (S .. P'Last), "&");
-
-         if E = 0 then
-            --  last parameter
-
-            Set_Parameters (D, P (C .. I - 1), P (S .. P'Last));
-            exit;
-
-         else
-            Set_Parameters (D, P (C .. I - 1), P (S .. E - 1));
-            C := E + 1;
-         end if;
-      end loop;
-   end Set_Parameters;
-
-   procedure Set_Parameters (D         : in out Data;
-                             Parameter : in     Stream_Element_Array) is
-   begin
-      D.Binary_Data := new Stream_Element_Array'(Parameter);
-   end Set_Parameters;
-
-   -----------------
-   -- Set_Request --
-   -----------------
-
-   procedure Set_Request (D            : in out Data;
-                          Method       : in     Request_Method;
-                          URI          : in     String;
-                          HTTP_Version : in     String;
-                          Parameters   : in     String := "") is
-   begin
-      D.Method       := Method;
-      D.URI          := To_Unbounded_String (URI);
-      D.HTTP_Version := To_Unbounded_String (HTTP_Version);
-
-      Set_Parameters (D, Parameters);
-   end Set_Request;
-
-   -----------------
-   -- Set_Session --
-   -----------------
-
-   procedure Set_Session  (D  : in out Data;
-                           ID : in     String) is
-   begin
-      D.Session_ID := To_Unbounded_String (ID);
-   end Set_Session;
+      return AWS.Session.Value (Session (D));
+   end Session;
 
    ---------
    -- URI --
@@ -365,65 +231,5 @@ package body AWS.Status is
    begin
       return To_String (D.URI);
    end URI;
-
-   -----------------------
-   -- Set_Authorization --
-   -----------------------
-
-   procedure Set_Authorization (D             : in out Data;
-                                Authorization : in     String) is
-      Basic_Token : constant String := "Basic ";
-   begin
-      if Messages.Is_Match (Authorization, Basic_Token) then
-
-         declare
-            use Ada.Streams;
-
-            Auth_Bin : Stream_Element_Array :=
-              Translater.Base64_Decode
-              (Authorization (Basic_Token'Length + Authorization'First
-                              .. Authorization'Last));
-            Auth_Str : String (1 .. Auth_Bin'Length);
-            K        : Positive := Auth_Str'First;
-            Delimit  : Natural;
-         begin
-
-            for i in Auth_Bin'Range loop
-               Auth_Str (K) :=
-                 Character'Val (Stream_Element'Pos (Auth_Bin (i)));
-               K := K + 1;
-            end loop;
-
-            Delimit := Fixed.Index (Auth_Str, ":");
-
-            if Delimit = 0 then
-               D.Auth_Name := To_Unbounded_String (Auth_Str);
-            else
-               D.Auth_Name     :=
-                 To_Unbounded_String (Auth_Str (1 .. Delimit - 1));
-               D.Auth_Password :=
-                 To_Unbounded_String (Auth_Str (Delimit + 1 .. Auth_Str'Last));
-            end if;
-         end;
-      end if;
-   end Set_Authorization;
-
-   ------------------------
-   -- Authorization_Name --
-   ------------------------
-
-   function Authorization_Name (D : in Data) return String is
-   begin
-      return To_String (D.Auth_Name);
-   end Authorization_Name;
-
-   ----------------------------
-   -- Authorization_Password --
-   ----------------------------
-
-   function Authorization_Password (D : in Data) return String is
-   begin
-      return To_String (D.Auth_Password);
-   end Authorization_Password;
 
 end AWS.Status;
