101cd020bd1197c0770951bd859b1704ae5c9512
Pascal Obry <obry@adacore.com>
2001-04-28 20:06:36 +0000

    Initial revision.

---
+++ demos/runme_info.adb
@@ -0,0 +1,91 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2001                            --
+--                               Pascal Obry                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+with Ada.Text_IO;
+with Ada.Strings.Fixed;
+with Ada.Command_Line;
+with GNAT.OS_Lib;
+
+package body Runme_Info is
+
+   use Ada;
+   use GNAT;
+   use type GNAT.OS_Lib.String_Access;
+
+   Executable : constant OS_Lib.String_Access
+     := OS_Lib.Locate_Exec_On_Path (Client_Name);
+
+   function Get_File_Path (Pathname : in String) return String;
+   --  Given a filename (absolute or relative) it will return the
+   --  absolute pathname to this file. A trailing directory separator is
+   --  returned.
+
+   -------------------
+   -- Get_File_Path --
+   -------------------
+
+   function Get_File_Path (Pathname : in String) return String is
+      File : Text_IO.File_Type;
+   begin
+      Text_IO.Open (File, Text_IO.In_File, Pathname);
+
+      declare
+         Name : constant String := Text_IO.Name (File);
+      begin
+         Text_IO.Close (File);
+         return Name (Name'First ..
+                      Strings.Fixed.Index (Name, "\", Strings.Backward));
+      end;
+   end Get_File_Path;
+
+   -----------------------------------
+   -- Get_Client_Name_Full_Pathname --
+   -----------------------------------
+
+   function Get_Client_Name_Full_Pathname return String is
+   begin
+      if Executable = null then
+         return Client_Name;
+      else
+         return Get_File_Path (Executable.all) & Client_Name;
+      end if;
+   end Get_Client_Name_Full_Pathname;
+
+   -------------------------
+   -- Get_Executable_Path --
+   -------------------------
+
+   function Get_Executable_Path return String is
+      CN : constant String := Command_Line.Command_Name;
+   begin
+      return CN (CN'First .. Strings.Fixed.Index (CN, "\", Strings.Backward));
+   end Get_Executable_Path;
+
+end Runme_Info;
---
+++ demos/runme_service.adb
@@ -0,0 +1,133 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2001                            --
+--                               Pascal Obry                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+with Ada.Text_IO;
+with GNAT.Directory_Operations;
+
+with Service.Status;
+
+with SSL;
+
+with AWS.Server;
+pragma Elaborate_All (AWS.Server);
+with AWS.Log;
+with Runme_CB;
+
+package body Runme_Service is
+
+   use Ada;
+   use GNAT;
+
+   Stop_Request   : Boolean := False;
+   Update_Request : Boolean := False;
+
+   Running        : Boolean := False;
+
+   WSS : AWS.Server.HTTP (3, 4433, True,
+                          Runme_CB.Service_Sec'Access, False);
+
+   WS  : AWS.Server.HTTP (3, 1234, False,
+                          Runme_CB.Service'Access, True);
+
+   Output : Text_IO.File_Type;
+
+   ----------
+   -- Stop --
+   ----------
+
+   procedure Stop is
+   begin
+      Stop_Request := True;
+
+      AWS.Server.Shutdown (WS);
+      AWS.Server.Shutdown (WSS);
+      AWS.Log.Stop;
+      Text_IO.Close (Output);
+
+      Running := False;
+      Service.Status.Set
+        (Target    => Server.Status,
+         New_State => Service.Status.Stopped_State);
+   end Stop;
+
+   -----------------
+   -- Interrogate --
+   -----------------
+
+   procedure Interrogate is
+   begin
+      if Running then
+         Service.Status.Set
+           (Target    => Server.Status,
+            New_State => Service.Status.Run_State);
+      else
+         Service.Status.Set
+           (Target    => Server.Status,
+            New_State => Service.Status.Stopped_State);
+      end if;
+   end Interrogate;
+
+   ----------
+   -- Main --
+   ----------
+
+   procedure Main is
+   begin
+      --  change to the right directory
+      Directory_Operations.Change_Dir (Runme_Info.Get_Executable_Path);
+
+      --  set SSL certificate
+      SSL.Set_Certificate (Runme_Info.Get_Executable_Path & "cert.pem");
+
+      --  all output goes to a log file.
+      Text_IO.Create (Output, Text_IO.Out_File, "runme.service.log");
+      Text_IO.Set_Output (Output);
+
+      --  update state.
+
+      Service.Status.Set
+        (Target    => Server.Status,
+         New_State => Service.Status.Run_State);
+
+      --  start servers.
+
+      Running := True;
+
+      AWS.Server.Start (WSS, "Runme Secure");
+      AWS.Server.Start (WS, "Runme", "/Admin-Page");
+      AWS.Log.Start    (Split => AWS.Log.Daily);
+
+      loop
+         delay 10.0;
+         exit when Stop_Request;
+      end loop;
+   end Main;
+
+end Runme_Service;
