da4ce50fffd655787d6147a5cab1c976f1ab0cd0
Pascal Obry <obry@adacore.com>
2001-05-26 08:59:49 +0000

    Update copyright notice.

--- src/aws-server-hotplug.adb
+++ src/aws-server-hotplug.adb
@@ -1,8 +1,8 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                            Copyright (C) 2000                            --
---                               Pascal Obry                                --
+--                         Copyright (C) 2000-2001                          --
+--                     Dmitriy Anisimkov & Pascal Obry                      --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --
