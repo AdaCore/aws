3e7b09b7f62905f5c5af6a1b97377ddb8de795af
Pascal Obry <obry@adacore.com>
2001-08-04 15:35:48 +0000

    Initial revision.

---
+++ src/aws-config-ini.adb
@@ -0,0 +1,264 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                         Copyright (C) 2000-2001                          --
+--                               ACT-Europe                                 --
+--                                                                          --
+--  Authors: Dmitriy Anisimov - Pascal Obry                                 --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+with Ada.Command_Line;
+with Ada.Strings.Fixed;
+with Ada.Strings.Maps;
+with Ada.Strings.Unbounded;
+with Ada.Text_IO;
+
+with AWS.Utils;
+
+package body AWS.Config.Ini is
+
+   use Ada.Strings.Unbounded;
+
+   ----------
+   -- Read --
+   ----------
+
+   function Read (File_Name : in String := "") return Object is
+
+      use Ada;
+
+      procedure Error_Message (Filename : in String; Message : in String);
+      --  Output error message with filename and line number.
+
+      procedure Process_Ini (Filename : in String);
+      --  Read init file and set variables accordingly.
+
+      function Program_Ini_File return String;
+      --  Returns initialization filename for current server (using the
+      --  executable name and adding .ini)
+
+      procedure Set_Value
+        (Filename : in String;
+         Key      : in String;
+         Value    : in String);
+
+      Line : Natural;
+      --  current line number parsed
+
+      Params : Object := Default_Config;
+
+      -------------------
+      -- Error_Message --
+      -------------------
+
+      procedure Error_Message (Filename : in String; Message : in String) is
+      begin
+         Text_IO.Put ('(' & Filename & ':');
+         Text_IO.Put (AWS.Utils.Image (Line));
+         Text_IO.Put_Line (") " & Message & '.');
+      end Error_Message;
+
+      ---------------
+      -- Set_Value --
+      ---------------
+
+      procedure Set_Value
+        (Filename : in String;
+         Key      : in String;
+         Value    : in String)
+      is
+         use Ada.Strings.Unbounded;
+
+         function "+" (S : in String)
+           return Unbounded_String
+           renames To_Unbounded_String;
+
+         Expected_Type : Unbounded_String;
+
+         I : Parameter_Name;
+
+      begin
+
+         begin
+            I := Parameter_Name'Value (Key);
+         exception
+            when others =>
+               Error_Message (Filename, "unrecognized option " & Key);
+               return;
+         end;
+
+         case Params (I).Kind is
+            when Str =>
+               Expected_Type := +"string";
+               Params (I).Str_Value := +Value;
+
+            when Dir =>
+               Expected_Type := +"string";
+
+               if Value (Value'Last) = '/'
+                 or else Value (Value'Last) = '\'
+               then
+                  Params (I).Dir_Value := +Value;
+               else
+                  Params (I).Dir_Value := +(Value & '/');
+               end if;
+
+            when Pos =>
+               Expected_Type := +"positive";
+               Params (I).Pos_Value := Positive'Value (Value);
+
+            when Dur =>
+               Expected_Type := +"duration";
+               Params (I).Dur_Value := Duration'Value (Value);
+
+            when Bool =>
+               Expected_Type := +"boolean";
+               Params (I).Bool_Value := Boolean'Value (Value);
+
+         end case;
+
+      exception
+         when others =>
+            Error_Message
+              (Filename,
+               "wrong value for " & Key
+               & " " & To_String (Expected_Type) & " expected");
+
+      end Set_Value;
+
+      -----------------
+      -- Process_Ini --
+      -----------------
+
+      procedure Process_Ini (Filename : in String) is
+
+         Separators : constant Strings.Maps.Character_Set
+           := Strings.Maps.To_Set (' ' & ASCII.HT);
+
+         File    : Text_IO.File_Type;
+         Buffer  : String (1 .. 1024);
+         Last    : Natural;
+
+         K_First : Natural;
+         K_Last  : Natural;
+
+         V_First : Natural;
+         V_Last  : Natural;
+
+      begin
+         Text_IO.Open (Name => Filename,
+                       File => File,
+                       Mode => Text_IO.In_File);
+         Line := 0;
+
+         while not Text_IO.End_Of_File (File) loop
+
+            Text_IO.Get_Line (File, Buffer, Last);
+            Line := Line + 1;
+
+            --  Remove comments
+
+            for I in 1 .. Last loop
+               if Buffer (I) = '#' then
+                  Last := I - 1;
+                  exit;
+               end if;
+            end loop;
+
+            if Last /= 0 then
+
+               --  Looks for Key token
+
+               Strings.Fixed.Find_Token
+                 (Buffer (1 .. Last), Separators, Strings.Outside,
+                  K_First, K_Last);
+
+               --  Looks for associated value
+
+               Strings.Fixed.Find_Token
+                 (Buffer (K_Last + 1 .. Last), Separators, Strings.Outside,
+                  V_First, V_Last);
+
+               if K_Last /= 0 and then V_Last /= 0 then
+
+                  declare
+                     Key   : constant String := Buffer (K_First .. K_Last);
+                     Value : constant String := Buffer (V_First .. V_Last);
+                  begin
+                     Set_Value (Filename, Key, Value);
+                  end;
+
+               else
+                  Error_Message (Filename, "wrong format");
+               end if;
+
+            end if;
+         end loop;
+
+         Text_IO.Close (File);
+      exception
+         when Text_IO.Name_Error =>
+            null;
+      end Process_Ini;
+
+      ----------------------
+      -- Program_Ini_File --
+      ----------------------
+
+      function Program_Ini_File return String is
+         Exec_Name : constant String := Ada.Command_Line.Command_Name;
+         Last      : Natural;
+         First     : Natural;
+      begin
+         First := Strings.Fixed.Index
+           (Exec_Name, Strings.Maps.To_Set ("/\"), Going => Strings.Backward);
+
+         if First = 0 then
+            First := Exec_Name'First;
+         end if;
+
+         Last := Strings.Fixed.Index
+           (Exec_Name (First .. Exec_Name'Last), ".", Strings.Backward);
+
+         if Last = 0 then
+            return Exec_Name & ".ini";
+         else
+            return Exec_Name (Exec_Name'First .. Last) & "ini";
+         end if;
+      end Program_Ini_File;
+
+   begin
+      Process_Ini ("aws.ini");
+      Process_Ini (Program_Ini_File);
+
+      if File_Name /= "" then
+         Process_Ini (File_Name);
+      end if;
+
+      return Params;
+   end Read;
+
+end AWS.Config.Ini;
---
+++ src/aws-config-ini.ads
@@ -0,0 +1,41 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                         Copyright (C) 2000-2001                          --
+--                               ACT-Europe                                 --
+--                                                                          --
+--  Authors: Dmitriy Anisimov - Pascal Obry                                 --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+package AWS.Config.Ini is
+
+   function Read (File_Name : in String := "") return Object;
+   --  Read aws.ini, <program name>.ini and <File_Name>.ini files
+   --  if present and return configuration record. File aws.ini is
+   --  parsed before <program name>.ini which is parsed before <File_Name>.ini
+   --  and the later overrides values set by the former.
+
+end AWS.Config.Ini;
