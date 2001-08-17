a43cba4a6b9e7bc5109625299cf512a6edc2dd99
Pascal Obry <obry@adacore.com>
2001-08-17 12:07:40 +0000

    Initial revision.

---
+++ src/aws-server-push.adb
@@ -0,0 +1,320 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                          Copyright (C) 2000-2001                         --
+--                      Dmitriy Anisimkov & Pascal Obry                     --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+with Ada.Unchecked_Deallocation;
+with Ada.Calendar;
+with System;
+with GNAT.Calendar.Time_IO;
+
+with Sockets.Stream_IO;
+
+with AWS.Utils;
+
+package body AWS.Server.Push is
+
+   function To_Holder
+     (Socket      : in Socket_Type;
+      Environment : in Client_Environment;
+      Kind        : in Mode)
+     return Client_Holder;
+
+   procedure Close (Socket : in out Client_Holder);
+
+   New_Line : constant String := ASCII.CR & ASCII.LF;
+   --  HTTP new line.
+
+   Boundary : constant String := "--AWS.Server.Push.Boundary_"
+     & GNAT.Calendar.Time_IO.Image (Ada.Calendar.Clock, "%s")
+     & New_Line;
+   --  This is the multi-part boundary string used by AWS push server.
+
+   ---------------
+   -- To_Holder --
+   ---------------
+
+   function To_Holder
+     (Socket : in Socket_Type;
+      Environment : in Client_Environment;
+      Kind : in Mode)
+     return Client_Holder
+   is
+      use Sockets.Stream_IO;
+      Stream : Stream_Access := new Socket_Stream_Type;
+   begin
+      Initialize (Socket_Stream_Type'Class (Stream.all),
+                  Sockets.Socket_FD (Socket));
+
+      return (Kind        => Kind,
+              Environment => Environment,
+              Socket      => new Socket_Type'(Socket),
+              Stream      => Stream);
+   end To_Holder;
+
+   -----------
+   -- Close --
+   -----------
+
+   procedure Close (Socket : in out Client_Holder) is
+
+      procedure Free is new Ada.Unchecked_Deallocation
+        (Ada.Streams.Root_Stream_Type'Class, Stream_Access);
+
+      procedure Free is
+         new Ada.Unchecked_Deallocation (Socket_Type, Socket_Access);
+
+   begin
+      Sockets.Shutdown (Socket.Socket.all);
+      Free (Socket.Stream);
+      Free (Socket.Socket);
+   end Close;
+
+   ---------------
+   -- Broadcast --
+   ---------------
+
+   procedure Broadcast
+     (Server       : in out Object;
+      Data         : in     Client_Output_Type;
+      Content_Type : in     String             := "") is
+   begin
+      Server.Broadcast (Data, Content_Type);
+   end Broadcast;
+
+   --------------
+   -- Data_For --
+   --------------
+
+   procedure Data_For
+     (Server       : in out Object;
+      Client_ID    : in     Client_Key;
+      Data         : in     Client_Output_Type;
+      Content_Type : in     String             := "") is
+   begin
+      Server.Data_For (Client_ID, Data, Content_Type);
+   end Data_For;
+
+   ----------------
+   -- Unregister --
+   ----------------
+
+   procedure Unregister
+     (Server    : in out Object;
+      Client_ID : in     Client_Key) is
+   begin
+      Server.Unregister (Client_ID);
+   end Unregister;
+
+   --------------
+   -- Register --
+   --------------
+
+   procedure Register
+     (Server      : in out Object;
+      Client_ID   : in     Client_Key;
+      Socket      : in     Socket_Type;
+      Environment : in     Client_Environment;
+      Kind        : in     Mode               := Plain) is
+   begin
+      Server.Register (Client_ID,
+                       To_Holder (Socket, Environment, Kind));
+   end Register;
+
+   -----------------
+   -- Object --
+   -----------------
+
+   protected body Object is
+
+      procedure Send_Data
+        (Holder       : in Client_Holder;
+         Data         : in Client_Output_Type;
+         Content_Type : in String);
+      --  Send Data to a client identified by Holder.
+
+      ---------------
+      -- Broadcast --
+      ---------------
+
+      procedure Broadcast
+        (Data : in Client_Output_Type;
+         Content_Type : in String)
+      is
+
+         procedure Action
+           (Key          : in     Client_Key;
+            Value        : in     Client_Holder;
+            Order_Number : in     Positive;
+            Continue     : in out Boolean);
+
+         procedure Free
+           (Key          : in     Client_Key;
+            Value        : in     Client_Holder;
+            Order_Number : in     Positive;
+            Continue     : in out Boolean);
+
+         For_Remove : Table.Table_Type;
+
+         ------------
+         -- Action --
+         ------------
+
+         procedure Action
+           (Key          : in     Client_Key;
+            Value        : in     Client_Holder;
+            Order_Number : in     Positive;
+            Continue     : in out Boolean) is
+         begin
+            Send_Data (Value, Data, Content_Type);
+         exception
+            when others =>
+               Table.Insert (For_Remove, Key, Value);
+         end Action;
+
+         ----------
+         -- Free --
+         ----------
+
+         procedure Free
+           (Key          : in     Client_Key;
+            Value        : in     Client_Holder;
+            Order_Number : in     Positive;
+            Continue     : in out Boolean) is
+         begin
+            Unregister (Key);
+         end Free;
+
+         procedure For_Each is new Table.Disorder_Traverse_G (Action);
+
+         procedure Remove_Each is new Table.Disorder_Traverse_G (Free);
+
+      begin
+         For_Each (Container);
+         Remove_Each (For_Remove);
+         Table.Destroy (For_Remove);
+      end Broadcast;
+
+      --------------
+      -- Data_For --
+      --------------
+
+      procedure Data_For
+        (Client_ID    : in Client_Key;
+         Data         : in Client_Output_Type;
+         Content_Type : in String)
+      is
+         Value : constant Client_Holder := Table.Value (Container, Client_ID);
+      begin
+         Send_Data (Value, Data, Content_Type);
+      exception
+         when others =>
+            Unregister (Client_ID);
+            raise Client_Gone;
+      end Data_For;
+
+      --------------
+      -- Register --
+      --------------
+
+      procedure Register
+        (Client_ID : in Client_Key;
+         Holder    : in Client_Holder) is
+      begin
+         Table.Insert (Container, Client_ID, Holder);
+
+         String'Write (Holder.Stream,
+                       "HTTP/1.1 200 OK" & New_Line
+                       & "Server: AWS (Ada Web Server) v" & Version & New_line
+                       & "Connection: Close" & New_Line);
+
+         if Holder.Kind = Chunked then
+            String'Write
+              (Holder.Stream,
+               "Transfer-Encoding: chunked" & New_Line & New_Line);
+
+         elsif Holder.Kind = Multipart then
+            String'Write
+              (Holder.Stream,
+               "Content-type: multipart/x-mixed-replace; boundary="
+               & Boundary & New_Line);
+         end if;
+      end Register;
+
+      ---------------
+      -- Send_Data --
+      ---------------
+
+      procedure Send_Data
+        (Holder       : in Client_Holder;
+         Data         : in Client_Output_Type;
+         Content_Type : in String)
+      is
+
+         Data_To_Send : constant Stream_Output_Type :=
+           To_Stream_Output (Data, Holder.Environment);
+
+      begin
+         if Holder.Kind = Multipart then
+            String'Write
+              (Holder.Stream,
+               Boundary
+               & "Content-type: " & Content_Type & New_Line & New_Line);
+
+         elsif Holder.Kind = Chunked then
+            String'Write
+              (Holder.Stream,
+               Utils.Hex (Data_To_Send'Size / System.Storage_Unit) & New_Line);
+         end if;
+
+         Stream_Output_Type'Write (Holder.Stream, Data_To_Send);
+
+         if Holder.Kind = Multipart then
+            String'Write (Holder.Stream, New_Line & New_Line);
+
+         elsif Holder.Kind = Chunked then
+            String'Write (Holder.Stream, New_Line);
+         end if;
+
+      end Send_Data;
+
+      ----------------
+      -- Unregister --
+      ----------------
+
+      procedure Unregister (Client_ID : in Client_Key) is
+         Value : Client_Holder;
+      begin
+         Table.Remove (Container, Client_ID, Value);
+         Close (Value);
+      end Unregister;
+
+   end Object;
+
+end AWS.Server.Push;
+
---
+++ src/aws-server-push.ads
@@ -0,0 +1,139 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                          Copyright (C) 2000-2001                         --
+--                      Dmitriy Anisimkov & Pascal Obry                     --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+--  Package to support Server Push feature. This is only supported by Netscape
+--  browsers. It will not work with Microsoft Internet Explorer.
+
+with Sockets;
+with Ada.Streams;
+with Table_Of_Strings_And_Static_Values_G;
+
+generic
+
+   type Client_Output_Type (<>) is private;
+   --  Data type client want to send through server push.
+
+   type Stream_Output_Type (<>) is private;
+   --  Data type to be sent through the socket stream.
+
+   type Client_Environment is private;
+   --  Data type to keep client context. This context will be passed to the
+   --  convertion routine below.
+
+   with function To_Stream_Output
+     (Output : in Client_Output_Type;
+      Client : in Client_Environment)
+     return Stream_Output_Type;
+   --  Function used for convert Client_Output_Type to Stream_Output.
+
+package AWS.Server.Push is
+
+   Client_Gone : exception;
+   --  Raised when a client is not responding.
+
+   type Object is limited private;
+
+   type Mode is (Plain, Multipart, Chunked);
+
+   type Client_Holder is tagged private;
+
+   subtype Socket_Type is Sockets.Socket_FD'Class;
+
+   subtype Client_Key is String;
+
+   procedure Register
+     (Server      : in out Object;
+      Client_ID   : in     Client_Key;
+      Socket      : in     Socket_Type;
+      Environment : in     Client_Environment;
+      Kind        : in     Mode               := Plain);
+   --  Add client identified by Client_ID to the server subscription
+   --  list. After registering this client will be able to receive pushed data
+   --  from the server in brodcasting mode.
+
+   procedure Unregister
+     (Server    : in out Object;
+      Client_ID : in     Client_Key);
+   --  Removes client from server subscription list.
+
+   procedure Data_For
+     (Server       : in out Object;
+      Client_ID    : in     Client_Key;
+      Data         : in     Client_Output_Type;
+      Content_Type : in     String             := "");
+   --  Push data to a specified client identified by Client_ID.
+
+   procedure Broadcast
+     (Server       : in out Object;
+      Data         : in     Client_Output_Type;
+      Content_Type : in     String             := "");
+   --  Push data to the every client subscribed to the server.
+
+private
+
+   type Stream_Access is access all Ada.Streams.Root_Stream_Type'Class;
+   type Socket_Access is access all Socket_Type;
+
+   type Client_Holder is tagged record
+      Stream      : Stream_Access;
+      Socket      : Socket_Access;
+      Kind        : Mode;
+      Environment : Client_Environment;
+   end record;
+
+   package Table is new Table_Of_Strings_And_Static_Values_G
+     (Character_Type => Character,
+      String_Type    => String,
+      Less           => "<",
+      Equals         => "=",
+      Value_Type     => Client_Holder);
+
+   protected type Object is
+
+      procedure Register
+        (Client_ID : in Client_Key;
+         Holder    : in Client_Holder);
+
+      procedure Data_For
+        (Client_ID    : in Client_Key;
+         Data         : in Client_Output_Type;
+         Content_Type : in String);
+
+      procedure Broadcast
+        (Data         : in Client_Output_Type;
+         Content_Type : in String);
+
+      procedure Unregister (Client_ID : in Client_Key);
+
+   private
+      Container : Table.Table_Type;
+   end Object;
+
+end AWS.Server.Push;
