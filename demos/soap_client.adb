59c729d50d27b2b04269e5c471648c4a3484a6cf
Pascal Obry <obry@adacore.com>
2001-08-26 21:20:13 +0000

    Initial revision.

---
+++ demos/soap_client.adb
@@ -0,0 +1,124 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2001                            --
+--                      Dmitriy Anisimov - Pascal Obry                      --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+--  A simple SOAP client.
+
+with Ada.Text_IO;
+
+with SOAP.Types;
+with SOAP.Parameters;
+with SOAP.Client;
+with SOAP.Message.Payload;
+with SOAP.Message.XML;
+with SOAP.Message.Response.Error;
+
+procedure SOAP_Client is
+
+   use Ada;
+   use SOAP;
+   use SOAP.Types;
+   use type SOAP.Parameters.Set;
+
+   -----------
+   -- Error --
+   -----------
+
+   procedure Error (E : in Message.Response.Error.Object) is
+      P : constant Parameters.Set := SOAP.Message.Parameters (E);
+   begin
+      Text_IO.Put_Line ("Faultcode   : "
+                        & SOAP.Parameters.Get (P, "faultcode"));
+
+      Text_IO.Put_Line ("Faultstring : "
+                        & SOAP.Parameters.Get (P, "faultstring"));
+   end Error;
+
+   ----------
+   -- Good --
+   ----------
+
+   procedure Good is
+
+      P_Set : Parameters.Set := +I ("p1", 10) & I ("p2", 32) & F ("p3", 12.3);
+
+      P     : Message.Payload.Object;
+
+   begin
+      P := Message.Payload.Build ("This_Proc", P_Set);
+
+      declare
+         R : constant Message.Response.Object'Class :=
+           SOAP.Client.Call ("http://pascal:8080/soapdemo", P);
+
+         P : constant Parameters.Set
+           := SOAP.Message.Parameters (R);
+
+         Myres : constant Integer := SOAP.Parameters.Get (P, "myres");
+      begin
+         Text_IO.Put_Line ("---------- XML Response ----------");
+         Text_IO.Put_Line (Message.XML.Image (R));
+         Text_IO.New_Line;
+         Text_IO.Put_Line ("Myres = " & Integer'Image (Myres));
+         Text_IO.New_Line;
+      end;
+   end Good;
+
+   ---------
+   -- Bad --
+   ---------
+
+   procedure Bad is
+      P_Set : Parameters.Set := +I ("p", 10);
+
+      P     : Message.Payload.Object;
+
+   begin
+      P := Message.Payload.Build ("This_Proc", P_Set);
+
+      declare
+         R : constant Message.Response.Object'Class :=
+           SOAP.Client.Call ("http://pascal:8080/soapdemo", P);
+      begin
+         if Message.Response.Is_Error (R) then
+            Error (Message.Response.Error.Object (R));
+
+         else
+            Text_IO.Put_Line ("ERROR: This should be a SOAP Error Message");
+         end if;
+      end;
+   end Bad;
+
+begin
+   Text_IO.Put_Line ("==> Call GOOD");
+   Good;
+
+   Text_IO.Put_Line ("==> Call BAD");
+   Bad;
+end SOAP_Client;
---
+++ demos/soap_server.adb
@@ -0,0 +1,137 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2001                            --
+--                      Dmitriy Anisimov - Pascal Obry                      --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+--  A simple SOAP server.
+
+with Ada.Text_IO;
+
+with AWS.Response;
+with AWS.Server;
+with AWS.Status;
+
+with SOAP.Message.XML;
+with SOAP.Message.Payload;
+with SOAP.Message.Response.Error;
+with SOAP.Parameters;
+with SOAP.Types;
+
+procedure SOAP_Server is
+
+   use Ada;
+
+   WS  : AWS.Server.HTTP (5);
+
+   -------------
+   -- SOAP_CB --
+   -------------
+
+   function SOAP_CB (Request : in AWS.Status.Data) return AWS.Response.Data is
+      use SOAP.Types;
+      use SOAP.Parameters;
+
+      PL : constant SOAP.Message.Payload.Object
+        := SOAP.Message.Payload.Object
+             (SOAP.Message.XML.Load_Payload (AWS.Status.Payload (Request)));
+
+      P  : constant SOAP.Parameters.Set
+        := SOAP.Message.Parameters (PL);
+
+      R  : SOAP.Message.Response.Object;
+      RP : SOAP.Parameters.Set;
+
+   begin
+      --  Print SOAP request for the demo
+
+      Text_IO.Put_Line ("SOAP Request");
+
+      Text_IO.Put_Line
+        ("   Procedure : " & SOAP.Message.Payload.Procedure_Name (PL));
+
+      Text_IO.Put_Line ("  "
+                        & Natural'Image (SOAP.Parameters.Argument_Count (P))
+                        & " parameter(s)");
+
+      --  Let's have a look at the parameters named, p1, p2, p3
+
+      Text_IO.Put_Line
+        ("   p1 = " & SOAP.Types.Image (SOAP.Parameters.Argument (P, "p1")));
+      Text_IO.Put_Line
+        ("   P2 = " & SOAP.Types.Image (SOAP.Parameters.Argument (P, "p2")));
+      Text_IO.Put_Line
+        ("   p3 = " & SOAP.Types.Image (SOAP.Parameters.Argument (P, "p3")));
+
+      Text_IO.New_Line;
+
+      --  Real job start here
+
+      R := SOAP.Message.Response.From (PL);
+
+      declare
+         P1 : constant Integer := SOAP.Parameters.Get (P, "p1");
+         P2 : constant Integer := SOAP.Parameters.Get (P, "p2");
+      begin
+         RP := +I ("myres", P1 + P2);
+      end;
+
+      SOAP.Message.Set_Parameters (R, RP);
+
+      return SOAP.Message.Response.Build (R);
+
+   exception
+      when SOAP.Types.Data_Error =>
+         --  Here we have a problem with some parameters, return a SOAP error.
+         Text_IO.New_Line;
+
+         return SOAP.Message.Response.Build
+           (SOAP.Message.Response.Error.Build
+            (SOAP.Message.Response.Error.Client, "Parameter error"));
+   end SOAP_CB;
+
+   --------
+   -- CB --
+   --------
+
+   function CB (Request : in AWS.Status.Data) return AWS.Response.Data is
+      URI : constant String := AWS.Status.URI (Request);
+
+   begin
+      if URI = "/soapdemo" then
+         return SOAP_CB (Request);
+
+      else
+         return AWS.Response.Build
+           ("text/html",
+            "<p>This is not a SOAP action !");
+      end if;
+   end CB;
+
+begin
+   AWS.Server.Start (WS, "SOAP demo", Callback => CB'Unrestricted_Access);
+end SOAP_Server;
