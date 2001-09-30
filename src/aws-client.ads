dce3cdc5239f87f447e21835b426c59575586264
Pascal Obry <obry@adacore.com>
2001-09-30 13:29:40 +0000

    (Timeouts_Values): new type. (Get): add Timeouts parameter. (Head): idem. (Put): idem. (Post): idem. (HTTP_Connection): add With_Timeouts discriminant and it is now a  limited private type. Add parameters Current_Phase, Timeouts and  Cleaner. (Create): now a procedure (was a function) this is required to be  able to initialize the limited private type. (Client_Phase): new type. (Cleaner_Task): new task type. (Cleaner_Access): an access type to task type above. (Build_Cleaner): new function.

--- src/aws-client.ads
+++ src/aws-client.ads
@@ -47,13 +47,21 @@ package AWS.Client is
    --  Number of time a data is requested from the Server if the connection
    --  fails.
 
+   type Timeouts_Values is record
+      Send    : Natural;
+      Receive : Natural;
+   end record;
+
+   No_Timeout : constant Timeouts_Values := (0, 0);
+
    function Get
      (URL        : in String;
-      User       : in String := No_Data;
-      Pwd        : in String := No_Data;
-      Proxy      : in String := No_Data;
-      Proxy_User : in String := No_Data;
-      Proxy_Pwd  : in String := No_Data)
+      User       : in String          := No_Data;
+      Pwd        : in String          := No_Data;
+      Proxy      : in String          := No_Data;
+      Proxy_User : in String          := No_Data;
+      Proxy_Pwd  : in String          := No_Data;
+      Timeouts   : in Timeouts_Values := No_Timeout)
      return Response.Data;
    --  retreive the message data given a specific URL. It open a connection
    --  with the server and ask for the resource specified in the URL it then
@@ -69,11 +77,12 @@ package AWS.Client is
 
    function Head
      (URL        : in String;
-      User       : in String := No_Data;
-      Pwd        : in String := No_Data;
-      Proxy      : in String := No_Data;
-      Proxy_User : in String := No_Data;
-      Proxy_Pwd  : in String := No_Data)
+      User       : in String          := No_Data;
+      Pwd        : in String          := No_Data;
+      Proxy      : in String          := No_Data;
+      Proxy_User : in String          := No_Data;
+      Proxy_Pwd  : in String          := No_Data;
+      Timeouts   : in Timeouts_Values := No_Timeout)
      return Response.Data;
    --  Idem as above but we do not get the message body.
    --  Head will retry one time if it fails.
@@ -81,11 +90,12 @@ package AWS.Client is
    function Put
      (URL        : in String;
       Data       : in String;
-      User       : in String := No_Data;
-      Pwd        : in String := No_Data;
-      Proxy      : in String := No_Data;
-      Proxy_User : in String := No_Data;
-      Proxy_Pwd  : in String := No_Data)
+      User       : in String          := No_Data;
+      Pwd        : in String          := No_Data;
+      Proxy      : in String          := No_Data;
+      Proxy_User : in String          := No_Data;
+      Proxy_Pwd  : in String          := No_Data;
+      Timeouts   : in Timeouts_Values := No_Timeout)
      return Response.Data;
    --  Send to the server URL a PUT request with Data
    --  Put will retry one time if it fails.
@@ -93,11 +103,12 @@ package AWS.Client is
    function Post
      (URL        : in String;
       Data       : in String;
-      User       : in String := No_Data;
-      Pwd        : in String := No_Data;
-      Proxy      : in String := No_Data;
-      Proxy_User : in String := No_Data;
-      Proxy_Pwd  : in String := No_Data)
+      User       : in String          := No_Data;
+      Pwd        : in String          := No_Data;
+      Proxy      : in String          := No_Data;
+      Proxy_User : in String          := No_Data;
+      Proxy_Pwd  : in String          := No_Data;
+      Timeouts   : in Timeouts_Values := No_Timeout)
      return Response.Data;
    --  Send to the server URL a POST request with Data
    --  Post will retry one time if it fails.
@@ -105,11 +116,12 @@ package AWS.Client is
    function Post
      (URL        : in String;
       Data       : in Ada.Streams.Stream_Element_Array;
-      User       : in String := No_Data;
-      Pwd        : in String := No_Data;
-      Proxy      : in String := No_Data;
-      Proxy_User : in String := No_Data;
-      Proxy_Pwd  : in String := No_Data)
+      User       : in String          := No_Data;
+      Pwd        : in String          := No_Data;
+      Proxy      : in String          := No_Data;
+      Proxy_User : in String          := No_Data;
+      Proxy_Pwd  : in String          := No_Data;
+      Timeouts   : in Timeouts_Values := No_Timeout)
      return Response.Data;
    --  Idem as above but with binary data.
 
@@ -117,11 +129,12 @@ package AWS.Client is
      (URL        : in String;
       Data       : in String;
       SOAPAction : in String;
-      User       : in String := No_Data;
-      Pwd        : in String := No_Data;
-      Proxy      : in String := No_Data;
-      Proxy_User : in String := No_Data;
-      Proxy_Pwd  : in String := No_Data)
+      User       : in String          := No_Data;
+      Pwd        : in String          := No_Data;
+      Proxy      : in String          := No_Data;
+      Proxy_User : in String          := No_Data;
+      Proxy_Pwd  : in String          := No_Data;
+      Timeouts   : in Timeouts_Values := No_Timeout)
      return Response.Data;
    --  Send to the server URL a POST request with Data
    --  Post will retry one time if it fails.
@@ -130,19 +143,20 @@ package AWS.Client is
    --  Keep-Alive client implementation --
    ---------------------------------------
 
-   type HTTP_Connection is private;
+   type HTTP_Connection (With_Timeouts : Boolean := False) is limited private;
 
-   function Create
-     (Host       : in String;
-      User       : in String   := No_Data;
-      Pwd        : in String   := No_Data;
-      Proxy      : in String   := No_Data;
-      Proxy_User : in String   := No_Data;
-      Proxy_Pwd  : in String   := No_Data;
-      Retry      : in Positive := Retry_Default;
-      SOAPAction : in String   := No_Data;
-      Persistent : in Boolean  := True)
-     return HTTP_Connection;
+   procedure Create
+     (Connection : in out HTTP_Connection;
+      Host       : in     String;
+      User       : in     String          := No_Data;
+      Pwd        : in     String          := No_Data;
+      Proxy      : in     String          := No_Data;
+      Proxy_User : in     String          := No_Data;
+      Proxy_Pwd  : in     String          := No_Data;
+      Retry      : in     Positive        := Retry_Default;
+      SOAPAction : in     String          := No_Data;
+      Persistent : in     Boolean         := True;
+      Timeouts   : in     Timeouts_Values := No_Timeout);
    --  Create a new connection. This is to be used with Keep-Alive client API
    --  below. The request will be tried Retry time if it fails.
 
@@ -188,22 +202,50 @@ private
 
    type Socket_Access is access Sockets.Socket_FD'Class;
 
-   type HTTP_Connection is record
-      Connect_URL : AWS.URL.Object;
-      Host        : Unbounded_String;
-      Host_URL    : AWS.URL.Object;
-      User        : Unbounded_String;
-      Pwd         : Unbounded_String;
-      Proxy       : Unbounded_String;
-      Proxy_URL   : AWS.URL.Object;
-      Proxy_User  : Unbounded_String;
-      Proxy_Pwd   : Unbounded_String;
-      Opened      : Boolean;
-      Persistent  : Boolean;
-      SOAPAction  : Unbounded_String;
-      Cookie      : Unbounded_String;
-      Socket      : Socket_Access;
-      Retry       : Positive;
+   type Client_Phase is (Not_Monitored, Send, Receive, Stopped);
+
+   task type Cleaner_Task (Connection : access HTTP_Connection) is
+      entry Send;
+      --  Activate the send timeout if defined.
+      entry Receive;
+      --  Activate the receive timeout if defined.
+      entry Next_Phase;
+      --  Change phase, stop the current Send timeout and be prepare for the
+      --  Receive timeout. If not on a Send timeout terminate the tasK
+      entry Stop;
+      --  Stop cleaner task, this task will exit.
+   end Cleaner_Task;
+   --  ??? Cleaner_Task is used to monitor the timeouts during the Send and
+   --  Receive phase. This is the current implementation and should be fixed
+   --  at some point. Right now there is no cross implementation of a Socket
+   --  timeout.
+
+   type Cleaner_Access is access Cleaner_Task;
+
+   function Build_Cleaner
+     (Connection : access HTTP_Connection)
+     return Cleaner_Access;
+   --  Returns a Cleaner_Task if timeouts is activated and null otherwise.
+
+   type HTTP_Connection (With_Timeouts : Boolean := False) is limited record
+      Connect_URL   : AWS.URL.Object;
+      Host          : Unbounded_String;
+      Host_URL      : AWS.URL.Object;
+      User          : Unbounded_String;
+      Pwd           : Unbounded_String;
+      Proxy         : Unbounded_String;
+      Proxy_URL     : AWS.URL.Object;
+      Proxy_User    : Unbounded_String;
+      Proxy_Pwd     : Unbounded_String;
+      Opened        : Boolean;
+      Persistent    : Boolean;
+      SOAPAction    : Unbounded_String;
+      Cookie        : Unbounded_String;
+      Socket        : Socket_Access;
+      Retry         : Positive;
+      Current_Phase : Client_Phase;
+      Timeouts      : Timeouts_Values;
+      Cleaner       : Cleaner_Access := Build_Cleaner (HTTP_Connection'Access);
    end record;
 
 end AWS.Client;
