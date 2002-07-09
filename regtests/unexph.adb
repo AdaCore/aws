2a5ba490a134758e7afea868ff81e55c35f1c4df
Dmitriy Anisimkov <anisimkov@ada-ru.org>
2002-07-09 15:19:08 +0000

    Unexpected_Exception_Handler

---
+++ regtests/unexph.adb
@@ -0,0 +1,113 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                         Copyright (C) 2000-2001                          --
+--                               ACT-Europe                                 --
+--                                                                          --
+--  Authors: Dmitriy Anisimokv - Pascal Obry                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+with Ada.Text_IO;
+with Ada.Exceptions;
+
+with AWS.Server;
+with AWS.Client;
+with AWS.Status;
+with AWS.MIME;
+with AWS.Response;
+
+procedure Unexph is
+
+   use Ada;
+   use Ada.Text_IO;
+   use AWS;
+
+   function CB (Request : in Status.Data) return Response.Data;
+   
+   procedure UEH (E : in Ada.Exceptions.Exception_Occurrence;
+            Termination : in Boolean);
+
+   task Server is
+      entry Wait_Start;
+      entry Stop;
+   end Server;
+
+   HTTP : AWS.Server.HTTP;
+
+   R : Response.Data;
+
+   --------
+   -- CB --
+   --------
+
+   function CB (Request : in Status.Data) return Response.Data is
+   begin
+      raise Program_Error;
+      return Response.Build (MIME.Text_HTML, "be happy.");
+   end CB;
+
+   ------------
+   -- Server --
+   ------------
+
+   task body Server is
+   begin
+
+      AWS.Server.Set_Unexpected_Exception_Handler (HTTP,
+         UEH'Unrestricted_Access);
+
+      AWS.Server.Start
+        (HTTP, "Test unexpected exception handler",
+         CB'Unrestricted_Access, Port => 7645, Max_Connection => 3);
+
+      accept Wait_Start;
+      accept Stop;
+
+   exception
+      when E : others =>
+         Put_Line ("Server Error " & Exceptions.Exception_Information (E));
+   end Server;
+
+   procedure UEH (E : in Ada.Exceptions.Exception_Occurrence;
+        Termination : in Boolean)
+   is
+   begin
+      Ada.Text_IO.Put_Line (Exceptions.Exception_Name (E)
+         & ' ' & Boolean'Image (Termination));
+   end;
+
+begin
+   Server.Wait_Start;
+
+   R := Client.Get ("http://localhost:7645/test", Timeouts => (2, 2));
+
+   Server.Stop;
+
+exception
+   when E : others =>
+      Server.Stop;
+      Put_Line ("Main Error " & Exceptions.Exception_Information (E));
+end Unexph;
