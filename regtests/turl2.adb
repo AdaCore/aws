ef4d8cbf2fde7d673aecce69dae9a26fb04c2497
Pascal Obry <obry@adacore.com>
2002-10-03 17:47:52 +0000

    Initial revision.

---
+++ regtests/turl2.adb
@@ -0,0 +1,72 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                         Copyright (C) 2000-2001                          --
+--                               ACT-Europe                                 --
+--                                                                          --
+--  Authors: Dmitriy Anisimkov - Pascal Obry                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+with Ada.Exceptions; use Ada.Exceptions;
+with Ada.Text_IO;    use Ada.Text_IO;
+with AWS.URL;        use AWS.URL;
+
+procedure Turl2 is
+
+   procedure Test (URL : in String) is
+      O : Object;
+   begin
+      O := Parse (URL);
+
+      Put_Line (URL);
+      Put_Line (AWS.URL.URL (O));
+      Put_Line ("   Server     : " & Host (O));
+      Put_Line ("   Port       : " & Port (O));
+      Put_Line ("   Path       : " & Path (O));
+      Put_Line ("   File       : " & File (O));
+      Put_Line ("   Parameters : " & Parameters (O));
+      Put_Line ("   URI        : " & Pathname (O));
+
+      Normalize (O);
+
+      Put_Line ("   *");
+      Put_Line ("   Server     : " & Host (O));
+      Put_Line ("   Port       : " & Port (O));
+      Put_Line ("   Path       : " & Path (O));
+      Put_Line ("   File       : " & File (O));
+      Put_Line ("   Parameters : " & Parameters (O));
+      Put_Line ("   URI        : " & Pathname (O));
+
+   exception
+      when E : URL_Error =>
+         Put_Line ("Error on " & URL & " - " & Exception_Message (E));
+   end Test;
+
+begin
+   Test ("http://www.myserver.com:89/app/request?p1=9&p2=toto");
+   Test ("http://www.myserver.com:89/app/..\..\toto?url=http://titi");
+   Test ("http://www.myserver.com:89/app/.\..\toto?url=http://titi&a=2");
+end Turl2;
