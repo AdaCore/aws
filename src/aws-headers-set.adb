2d3fdd02672be9069ded39682cf67a43341440d2
Dmitriy Anisimkov <anisimkov@ada-ru.org>
2002-12-21 08:13:41 +0000

    (Read): New routine, reading the header from the socket.

--- src/aws-headers-set.adb
+++ src/aws-headers-set.adb
@@ -33,6 +33,7 @@
 with Ada.Strings.Fixed;
 with Ada.Text_IO;
 with AWS.Containers.Tables.Set;
+with AWS.Net.Buffered;
 
 package body AWS.Headers.Set is
 
@@ -70,27 +71,75 @@ package body AWS.Headers.Set is
       Tables.Set.Free (P_List (Headers));
    end Free;
 
-   -----------
-   -- Parse --
-   -----------
+   ----------
+   -- Read --
+   ----------
 
-   procedure Parse (Headers : in out List; Line : in String) is
-      use Ada.Strings;
-      Delimiter_Index : constant Natural := Fixed.Index (Line, ":");
+   procedure Read (Headers : in out List; Socket : in Net.Socket_Type'Class)
+   is
+
+      procedure Parse_Header_Lines (Line : in String);
+      --  Parse the Line eventually catenated with the next line if it is a
+      --  continuation line see [RFC 2616 - 4.2].
+
+      ------------------------
+      -- Parse_Header_Lines --
+      ------------------------
+
+      procedure Parse_Header_Lines (Line : in String) is
+         End_Of_Message : constant String := "";
+      begin
+         if Line = End_Of_Message then
+            return;
+         else
+            declare
+               use Ada.Strings;
+
+               Next_Line       : constant String :=
+                  Net.Buffered.Get_Line (Socket);
+               Delimiter_Index : Natural;
+
+            begin
+               if Next_Line /= End_Of_Message
+                    and then
+                 (Next_Line (1) = ' ' or else Next_Line (1) = ASCII.HT)
+               then
+                  --  Continuing value on the next line. Header fields can be
+                  --  extended over multiple lines by preceding each extra
+                  --  line with at least one SP or HT.
+                  Parse_Header_Lines (Line & Next_Line);
+
+               else
+                  if Debug_Flag then
+                     Ada.Text_IO.Put_Line ('>' & Line);
+                  end if;
+
+                  --  Put name and value to the container separately.
+
+                  Delimiter_Index := Fixed.Index (Line, ":");
+
+                  if Delimiter_Index = 0 then
+                     --  No delimiter, this is not a valid Header Line
+                     raise Format_Error;
+                  end if;
+
+                  Add (Headers,
+                       Name  => Line (Line'First .. Delimiter_Index - 1),
+                       Value => Fixed.Trim
+                                  (Line (Delimiter_Index + 1 .. Line'Last),
+                                   Side => Both));
+
+                  --  Parse next header line.
+
+                  Parse_Header_Lines (Next_Line);
+               end if;
+            end;
+         end if;
+      end Parse_Header_Lines;
+
    begin
-      if Debug_Flag then
-         Ada.Text_IO.Put_Line ('>' & Line);
-      end if;
-
-      if Delimiter_Index = 0 then
-         --  No delimiter, this is not a valid Header Line
-         raise Format_Error;
-      end if;
-
-      Add (Headers,
-           Line (Line'First .. Delimiter_Index - 1),
-           Fixed.Trim (Line (Delimiter_Index + 1 .. Line'Last), Both));
-   end Parse;
+      Parse_Header_Lines (Net.Buffered.Get_Line (Socket));
+   end Read;
 
    -----------
    -- Reset --
