c4900816f077587c4eefe67304ef78ccb6f1f20e
Pascal Obry <obry@adacore.com>
2003-01-30 19:58:59 +0000

    Initial revision.

---
+++ regtests/error_strm.ads
@@ -0,0 +1,68 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2003                            --
+--                               ACT-Europe                                 --
+--                                                                          --
+--  Authors: Dmitriy Anisimokv - Pascal Obry                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+--  Test for user defined stream raising an exception
+
+with Ada.Streams;
+
+with AWS.Resources.Streams;
+
+package Error_Strm is
+
+   use AWS.Resources;
+   use Ada.Streams;
+
+   type File_Tagged is new Streams.Stream_Type with private;
+
+   function End_Of_File
+     (Resource : in File_Tagged)
+      return Boolean;
+
+   procedure Read
+     (Resource : in out File_Tagged;
+      Buffer   :    out Stream_Element_Array;
+      Last     :    out Stream_Element_Offset);
+
+   procedure Close (File : in out File_Tagged);
+
+   procedure Create
+     (Resource : in out AWS.Resources.Streams.Stream_Type'Class;
+      Size     : in     Stream_Element_Offset);
+
+private
+
+   type File_Tagged is new Streams.Stream_Type with record
+      Offset : Stream_Element_Offset;
+      Size   : Stream_Element_Offset;
+   end record;
+
+end Error_Strm;
---
+++ regtests/strm2.adb
@@ -0,0 +1,120 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2003                            --
+--                               ACT-Europe                                 --
+--                                                                          --
+--  Authors: Dmitriy Anisimokv - Pascal Obry                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+--  Test for user defined stream raising an exception
+
+with Ada.Text_IO;
+with Ada.Exceptions;
+with Ada.Streams;
+
+with AWS.Server;
+with AWS.Client;
+with AWS.Status;
+with AWS.MIME;
+with AWS.Response;
+with AWS.Messages;
+
+with AWS.Resources.Streams;
+
+with Error_Strm;
+
+procedure Strm2 is
+
+   use Ada;
+   use Ada.Text_IO;
+   use AWS;
+
+   function CB (Request : in Status.Data) return Response.Data;
+
+   task Server is
+      entry Wait_Start;
+      entry Stop;
+   end Server;
+
+   HTTP    : AWS.Server.HTTP;
+   Connect : Client.HTTP_Connection;
+   R       : Response.Data;
+
+   --------
+   -- CB --
+   --------
+
+   function CB (Request : in Status.Data) return Response.Data is
+      File : AWS.Resources.Streams.Stream_Access := new Error_Strm.File_Tagged;
+   begin
+      if Status.URI (Request) = "/toto" then
+         return AWS.Response.Stream
+           ("text/plain", File, Response.Undefined_Length);
+      else
+         return AWS.Response.Build
+           ("text/plain", "Unknown resource", Messages.S404);
+      end if;
+   end CB;
+
+   ------------
+   -- Server --
+   ------------
+
+   task body Server is
+   begin
+      AWS.Server.Start
+        (HTTP, "Testing user defined stream.",
+         CB'Unrestricted_Access, Port => 1245, Max_Connection => 3);
+
+      accept Wait_Start;
+      accept Stop;
+   exception
+      when E : others =>
+         Put_Line ("Server Error " & Exceptions.Exception_Information (E));
+   end Server;
+
+begin
+   Server.Wait_Start;
+
+   Client.Create
+     (Connection => Connect,
+      Host       => "http://localhost:1245",
+      Timeouts   => (5, 5));
+
+   Client.Get (Connect, R, "/toto");
+
+   Client.Close (Connect);
+
+   Text_IO.Put_Line ("> " & Response.Message_Body (R));
+
+   Server.Stop;
+
+exception
+   when E : others =>
+      Put_Line ("Main Error " & Exceptions.Exception_Information (E));
+      Server.Stop;
+end Strm2;
