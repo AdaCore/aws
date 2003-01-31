3c34f9f6de889bcd2f03c459268008702dd430c1
Pascal Obry <obry@adacore.com>
2003-01-31 17:47:50 +0000

    Initial revision.

---
+++ regtests/unexph2.adb
@@ -0,0 +1,136 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2003                            --
+--                               ACT-Europe                                 --
+--                                                                          --
+--  Authors: Dmitriy Anisimokv - Pascal Obry                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+with Ada.Text_IO;
+with Ada.Exceptions;
+
+with GNAT.OS_Lib;
+
+with AWS.Client;
+with AWS.Exceptions;
+with AWS.MIME;
+with AWS.Response;
+with AWS.Server;
+with AWS.Status;
+with AWS.Log;
+
+procedure Unexph2 is
+
+   use Ada;
+   use Ada.Text_IO;
+   use AWS;
+   use GNAT;
+
+   HTTP : AWS.Server.HTTP;
+
+   R : Response.Data;
+
+   task Server is
+      entry Wait_Start;
+      entry Stop;
+   end Server;
+
+   ----------------------
+   -- Create_500_Tmplt --
+   ----------------------
+
+   procedure Create_500_Tmplt is
+      File : Text_IO.File_Type;
+   begin
+      Text_IO.Create (File, Text_IO.Out_File, "500.tmplt");
+      Text_IO.Put_Line (File, "This is the 500.tmplt file content.");
+      Text_IO.Close (File);
+   end Create_500_Tmplt;
+
+   ----------------------
+   -- Delete_500_Tmplt --
+   ----------------------
+
+   procedure Delete_500_Tmplt is
+      Success : Boolean;
+   begin
+      OS_Lib.Delete_File ("500.tmplt", Success);
+   end Delete_500_Tmplt;
+
+   --------
+   -- CB --
+   --------
+
+   function CB (Request : in Status.Data) return Response.Data is
+   begin
+      raise Constraint_Error;
+      return Response.Build (MIME.Text_HTML, "be happy.");
+   end CB;
+
+   ------------
+   -- Server --
+   ------------
+
+   task body Server is
+   begin
+      AWS.Server.Start
+        (HTTP, "Test default unexpected exception handler",
+         CB'Unrestricted_Access, Port => 1240, Max_Connection => 3);
+
+      accept Wait_Start;
+      accept Stop;
+
+   exception
+      when E : others =>
+         Put_Line ("Server Error " & Ada.Exceptions.Exception_Information (E));
+   end Server;
+
+begin
+   Server.Wait_Start;
+
+   R := Client.Get ("http://localhost:1240/test", Timeouts => (2, 2));
+
+   Text_IO.Put_Line ("----------------------");
+   Text_IO.Put_Line (Response.Message_Body (R));
+
+   Create_500_Tmplt;
+
+   R := Client.Get ("http://localhost:1240/test", Timeouts => (2, 2));
+
+   Text_IO.Put_Line ("----------------------");
+   Text_IO.Put_Line (Response.Message_Body (R));
+   Text_IO.Put_Line ("----------------------");
+
+   Delete_500_Tmplt;
+
+   Server.Stop;
+
+exception
+   when E : others =>
+      Server.Stop;
+      Put_Line ("Main Error " & Ada.Exceptions.Exception_Information (E));
+end Unexph2;
