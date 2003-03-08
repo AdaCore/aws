527d7a3840cb523eb08d64a76774332e54dbeae7
Pascal Obry <obry@adacore.com>
2003-03-08 09:07:31 +0000

    (Line): Fix previous code which does not compile.

--- src/aws-server.adb
+++ src/aws-server.adb
@@ -264,8 +264,6 @@ package body AWS.Server is
 
       HTTP_Server : HTTP_Access;
       Slot_Index  : Positive;
-      Free_Slots_Keep_Alive_Limit : constant Positive
-        := CNF.Free_Slots_Keep_Alive_Limit (HTTP_Server.Properties);
 
    begin
 
@@ -317,10 +315,13 @@ package body AWS.Server is
                end select;
             end if;
 
-            Protocol_Handler
-              (HTTP_Server.all,
-               Slot_Index,
-               Free_Slots >= Free_Slots_Keep_Alive_Limit);
+            declare
+               Keep_Alive_Limit : constant Positive
+                 := CNF.Free_Slots_Keep_Alive_Limit (HTTP_Server.Properties);
+            begin
+               Protocol_Handler
+                 (HTTP_Server.all, Slot_Index, Free_Slots >= Keep_Alive_Limit);
+            end;
 
             HTTP_Server.Slots.Release (Slot_Index);
          end;
