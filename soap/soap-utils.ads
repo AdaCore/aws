544b90b59996fd942b38a0333be7af2677f23946
Pascal Obry <obry@adacore.com>
2003-03-11 08:12:05 +0000

    (To_T_Array): Remove generic formal parameter T_Array_Access. Returns  a T_Array instead of a T_Array_Access. (Safe_Pointers): New package.

--- soap/soap-utils.ads
+++ soap/soap-utils.ads
@@ -30,6 +30,7 @@
 
 --  $Id$
 
+with Ada.Finalization;
 with Ada.Strings.Unbounded;
 
 with SOAP.Types;
@@ -59,9 +60,8 @@ package SOAP.Utils is
    generic
       type T is private;
       type T_Array is array (Positive range <>) of T;
-      type T_Array_Access is access all T_Array;
       with function Get (O : in Types.Object'Class) return T;
-   function To_T_Array (From : in Types.Object_Set) return T_Array_Access;
+   function To_T_Array (From : in Types.Object_Set) return T_Array;
    --  Convert a Types.Object_Set to an array of T
 
    generic
@@ -83,5 +83,34 @@ package SOAP.Utils is
       Name   : in String  := "item")
       return Types.XSD_String;
    --  Returns the SOAP string for the given Unbounded_String value and name
+
+   --  Smart pointers support used for array access in SOAP record. The memory
+   --  used by a safe pointer is released automatically when no more reference
+   --  to the object exists.
+
+   generic
+      type T (<>) is private;
+      type T_Access is access T;
+   package Safe_Pointers is
+
+      type Ref_Counter is private;
+
+      type Safe_Pointer is new Ada.Finalization.Controlled with record
+         Item : T_Access;
+         Ref  : Ref_Counter;
+      end record;
+
+      procedure Initialize (SP : in out Safe_Pointer);
+      procedure Adjust     (SP : in out Safe_Pointer);
+      procedure Finalize   (SP : in out Safe_Pointer);
+
+      function To_Safe_Pointer (Item : in T) return Safe_Pointer;
+      --  Returns a Safe_Pointer for object Item
+
+   private
+
+      type Ref_Counter is access Natural;
+
+   end Safe_Pointers;
 
 end SOAP.Utils;
