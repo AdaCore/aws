4d3a6f6a8934a867ea8570475d488ac4eb010991
Pascal Obry <obry@adacore.com>
2003-03-19 17:03:40 +0000

    Initial revision.

---
+++ regtests/multiple.adb
@@ -0,0 +1,103 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2003                            --
+--                               ACT-Europe                                 --
+--                                                                          --
+--  Authors: Dmitriy Anisimokv - Pascal Obry                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+with Ada.Text_IO;
+with Ada.Exceptions;
+
+with AWS.Server;
+with AWS.Client;
+with AWS.Config;
+with AWS.Status;
+with AWS.MIME;
+with AWS.Response;
+with AWS.Parameters;
+with AWS.Messages;
+
+procedure Multiple is
+
+   use Ada;
+   use Ada.Text_IO;
+   use AWS;
+
+   function CB (Request : in Status.Data) return Response.Data;
+
+   HTTP1 : AWS.Server.HTTP;
+   HTTP2 : AWS.Server.HTTP;
+
+   --------
+   -- CB --
+   --------
+
+   function CB (Request : in Status.Data) return Response.Data is
+      Server : constant AWS.Server.HTTP_Access := AWS.Server.Get_Current;
+   begin
+      return Response.Build
+        (MIME.Text_HTML, "call ok : "
+           & Config.Server_Name (AWS.Server.Config (Server.all)));
+   end CB;
+
+   -------------
+   -- Request --
+   -------------
+
+   procedure Request (URL : in String) is
+      R : Response.Data;
+   begin
+      R := Client.Get (URL);
+      Put_Line ("=> " & Response.Message_Body (R));
+      New_Line;
+   end Request;
+
+begin
+   Put_Line ("Start main, wait for server to start...");
+
+   AWS.Server.Start
+     (HTTP1, "server1",
+      CB'Unrestricted_Access, Port => 1243, Max_Connection => 5);
+
+   AWS.Server.Start
+     (HTTP2, "server2",
+      CB'Unrestricted_Access, Port => 1244, Max_Connection => 5);
+
+   Request ("http://localhost:1243/call");
+   Request ("http://localhost:1244/call");
+   Request ("http://localhost:1244/call");
+   Request ("http://localhost:1243/call");
+
+   AWS.Server.Shutdown (HTTP1);
+   AWS.Server.Shutdown (HTTP2);
+
+   Put_Line ("Exit now");
+exception
+   when E : others =>
+      Put_Line ("Main Error " & Exceptions.Exception_Information (E));
+end Multiple;
