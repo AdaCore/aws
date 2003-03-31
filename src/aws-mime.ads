0d744848988f23a63547b115e4a2a9ee2cd643b5
Pascal Obry <obry@adacore.com>
2003-03-31 16:52:08 +0000

    New implementation, far more complete than the previous one. Add many MIME type constants. (Add_Extension): New procedure. (Add_Regexp): Idem. (Is_Audio): New function. (Is_Image): Idem. (Is_Video): Idem. (Is_Application): Idem. Update copyright notice.

--- src/aws-mime.ads
+++ src/aws-mime.ads
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                         Copyright (C) 2000-2001                          --
+--                         Copyright (C) 2000-2003                          --
 --                                ACT-Europe                                --
 --                                                                          --
 --  Authors: Dmitriy Anisimkov - Pascal Obry                                --
@@ -32,31 +32,131 @@
 
 package AWS.MIME is
 
-   Text_HTML           : aliased constant String := "text/html";
-   Text_XML            : aliased constant String := "text/xml";
-   Text_Plain          : aliased constant String := "text/plain";
+   --  Some content type constants. All of them will be defined into this
+   --  package and associated with the right extensions. It is possible to
+   --  add new MIME types with the routines below or by placing a file named
+   --  aws.mime into the startup directory.
+   --
+   --  A MIME type is written in two parts: type/format
 
-   Image_Gif           : aliased constant String := "image/gif";
-   Image_Jpeg          : aliased constant String := "image/jpeg";
-   Image_Png           : aliased constant String := "image/png";
+   ----------
+   -- Text --
+   ----------
 
-   Appl_Postscript     : aliased constant String := "application/postscript";
-   Appl_Pdf            : aliased constant String := "application/pdf";
-   Appl_Zip            : aliased constant String := "application/zip";
-   Appl_Octet_Stream   : aliased constant String := "application/octet-stream";
+   Text_HTML                   : constant String := "text/html";
+   Text_Plain                  : constant String := "text/plain";
+   Text_XML                    : constant String := "text/xml";
+   Text_X_SGML                 : constant String := "text/x-sgml";
 
-   Appl_Form_Data      : aliased constant String
+   -----------
+   -- Image --
+   -----------
+
+   Image_Gif                   : constant String := "image/gif";
+   Image_Jpeg                  : constant String := "image/jpeg";
+   Image_Png                   : constant String := "image/png";
+   Image_Tiff                  : constant String := "image/tiff";
+   Image_X_Portable_Anymap     : constant String := "image/x-portable-anymap";
+   Image_X_Portable_Bitmap     : constant String := "image/x-portable-bitmap";
+   Image_X_Portable_Graymap    : constant String := "image/x-portable-graymap";
+   Image_X_Portable_Pixmap     : constant String := "image/x-portable-pixmap";
+   Image_X_RGB                 : constant String := "image/x-rgb";
+   Image_X_Xbitmap             : constant String := "image/x-xbitmap";
+   Image_X_Xpixmap             : constant String := "image/x-xpixmap";
+   Image_X_Xwindowdump         : constant String := "image/x-xwindowdump";
+
+   -----------------
+   -- Application --
+   -----------------
+
+   Application_Postscript      : constant String := "application/postscript";
+   Application_Pdf             : constant String := "application/pdf";
+   Application_Zip             : constant String := "application/zip";
+   Application_Octet_Stream    : constant String := "application/octet-stream";
+   Application_Form_Data       : constant String
      := "application/x-www-form-urlencoded";
+   Application_Mac_Binhex40    : constant String := "application/mac-binhex40";
+   Application_Msword          : constant String := "application/msword";
+   Application_Powerpoint      : constant String := "application/powerpoint";
+   Application_Rtf             : constant String := "application/rtf";
+   Application_X_Compress      : constant String := "application/x-compress";
+   Application_X_Gtar          : constant String := "application/x-gtar";
+   Application_X_Gzip          : constant String := "application/x-gzip";
+   Application_X_Latex         : constant String := "application/x-latex";
+   Application_X_Sh            : constant String := "application/x-sh";
+   Application_X_Shar          : constant String := "application/x-shar";
+   Application_X_Tar           : constant String := "application/x-tar";
+   Application_X_Tcl           : constant String := "application/x-tcl";
+   Application_X_Tex           : constant String := "application/x-tex";
+   Application_X_Texinfo       : constant String := "application/x-texinfo";
+   Application_X_Troff         : constant String := "application/x-troff";
+   Application_X_Troff_Man     : constant String := "application/x-troff-man";
 
-   Multipart_Form_Data : aliased constant String := "multipart/form-data";
+   -----------
+   -- Audio --
+   -----------
 
-   Multipart_Mixed_Replace : constant String := "multipart/x-mixed-replace";
+   Audio_Basic                 : constant String := "audio/basic";
+   Audio_Mpeg                  : constant String := "audio/mpeg";
+   Audio_X_Wav                 : constant String := "audio/x-wav";
+   Audio_X_Pn_Realaudio        : constant String := "audio/x-pn-realaudio";
+   Audio_X_Pn_Realaudio_Plugin : constant String
+     := "audio/x-pn-realaudio-plugin";
+   Audio_X_Realaudio           : constant String := "audio/x-realaudio";
+
+   -----------
+   -- Video --
+   -----------
+
+   Video_Mpeg                  : constant String := "video/mpeg";
+   Video_Quicktime             : constant String := "video/quicktime";
+   Video_X_Msvideo             : constant String := "video/x-msvideo";
+
+   ---------------
+   -- Multipart --
+   ---------------
+
+   Multipart_Form_Data         : constant String := "multipart/form-data";
+   Multipart_X_Mixed_Replace   : constant String
+     := "multipart/x-mixed-replace";
+
+   -------------
+   -- Setting --
+   -------------
 
    function Content_Type (Filename : in String) return String;
-   --  Determine the MIME Content Type from the file's type extension.
-   --  Returns "application/octet-stream" if the file type is unknown.
+   --  Returns the MIME Content Type based on filename's extension or if not
+   --  found the MIME Content type where Filename matches one of the specific
+   --  rules set by Add_Regexp (see below).
+   --  Returns "application/octet-stream" if the file type is unknown
+   --  (i.e. not extension and no regular expression match filename).
 
-   function Is_Text (Mime_Type : in String) return Boolean;
-   --  Returns True if the Mime_Type is a kind of textual data.
+   procedure Add_Extension (Ext : in String; MIME_Type : in String);
+   --  Add extension Ext (file extension without the dot, e.g. "txt") to the
+   --  set of MIME type extension handled by this API. Ext will be mapped to
+   --  the MIME_Type string.
+
+   procedure Add_Regexp (Filename : in String; MIME_Type : in String);
+   --  Add a specific rule to the MIME type table. Filename is a regular
+   --  expression and will be mapped to the MIME_Type string.
+
+   ---------------
+   -- MIME Type --
+   ---------------
+
+   function Is_Text (MIME_Type : in String) return Boolean;
+   --  Returns True if the MIME_Type is a text data
+
+   function Is_Audio (MIME_Type : in String) return Boolean;
+   --  Returns True if the MIME_Type is an audio data
+
+   function Is_Image (MIME_Type : in String) return Boolean;
+   --  Returns True if the MIME_Type is an image data
+
+   function Is_Video (MIME_Type : in String) return Boolean;
+   --  Returns True if the MIME_Type is a video data
+
+   function Is_Application (MIME_Type : in String) return Boolean;
+   --  Returns True if the MIME_Type is an application data
 
 end AWS.MIME;
