78cd97861b7b4edf8d566c95b098c8aafdd74525
Dmitriy Anisimkov <anisimkov@ada-ru.org>
2003-06-20 05:56:06 +0000

    SOAP dispatchers for simple callback.

---
+++ soap/soap-dispatchers-callback.adb
@@ -0,0 +1,72 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                         Copyright (C) 2000-2003                          --
+--                                ACT-Europe                                --
+--                                                                          --
+--  Authors: Dmitriy Anisimkov - Pascal Obry                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+package body SOAP.Dispatchers.Callback is
+
+   ------------
+   -- Create --
+   ------------
+
+   function Create
+     (Base_Callback : in AWS.Response.Callback;
+      SOAP_Callback : in Callback_Routine)
+      return Handler is
+   begin
+      return (Dispatchers.Handler with Base_Callback, SOAP_Callback);
+   end Create;
+
+   -------------------
+   -- Dispatch_Base --
+   -------------------
+
+   function Dispatch_Base
+     (Dispatcher : in Handler;
+      Request    : in AWS.Status.Data)
+      return AWS.Response.Data is
+   begin
+      return Dispatcher.Base_Callback (Request);
+   end Dispatch_Base;
+
+   -------------------
+   -- Dispatch_SOAP --
+   -------------------
+
+   function Dispatch_SOAP
+     (Dispatcher : in Handler;
+      Request    : in Message.Payload.Object)
+      return AWS.Response.Data is
+   begin
+      return Dispatcher.SOAP_Callback (Request);
+   end Dispatch_SOAP;
+
+end SOAP.Dispatchers.Callback;
+
