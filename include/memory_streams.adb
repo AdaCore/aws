1dbc62ed5837d72988a58694eca5e9b083dd5507
Dmitriy Anisimkov <anisimkov@ada-ru.org>
2003-09-24 11:40:05 +0000

    New generic package.

---
+++ include/memory_streams.adb
@@ -0,0 +1,338 @@
+------------------------------------------------------------------------------
+--                       Generic memory stream                              --
+--                                                                          --
+--                        Copyright (C) 2003                                --
+--                        Dmitriy Anisimkov                                 --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+with Ada.Unchecked_Deallocation;
+
+package body Memory_Streams is
+
+   procedure Free (Item : in out Buffer_Access);
+
+   procedure Free is new Ada.Unchecked_Deallocation
+                           (Element_Array, Element_Access);
+   ------------
+   -- Append --
+   ------------
+
+   procedure Append
+     (File  : in out Stream_Type;
+      Value : in     Element_Array)
+   is
+      Block_Length : constant Element_Offset
+        := File.Last_Length + Value'Length;
+
+   begin
+      if File.Pointer /= null then
+         raise Constraint_Error;
+      end if;
+
+      if File.First = null then
+         File.First      := new Buffer_Type;
+
+         if Value'Length >= First_Block_Length then
+            File.First.Data := new Element_Array (1 .. Value'Length);
+         else
+            File.First.Data := new Element_Array (1 .. First_Block_Length);
+         end if;
+
+         File.First.Data (1 .. Value'Length) := Value;
+
+         File.Current     := File.First;
+         File.Last        := File.First;
+         File.Last_Length := Value'Length;
+      elsif Block_Length <= File.Last.Data'Length then
+         File.Last.Data (File.Last_Length + 1
+                         .. Block_Length) := Value;
+         File.Last_Length := Block_Length;
+      else
+         declare
+            Split_Value : constant Element_Index
+              := Value'First + File.Last.Data'Length - File.Last_Length;
+            Next_Length : constant Element_Index
+              := Value'Last - Split_Value + 1;
+         begin
+            File.Last.Data (File.Last_Length + 1 .. File.Last.Data'Last)
+              := Value (Value'First .. Split_Value - 1);
+
+            File.Last.Next := new Buffer_Type;
+            File.Last      := File.Last.Next;
+
+            if Next_Length >= Next_Block_Length then
+               File.Last.Data := new Element_Array (1 .. Next_Length);
+            else
+               File.Last.Data := new Element_Array (1 .. Next_Block_Length);
+            end if;
+
+            File.Last.Data (1 .. Next_Length)
+              := Value (Split_Value .. Value'Last);
+
+            File.Last_Length := Next_Length;
+         end;
+      end if;
+
+      File.Length := File.Length + Value'Length;
+   end Append;
+
+   ------------------
+   -- Back_Pointer --
+   ------------------
+
+   procedure Back_Pointer
+     (File   : in out Stream_Type;
+      Length : in     Element_Offset) is
+   begin
+      if File.Pointer = null then
+         raise Constraint_Error;
+      end if;
+
+      if Length = 0 then
+         Free (File.Pointer);
+
+         return;
+      end if;
+
+      if File.First = null then
+         File.First       := new Buffer_Type;
+         File.First.Data  := File.Pointer;
+         File.Current     := File.First;
+         File.Last        := File.First;
+         File.Last_Length := Length;
+      elsif File.Last.Data'Length = File.Last_Length then
+         File.Last.Next   := new Buffer_Type;
+         File.Last        := File.Last.Next;
+         File.Last.Data   := File.Pointer;
+         File.Last_Length := Length;
+      else
+         declare
+            Ptr : constant Element_Access
+              := new Element_Array'
+                       (File.Last.Data (1 .. File.Last_Length)
+                        & File.Pointer (1 .. Length));
+         begin
+            Free (File.Last.Data);
+            Free (File.Pointer);
+            File.Last.Data := Ptr;
+            File.Last_Length := Ptr'Length;
+         end;
+      end if;
+
+      File.Pointer := null;
+      File.Length  := File.Length + Length;
+   end Back_Pointer;
+
+   -----------
+   -- Close --
+   -----------
+
+   procedure Close (File : in out Stream_Type) is
+      First  : Buffer_Access;
+      Length : Element_Offset := 0;
+   begin
+      while File.First /= null loop
+         First := File.First;
+
+         if First.Next = null then
+            Length := Length + File.Last_Length;
+         else
+            Length := Length + First.Data'Length;
+         end if;
+
+         File.First := First.Next;
+         Free (First);
+      end loop;
+
+      File.Current := null;
+      File.Last    := null;
+
+      Reset (File);
+
+      if File.Length /= Length then
+         raise Program_Error;
+      end if;
+
+      File.Length := 0;
+   end Close;
+
+   -----------------
+   -- End_Of_File --
+   -----------------
+
+   function End_Of_File (Resource : in Stream_Type) return Boolean is
+   begin
+      return Resource.Current.Next = null
+        and then Resource.Current_Offset > Resource.Current.Data'Last;
+   end End_Of_File;
+
+   ----------
+   -- Free --
+   ----------
+
+   procedure Free (Item : in out Buffer_Access) is
+      procedure Deallocate is new Ada.Unchecked_Deallocation
+                                    (Buffer_Type, Buffer_Access);
+   begin
+      Free (Item.Data);
+      Deallocate (Item);
+   end Free;
+
+   -----------------
+   -- Get_Pointer --
+   -----------------
+
+   procedure Get_Pointer
+     (File    : in out Stream_Type;
+      Pointer :    out Element_Access) is
+   begin
+      if File.Pointer /= null then
+         raise Constraint_Error;
+      end if;
+
+      if File.First = null then
+         Pointer := new Element_Array (1 .. First_Block_Length);
+      else
+         Pointer := new Element_Array (1 .. Next_Block_Length);
+      end if;
+
+      File.Pointer := Pointer;
+   end Get_Pointer;
+
+   ----------
+   -- Read --
+   ----------
+
+   procedure Read
+     (Resource : in out Stream_Type;
+      Buffer   :    out Element_Array;
+      Last     :    out Element_Offset)
+   is
+      Buffer_Offset    : Element_Offset := Buffer'First;
+      Block_Over       : Boolean;
+
+      procedure Append (Data : in Element_Array);
+      --  Add the Data to the Buffer till necessary from the
+      --  Resource.Current_Offset and move it.
+
+      ------------
+      -- Append --
+      ------------
+
+      procedure Append (Data : in Element_Array) is
+         Buffer_Len_1    : constant Element_Offset
+           := Buffer'Last - Buffer_Offset;
+         --  Buffer remain length minus 1
+
+         Current_Len_1   : constant Element_Offset
+           := Data'Last - Resource.Current_Offset;
+         --  Data remain length minus 1
+
+         Current_Last    : Element_Index;
+      begin
+         Block_Over := Buffer_Len_1 >= Current_Len_1;
+
+         if Block_Over then
+            Last := Buffer_Offset + Current_Len_1;
+
+            Buffer (Buffer_Offset .. Last)
+              := Data (Resource.Current_Offset .. Data'Last);
+
+            Buffer_Offset := Last + 1;
+
+            Resource.Current_Offset := Data'Last + 1;
+         else
+            Last := Buffer'Last;
+
+            Current_Last := Resource.Current_Offset + Buffer_Len_1;
+
+            Buffer (Buffer_Offset .. Last)
+              := Data (Resource.Current_Offset .. Current_Last);
+
+            Resource.Current_Offset := Current_Last + 1;
+         end if;
+      end Append;
+
+   begin
+      if Resource.Pointer /= null then
+         raise Constraint_Error;
+      end if;
+
+      Last := Buffer'First - 1;
+
+      if Resource.Current = null then
+         return;
+      end if;
+
+      loop
+         if Resource.Current.Next = null then
+            --  Last block.
+
+            Append (Resource.Current.Data (1 .. Resource.Last_Length));
+
+            if Block_Over then
+               Resource.Current := null;
+
+               exit;
+            end if;
+         else
+            Append (Resource.Current.Data.all);
+
+            if Block_Over then
+               Resource.Current := Resource.Current.Next;
+
+               Resource.Current_Offset := Resource.Current.Data'First;
+            end if;
+         end if;
+
+         exit when Last = Buffer'Last;
+      end loop;
+   end Read;
+
+   -----------
+   -- Reset --
+   -----------
+
+   procedure Reset (File : in out Stream_Type) is
+   begin
+      if File.Pointer /= null then
+         raise Constraint_Error;
+      end if;
+
+      File.Current        := File.First;
+      File.Current_Offset := 1;
+   end Reset;
+
+   ----------
+   -- Size --
+   ----------
+
+   function Size (File   : in Stream_Type) return Element_Offset is
+   begin
+      return File.Length;
+   end Size;
+
+end Memory_Streams;
