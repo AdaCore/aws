90c4e536c078a0c2f421b90628edec27b35b9711
Pascal Obry <obry@adacore.com>
2003-10-31 16:47:53 +0000

    (Version): Set to 1.5w.

--- src/aws.ads
+++ src/aws.ads
@@ -34,7 +34,7 @@ package AWS is
 
    pragma Pure;
 
-   Version      : constant String := "1.4w";
+   Version      : constant String := "1.5w";
 
    HTTP_Version : constant String := "HTTP/1.1";
 
