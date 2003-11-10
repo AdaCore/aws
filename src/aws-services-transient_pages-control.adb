a4f901f7cb7b8a11f073e2bdc1834979d729f8ad
Pascal Obry <obry@adacore.com>
2003-11-10 15:29:47 +0000

    Initial revision.

---
+++ src/aws-services-transient_pages-control.adb
@@ -0,0 +1,79 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2003                            --
+--                                ACT-Europe                                --
+--                                                                          --
+--  Authors: Dmitriy Anisimkov - Pascal Obry                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $RCSfile$
+--  $Revision$
+--  $Date$
+--  $Author$
+
+with Ada.Unchecked_Deallocation;
+
+package body AWS.Services.Transient_Pages.Control is
+
+   -----------
+   -- Start --
+   -----------
+
+   procedure Register (Transient_Check_Interval : in Duration) is
+   begin
+      Cleaner_Control.Register (Transient_Check_Interval);
+   end Register;
+
+   --------------
+   -- Shutdown --
+   --------------
+
+   procedure Shutdown is
+
+      procedure Free is
+         new Ada.Unchecked_Deallocation (Cleaner, Cleaner_Access);
+
+      Need_Release : Boolean;
+
+   begin
+      Cleaner_Control.Stop (Need_Release);
+
+      if Need_Release then
+
+         Cleaner_Task.Stop;
+
+         --  Wait for task termination
+
+         while not Cleaner_Task'Terminated loop
+            delay 0.5;
+         end loop;
+
+         --  Release memory
+
+         Free (Cleaner_Task);
+      end if;
+   end Shutdown;
+
+end AWS.Services.Transient_Pages.Control;
---
+++ src/aws-services-transient_pages-control.ads
@@ -0,0 +1,50 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2003                            --
+--                                ACT-Europe                                --
+--                                                                          --
+--  Authors: Dmitriy Anisimkov - Pascal Obry                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $RCSfile$
+--  $Revision$
+--  $Date$
+--  $Author$
+
+--  This API control the transient pages cleaner task. Register must
+--  be called by each Web server and Shutdown to terminate it. This is
+--  done during sever initialization and finalization. Users should
+--  not call directly these routines.
+
+package AWS.Services.Transient_Pages.Control is
+
+   procedure Register (Transient_Check_Interval : in Duration);
+   --  Register the check interval for the cleaner task, increment server's
+   --  count.
+
+   procedure Shutdown;
+   --  Stop session cleaner task if there is no more server running
+
+end AWS.Services.Transient_Pages.Control;
