093ba41b65583fe5b83164d8bf15ab7514d815f1
Pascal Obry <obry@adacore.com>
2003-11-12 22:51:28 +0000

    (Close): Removed. (End_Of_File): Removed. (Read): Removed. (Size): Removed. (Open): Implementation using AWS.Resources.Streams.Disk API.

--- src/aws-resources-files.adb
+++ src/aws-resources-files.adb
@@ -30,29 +30,12 @@
 
 --  $Id$
 
+with Ada.IO_Exceptions;
+
 with AWS.OS_Lib;
-with AWS.Resources.Release;
+with AWS.Resources.Streams.Disk;
 
 package body AWS.Resources.Files is
-
-   -----------
-   -- Close --
-   -----------
-
-   procedure Close (Resource : in out File_Tagged) is
-   begin
-      Stream_IO.Close (Resource.File);
-   end Close;
-
-   -----------------
-   -- End_Of_File --
-   -----------------
-
-   function End_Of_File (Resource : in File_Tagged) return Boolean is
-   begin
-      return Resource.Current > Resource.Last
-        and then Stream_IO.End_Of_File (Resource.File);
-   end End_Of_File;
 
    ---------------
    -- File_Size --
@@ -99,76 +82,19 @@ package body AWS.Resources.Files is
    procedure Open
      (File :    out File_Type;
       Name : in     String;
-      Form : in     String    := "") is
+      Form : in     String    := "")
+   is
+      Stream : AWS.Resources.Streams.Stream_Access;
    begin
-      File := new File_Tagged;
+      Stream := new AWS.Resources.Streams.Disk.Stream_Type;
+      AWS.Resources.Streams.Create (File, Stream);
 
-      Stream_IO.Open
-        (File_Tagged (File.all).File,
-         Stream_IO.In_File, Name, Form);
-
-      File_Tagged (File.all).Stream
-        := Stream_IO.Stream (File_Tagged (File.all).File);
-
+      AWS.Resources.Streams.Disk.Open
+        (AWS.Resources.Streams.Disk.Stream_Type (Stream.all), Name, Form);
    exception
-      when Stream_IO.Name_Error =>
-         AWS.Resources.Release (File);
+      when Ada.IO_Exceptions.Name_Error =>
+         Free (File);
          raise;
    end Open;
-
-   ----------
-   -- Read --
-   ----------
-
-   procedure Read
-     (Resource : in out File_Tagged;
-      Buffer   :    out Stream_Element_Array;
-      Last     :    out Stream_Element_Offset)
-   is
-      use type Stream_Element_Offset;
-
-      Buf_Len : constant Stream_Element_Offset
-        := Resource.Last - Resource.Current + 1;
-   begin
-      if Buffer'Length <= Natural (Buf_Len) then
-         --  Enough chars in the buffer, return them
-         Buffer := Resource.Buffer
-           (Resource.Current .. Resource.Current + Buffer'Length - 1);
-         Resource.Current := Resource.Current + Buffer'Length;
-         Last := Buffer'Last;
-
-      else
-         --  Return the current buffer
-
-         Buffer (Buffer'First .. Buffer'First + Buf_Len - 1)
-           := Resource.Buffer (Resource.Current .. Resource.Last);
-
-         --  And read the remaining data directly on the file
-
-         Read (Resource.Stream.all,
-               Buffer (Buffer'First + Buf_Len .. Buffer'Last),
-               Last);
-
-         Resource.Current := Resource.Buffer'First;
-
-         if Last < Buffer'Last then
-            --  There is no more data, set the Resource object
-            Resource.Last := 0;
-
-         else
-            --  Fill Resource buffer
-            Read (Resource.Stream.all, Resource.Buffer, Resource.Last);
-         end if;
-      end if;
-   end Read;
-
-   ----------
-   -- Size --
-   ----------
-
-   function Size (Resource : in File_Tagged) return Stream_Element_Offset is
-   begin
-      return Stream_Element_Offset (Stream_IO.Size (Resource.File));
-   end Size;
 
 end AWS.Resources.Files;
