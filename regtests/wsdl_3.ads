9f36b30ade57e8ca97c2a52ab7960a4124f79ba8
Pascal Obry <obry@adacore.com>
2003-11-12 22:48:52 +0000

    Initial revision.

---
+++ regtests/wsdl_3.ads
@@ -0,0 +1,57 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2003                            --
+--                                ACT-Europe                                --
+--                                                                          --
+--  Authors: Dmitriy Anisimokv - Pascal Obry                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+with Ada.Strings.Unbounded;
+
+package WSDL_3 is
+
+   use Ada.Strings.Unbounded;
+
+   type Rec1 is record
+      Item1 : Integer;
+      Item2 : Natural;
+      Item3 : Positive;
+   end record;
+
+   type Rec2 is record
+      Field1 : Rec1;
+--      Field2 : Character;
+--      ??? Not properly supported for now
+      Field3 : Unbounded_String;
+      Field4 : Long_Float;
+   end record;
+
+   function Image_Rec1 (Rec : in Rec1) return String;
+
+   function Image_Rec2 (Rec : in Rec2) return String;
+
+end WSDL_3;
