959577f394d78e956393e17c7baefa44ac1342a1
Pascal Obry <obry@adacore.com>
2003-11-25 19:59:15 +0000

    (Base_Name): New routine. (File_Extension): Idem. (File_Name): Idem. (For_Every_Directory_Entry): Idem. Put routines in alphabetical order. Reformat code. Update copyright notice.

--- config/aws-os_lib__posix.adb
+++ config/aws-os_lib__posix.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                         Copyright (C) 2000-2001                          --
+--                         Copyright (C) 2000-2003                          --
 --                                ACT-Europe                                --
 --                                                                          --
 --  Authors: Dmitriy Anisimkov - Pascal Obry                                --
@@ -35,51 +35,82 @@
 
 with Ada.Calendar;
 with Ada.Streams;
+with Ada.Strings.Fixed;
 with Interfaces.C;
 
 with POSIX;
 with POSIX.Calendar;
 with POSIX.File_Status;
+with POSIX.Files;
 
 package body AWS.OS_Lib is
 
-   ---------------------
-   -- Is_Regular_File --
-   ---------------------
+   use Ada;
+   use POSIX;
 
-   function Is_Regular_File (Filename : in String) return Boolean is
+   ---------------
+   -- Base_Name --
+   ---------------
+
+   function Base_Name
+     (Path   : in String;
+      Suffix : in String := "")
+      return String
+   is
+      K : Natural := Strings.Fixed.Index (Path, "/", Strings.Backward);
+      L : Natural;
    begin
-      return POSIX.File_Status.Is_Regular_File
-        (POSIX.File_Status.Get_File_Status (POSIX.To_POSIX_String (Filename)));
-   exception
-      when others =>
-         return False;
-   end Is_Regular_File;
+      if K = 0 then
+         K := Path'First;
+      else
+         K := K + 1;
+      end if;
 
-   ------------------
-   -- Is_Directory --
-   ------------------
+      if Path (Path'Last - Suffix'Length + 1 .. Path'Last) = Suffix then
+         L := Path'Last - Suffix'Length;
+      else
+         L := Path'Last;
+      end if;
 
-   function Is_Directory (Filename : in String) return Boolean is
+      return Path (K .. L);
+   end Base_Name;
+
+   --------------------
+   -- File_Extension --
+   --------------------
+
+   function File_Extension (Path : in String) return String is
+      K : constant Natural
+        := Strings.Fixed.Index (Path, ".", Strings.Backward);
    begin
-      return POSIX.File_Status.Is_Directory
-        (POSIX.File_Status.Get_File_Status (POSIX.To_POSIX_String (Filename)));
-   exception
-      when others =>
-         return False;
-   end Is_Directory;
+      if K = 0 then
+         return "";
+      else
+         return Path (K .. Path'Last);
+      end if;
+   end File_Extension;
+
+   ---------------
+   -- File_Name --
+   ---------------
+
+   function File_Name (Path : in String) return String is
+   begin
+      return Base_Name (Path);
+   end File_Name;
 
    ---------------
    -- File_Size --
    ---------------
 
-   function File_Size (Filename : in String)
-                      return Ada.Streams.Stream_Element_Offset
+   function File_Size
+     (Filename : in String)
+      return Streams.Stream_Element_Offset
    is
       use POSIX.File_Status;
    begin
-      return Ada.Streams.Stream_Element_Offset
-        ((Size_Of (Get_File_Status (POSIX.To_POSIX_String (Filename)))));
+      return Streams.Stream_Element_Offset
+        ((Size_Of (Get_File_Status (To_POSIX_String (Filename)))));
    exception
       when POSIX.POSIX_Error =>
          raise No_Such_File;
@@ -89,17 +120,54 @@ package body AWS.OS_Lib is
    -- File_Timestamp --
    --------------------
 
-   function File_Timestamp (Filename : in String)
-                           return Ada.Calendar.Time is
+   function File_Timestamp (Filename : in String) return Ada.Calendar.Time is
    begin
       return POSIX.Calendar.To_Time
-        (POSIX.File_Status.Last_Modification_Time_Of
-         (POSIX.File_Status.Get_File_Status
-          (POSIX.To_POSIX_String (Filename))));
+        (File_Status.Last_Modification_Time_Of
+           (File_Status.Get_File_Status
+              (To_POSIX_String (Filename))));
    exception
       when POSIX.POSIX_Error =>
          raise No_Such_File;
    end File_Timestamp;
+
+   -------------------------------
+   -- For_Every_Directory_Entry --
+   -------------------------------
+
+   procedure For_Every_Directory_Entry (Directory_Name : in String) is
+
+      procedure Action
+        (D_Entry : in     Files.Directory_Entry;
+         Quit    : in out Boolean);
+
+      ------------
+      -- Action --
+      ------------
+
+      procedure Action
+        (D_Entry : in     Files.Directory_Entry;
+         Quit    : in out Boolean)
+      is
+         Filename : constant String
+           := To_String (Files.Filename_Of (D_Entry));
+      begin
+         Action (Filename, Is_Directory (Filename), Quit);
+      end Action;
+
+      --------------
+      -- Iterator --
+      --------------
+
+      procedure Iterator is
+         new POSIX.Files.For_Every_Directory_Entry (Action);
+
+   begin
+      Iterator (To_POSIX_String (Directory_Name));
+   exception
+      when POSIX_Error =>
+         raise No_Such_File;
+   end For_Every_Directory_Entry;
 
    ---------------
    -- GMT_Clock --
@@ -149,5 +217,31 @@ package body AWS.OS_Lib is
                                     + gmt.tm_min * 60
                                     + gmt.tm_sec));
    end GMT_Clock;
+
+   ------------------
+   -- Is_Directory --
+   ------------------
+
+   function Is_Directory (Filename : in String) return Boolean is
+   begin
+      return File_Status.Is_Directory
+        (File_Status.Get_File_Status (To_POSIX_String (Filename)));
+   exception
+      when others =>
+         return False;
+   end Is_Directory;
+
+   ---------------------
+   -- Is_Regular_File --
+   ---------------------
+
+   function Is_Regular_File (Filename : in String) return Boolean is
+   begin
+      return File_Status.Is_Regular_File
+        (File_Status.Get_File_Status (To_POSIX_String (Filename)));
+   exception
+      when others =>
+         return False;
+   end Is_Regular_File;
 
 end AWS.OS_Lib;
