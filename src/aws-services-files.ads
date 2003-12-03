88a71304dc994eb1628b855370d73840d64eb21d
Pascal Obry <obry@adacore.com>
2003-12-03 19:07:11 +0000

    Initial revision.

---
+++ src/aws-services-files.ads
@@ -0,0 +1,50 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2003                            --
+--                                ACT-Europe                                --
+--                                                                          --
+--  Authors: Dmitriy Anisimkov - Pascal Obry                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+with AWS.Response;
+with AWS.Status;
+
+package AWS.Services.Files is
+
+   function Open
+     (Filename : in String;
+      Request  : in Status.Data)
+      return Response.Data;
+   --  Returns a response object for Filename. This routine will return
+   --  the filename using the following rules, returns:
+   --
+   --  1) "filename.gz" if it exists and GZip compression supported by client
+   --  2) "filename"    if it exists
+   --  3) "filename.gz" if it exists and decompress this file on-the-fly
+   --  4) if no file found, return Messages.S404.
+
+end AWS.Services.Files;
