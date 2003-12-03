920239c70277009ff15c8db70328a2f7506ea69c
Pascal Obry <obry@adacore.com>
2003-12-03 15:35:22 +0000

    Update to new API.

--- workspace/pop.adb
+++ workspace/pop.adb
@@ -12,12 +12,15 @@ procedure POP is
    use Ada;
    use AWS;
 
-   Server : AWS.POP.Server;
+   Mailbox : AWS.POP.Mailbox;
 
-   M      : AWS.POP.Message;
-   A      : AWS.Pop.Attachment;
+   M       : AWS.POP.Message;
+   A       : AWS.Pop.Attachment;
 
-   procedure Display (M : in AWS.POP.Message; Quit : in out Boolean) is
+   procedure Display
+     (M     : in     AWS.POP.Message;
+      Index : in     Positive;
+      Quit  : in out Boolean) is
    begin
       Text_IO.Put_Line ("From    " & AWS.POP.From (M));
       Text_IO.Put_Line ("Subject " & AWS.POP.Subject (M));
@@ -25,18 +28,21 @@ procedure POP is
 
    procedure List_Message is new AWS.POP.For_Every_Message (Display);
 
+   procedure List_Message_Header is
+      new AWS.POP.For_Every_Message_Header (Display);
+
 begin
-   Server := AWS.POP.Initialize
-     ("pop.xyz.cc", "username", "password", AWS.POP.Clear_Text);
+   Mailbox := AWS.POP.Initialize
+     ("host", "username", "password", AWS.POP.Clear_Text);
 
-   Text_IO.Put_Line ("M " & Natural'Image (AWS.POP.Message_Count (Server)));
-   Text_IO.Put_Line ("S " & Natural'Image (AWS.POP.Mailbox_Size (Server)));
+   Text_IO.Put_Line ("M " & Natural'Image (AWS.POP.Message_Count (Mailbox)));
+   Text_IO.Put_Line ("S " & Natural'Image (AWS.POP.Size (Mailbox)));
 
-   M := AWS.POP.Get (Server, 3);
+   M := AWS.POP.Get (Mailbox, 3);
 
-   A := AWS.POP.Get (M, 2);
+--     A := AWS.POP.Get (M, 2);
 
-   AWS.POP.Write (A, ".");
+--     AWS.POP.Write (A, ".");
 
    Text_IO.Put_Line ("From    " & AWS.POP.From (M));
    Text_IO.Put_Line ("Subject " & AWS.POP.Subject (M));
@@ -46,7 +52,12 @@ begin
    Text_IO.New_Line;
    Text_IO.Put_Line ("Iterator:");
 
-   List_Message (Server, Remove => False);
+   List_Message (Mailbox, Remove => False);
 
-   AWS.POP.Close (Server);
+   Text_IO.New_Line;
+   Text_IO.Put_Line ("Header Iterator:");
+
+   List_Message_Header (Mailbox);
+
+   AWS.POP.Close (Mailbox);
 end POP;
