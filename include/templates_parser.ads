29b46d5779b97b20a8bf46d768e733165dfceff2
Pascal Obry <obry@adacore.com>
2003-12-07 09:25:17 +0000

    Update copyright notice.

--- include/templates_parser.ads
+++ include/templates_parser.ads
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                             Templates Parser                             --
 --                                                                          --
---                        Copyright (C) 1999 - 2001                         --
+--                        Copyright (C) 1999 - 2003                         --
 --                               Pascal Obry                                --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
