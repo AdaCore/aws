8ab75bbbce55ec9eeb9050ef9566d5f126003ae6
Dmitriy Anisimkov <anisimkov@ada-ru.org>
2003-12-18 13:19:35 +0000

    (Open): Remove call to Support_Compressed.

--- include/templates_parser-input.adb
+++ include/templates_parser-input.adb
@@ -116,7 +116,6 @@ package body Templates_Parser.Input is
 
       File := new File_Record;
       Open (File.all, Name, Form);
-      Support_Compressed (File.all, State => False);
    exception
       when Ada.IO_Exceptions.Name_Error =>
          Free (File);
