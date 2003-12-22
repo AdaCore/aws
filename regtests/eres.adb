61c79471c00c2e0ca7443de18e0ebe3630a5cb09
Dmitriy Anisimkov <anisimkov@ada-ru.org>
2003-12-22 14:12:41 +0000

    Test for AWS.Resources.Exist routine.

---
+++ regtests/eres.adb
@@ -0,0 +1,56 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2003                            --
+--                               ACT-Europe                                 --
+--                                                                          --
+--  Authors: Dmitriy Anisimkov - Pascal Obry                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+with Ada.Text_IO;
+with AWS.Resources;
+with AWS.Utils;
+
+with zresres;
+
+procedure ERes is
+
+   procedure Test (Name : in String) is
+      use AWS.Resources;
+   begin
+      Ada.Text_IO.Put_Line (File_Instance'Image (Exist (Name)));
+   end Test;
+
+begin
+   Test ("this-file-does-not-exist");
+   Test ("filea.txt");
+   Test ("fileb.txt.gz");
+   Test ("fileb.txt");
+   Test ("../demos/adains.png");
+   Test ("file1.txt");
+   Test ("file2.txt.gz");
+   Test ("file2.txt");
+end ERes;
