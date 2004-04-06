6f10aea148b62a78dfdfe93ba08a762cce29a710
Pascal Obry <obry@adacore.com>
2004-04-06 12:18:19 +0000

    (Reply): New routine. (Send): Idem. (Check_Session): Add support for WM_SMTP_SERVER. (Callback): Add handling for /wm_reply and /wm_send URI. (Login): Add support for WM_SMTP_SERVER. (Message): Change variable name from WM_INDEX to WM_MESSAGE. (Summary): Change variable name from WM_INDEX_V to WM_MESSAGE_V.

--- src/aws-services-web_mail.adb
+++ src/aws-services-web_mail.adb
@@ -30,6 +30,7 @@
 
 --  $Id$
 
+with Ada.Strings.Fixed;
 with Ada.Strings.Unbounded;
 
 with AWS.Config;
@@ -40,6 +41,7 @@ with AWS.POP;
 with AWS.Resources.Streams;
 with AWS.Services.Transient_Pages;
 with AWS.Session;
+with AWS.SMTP.Client;
 with AWS.Templates;
 with AWS.Messages;
 with AWS.Utils;
@@ -63,6 +65,12 @@ package body AWS.Services.Web_Mail is
    function Delete (Request : in AWS.Status.Data) return AWS.Response.Data;
    --  Delete a message, returns to the summary page
 
+   function Reply (Request : in AWS.Status.Data) return AWS.Response.Data;
+   --  Returns a reply form
+
+   function Send (Request : in AWS.Status.Data) return AWS.Response.Data;
+   --  Send a message, used by the reply form above
+
    --------------
    -- Callback --
    --------------
@@ -76,9 +84,10 @@ package body AWS.Services.Web_Mail is
       --  Checks current session data and set corresponding variables,
       --  POP_Server is set to Null_Unbounded_String if no session data found.
 
-      POP_Server : Unbounded_String;
-      User_Name  : Unbounded_String;
-      Password   : Unbounded_String;
+      SMTP_Server : Unbounded_String;
+      POP_Server  : Unbounded_String;
+      User_Name   : Unbounded_String;
+      Password    : Unbounded_String;
 
       -------------------
       -- Check_Session --
@@ -87,6 +96,8 @@ package body AWS.Services.Web_Mail is
       procedure Check_Session is
       begin
          if Session.Exist (WM_Session, "WM_POP_SERVER") then
+            SMTP_Server := To_Unbounded_String
+              (String'(Session.Get (WM_Session, "WM_SMTP_SERVER")));
             POP_Server := To_Unbounded_String
               (String'(Session.Get (WM_Session, "WM_POP_SERVER")));
             User_Name := To_Unbounded_String
@@ -111,8 +122,14 @@ package body AWS.Services.Web_Mail is
       elsif URI = "/wm_message" then
          return Message (Request);
 
+      elsif URI = "/wm_reply" then
+         return Reply (Request);
+
       elsif URI = "/wm_delete" then
          return Delete (Request);
+
+      elsif URI = "/wm_send" then
+         return Send (Request);
 
       else
          --  Check for a transient resource
@@ -197,6 +214,8 @@ package body AWS.Services.Web_Mail is
       WM_Session : constant Session.ID      := Status.Session (Request);
       P_List     : constant Parameters.List := Status.Parameters (Request);
    begin
+      Session.Set (WM_Session, "WM_SMTP_SERVER",
+                   Parameters.Get (P_List, "WM_SMTP_SERVER"));
       Session.Set (WM_Session, "WM_POP_SERVER",
                    Parameters.Get (P_List, "WM_POP_SERVER"));
       Session.Set (WM_Session, "WM_USER_NAME",
@@ -334,13 +353,183 @@ package body AWS.Services.Web_Mail is
                  (Templates.Assoc ("AWS_VERSION", AWS.Version),
                   Templates.Assoc ("WM_USER_NAME", User_Name),
                   Templates.Assoc ("WM_POP_SERVER", POP_Server),
-                  Templates.Assoc ("WM_INDEX", No_Message),
+                  Templates.Assoc
+                    ("WM_MESS_COUNT", POP.Message_Count (Mailbox)),
+                  Templates.Assoc ("WM_MESSAGE", No_Message),
                   Templates.Assoc ("WM_SUBJECT", POP.Subject (Mess)),
                   Templates.Assoc ("WM_DATE", POP.Date (Mess)),
                   Templates.Assoc ("WM_FROM", POP.From (Mess)),
                   Templates.Assoc ("WM_CC", POP.CC (Mess)))
                & Get_Content)));
    end Message;
+
+   -----------
+   -- Reply --
+   -----------
+
+   function Reply (Request : in AWS.Status.Data) return AWS.Response.Data is
+      use type Templates.Translate_Table;
+
+      WWW_Root   : String renames Config.WWW_Root (Config.Get_Current);
+      WM_Session : constant Session.ID := Status.Session (Request);
+      P_List     : constant Parameters.List := Status.Parameters (Request);
+
+      SMTP_Server : constant String
+        := Session.Get (WM_Session, "WM_SMTP_SERVER");
+
+      POP_Server : constant String
+        := Session.Get (WM_Session, "WM_POP_SERVER");
+
+      User_Name  : constant String
+        := Session.Get (WM_Session, "WM_USER_NAME");
+
+      Password   : constant String
+        := Session.Get (WM_Session, "WM_PASSWORD");
+
+      No_Message : constant Positive
+        := Positive'Value (Parameters.Get (P_List, "NO_MESSAGE"));
+
+      Mailbox : POP.Mailbox;
+      Mess    : POP.Message;
+
+      function Get_Content return Templates.Translate_Table;
+      --  Returns content, all lines being prefixed
+
+      -----------------
+      -- Get_Content --
+      -----------------
+
+      function Get_Content return Templates.Translate_Table is
+         Prefix  : constant String := "> ";
+         Content : Unbounded_String;
+         K       : Positive := Prefix'Length + 1;
+      begin
+         --  Get message content
+
+         Content := POP.Content (Mess);
+
+         --  Prefix first line with "> "
+
+         Content := Prefix & Content;
+
+         --  Prefix every lines with "> "
+
+         while K < Length (Content) loop
+            if Element (Content, K) = ASCII.LF then
+               Insert (Content, K + 1, Prefix);
+            end if;
+            K := K + 1;
+         end loop;
+
+         return Templates.Translate_Table'
+           (1 => Templates.Assoc ("WM_CONTENT", Content));
+      end Get_Content;
+
+   begin
+      Mailbox := POP.Initialize (POP_Server, User_Name, Password);
+
+      Mess := POP.Get (Mailbox, No_Message);
+
+      POP.Close (Mailbox);
+
+      return Response.Build
+        (MIME.Text_HTML,
+         Unbounded_String'
+           (Templates.Parse
+              (WWW_Root & "/wm_reply.thtml",
+               Templates.Translate_Table'
+                 (Templates.Assoc ("AWS_VERSION", AWS.Version),
+                  Templates.Assoc ("WM_USER_NAME", User_Name),
+                  Templates.Assoc ("WM_SMTP_SERVER", SMTP_Server),
+                  Templates.Assoc ("WM_POP_SERVER", POP_Server),
+                  Templates.Assoc
+                    ("WM_MESS_COUNT", POP.Message_Count (Mailbox)),
+                  Templates.Assoc ("WM_MESSAGE", No_Message),
+                  Templates.Assoc ("WM_SUBJECT", POP.Subject (Mess)),
+                  Templates.Assoc ("WM_DATE", POP.Date (Mess)),
+                  Templates.Assoc ("WM_FROM", POP.From (Mess)),
+                  Templates.Assoc ("WM_CC", POP.CC (Mess)))
+               & Get_Content)));
+   end Reply;
+
+   ----------
+   -- Send --
+   ----------
+
+   function Send (Request : in AWS.Status.Data) return AWS.Response.Data is
+      WWW_Root   : String renames Config.WWW_Root (Config.Get_Current);
+      WM_Session : constant Session.ID := Status.Session (Request);
+      P_List     : constant Parameters.List := Status.Parameters (Request);
+
+      POP_Server : constant String
+        := Session.Get (WM_Session, "WM_POP_SERVER");
+
+      SMTP_Server : constant String
+        := Session.Get (WM_Session, "WM_SMTP_SERVER");
+
+      User_Name  : constant String
+        := Session.Get (WM_Session, "WM_USER_NAME");
+
+      function Get_From return SMTP.E_Mail_Data;
+      --  Build the From e-mail
+
+      --------------
+      -- Get_From --
+      --------------
+
+      function Get_From return SMTP.E_Mail_Data is
+         K      : constant Natural := Strings.Fixed.Index (POP_Server, ".");
+         E_Mail : Unbounded_String;
+      begin
+         if K = 0 then
+            --  No domain specified, this is a local machine
+            E_Mail := To_Unbounded_String (User_Name & '@' & POP_Server);
+         else
+            --  Get the domain name after the first dot
+            E_Mail := To_Unbounded_String
+              (User_Name & '@' & POP_Server (K + 1 .. POP_Server'Last));
+         end if;
+
+         return SMTP.E_Mail (To_String (E_Mail), To_String (E_Mail));
+      end Get_From;
+
+      WM_SMTP     : SMTP.Receiver;
+      Result      : SMTP.Status;
+   begin
+      WM_SMTP := SMTP.Client.Initialize (SMTP_Server);
+
+      SMTP.Client.Send
+        (WM_SMTP,
+         From    => Get_From,
+         To      => SMTP.Parse (Parameters.Get (P_List, "WM_TO")),
+         Subject => Parameters.Get (P_List, "WM_SUBJECT"),
+         Message => Parameters.Get (P_List, "WM_CONTENT"),
+         Status  => Result);
+
+      if SMTP.Is_Ok (Result) then
+         return Response.URL ("/wm_summary");
+      else
+         return Response.Build
+           (MIME.Text_HTML,
+            String'
+              (Templates.Parse
+                 (WWW_Root & "/wm_error.thtml",
+                  Templates.Translate_Table'
+                    (1 => Templates.Assoc
+                       ("ERROR_MESSAGE", SMTP.Status_Message (Result))))));
+      end if;
+
+   exception
+      when Constraint_Error =>
+         return Response.Build
+           (MIME.Text_HTML,
+            String'
+              (Templates.Parse
+                 (WWW_Root & "/wm_error.thtml",
+                  Templates.Translate_Table'
+                    (1 => Templates.Assoc
+                       ("ERROR_MESSAGE", "Can't parse e-mail")))));
+   end Send;
 
    -------------
    -- Summary --
@@ -416,7 +605,7 @@ package body AWS.Services.Web_Mail is
                   Templates.Assoc
                     ("WM_MESS_COUNT", POP.Message_Count (Mailbox)),
                   Templates.Assoc ("WM_MAILBOX_SIZE", POP.Size (Mailbox)),
-                  Templates.Assoc ("WM_INDEX_V", Index_V),
+                  Templates.Assoc ("WM_MESSAGE_V", Index_V),
                   Templates.Assoc ("WM_SIZE_V", Size_V),
                   Templates.Assoc ("WM_FROM_V", From_V),
                   Templates.Assoc ("WM_DATE_V", Date_V),
