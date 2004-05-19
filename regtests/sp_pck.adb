72d7f2269f6ea7351167525d8f419c0b5077ae85
Dmitriy Anisimkov <anisimkov@ada-ru.org>
2004-05-19 06:19:06 +0000

    All common code moved to Sp_Pck.

--- regtests/sp_pck.adb
+++ regtests/sp_pck.adb
@@ -32,7 +32,89 @@
 
 --  Server push regression test.
 
+with Ada.Strings.Fixed;
+with Ada.Exceptions;
+
+with AWS.Messages;
+with AWS.MIME;
+with AWS.OS_Lib;
+with AWS.Parameters;
+
 package body Sp_Pck is
+
+   use Ada;
+   use Ada.Text_IO;
+   use AWS;
+
+   --------
+   -- CB --
+   --------
+
+   function CB (Request : in Status.Data) return Response.Data is
+      use AWS.Parameters;
+      P_List : List := AWS.Status.Parameters (Request);
+      Mode_Image : String := Get (P_List, "mode");
+      Mode_Value : Server_Push.Mode := Server_Push.Mode'Value (Mode_Image);
+   begin
+      Server_Push.Register
+        (Server      => Push,
+         Client_ID   => Mode_Image,
+         Socket      => AWS.Status.Socket (Request),
+         Environment => Picture (Mode_Value),
+         Kind        => Mode_Value);
+      return Response.Socket_Taken;
+   end CB;
+
+   --------------------
+   -- Client_Process --
+   --------------------
+
+   procedure Client_Process (URL : String) is
+   begin
+      Data := 1000.0;
+
+      --  Init the all modes server push transfers.
+
+      for J in Connect'Range loop
+         Client.Create
+           (Connection  => Connect (J),
+            Host        => URL,
+            Timeouts    => (15, 15),
+            Server_Push => True);
+
+         Client.Get (Connect (J), Answer, "/uri?mode="
+            & Server_Push.Mode'Image (J));
+      end loop;
+
+      for K in 1 .. 32 loop
+
+         Data := Data + 1.0;
+
+         Server_Push.Send (Push, Data, "text/plain");
+
+         for J in Connect'Range loop
+            Put_Line ("--------------------------------");
+            Output (Client.Read_Until (Connect (J), End_Of_Part));
+
+            Data := Data + 0.01;
+
+            Server_Push.Send_To
+              (Push,
+               Server_Push.Mode'Image (J),
+               Data,
+               "text/plain");
+
+            Output (Client.Read_Until (Connect (J), End_Of_Part));
+         end loop;
+
+      end loop;
+
+      for J in Connect'Range loop
+         Client.Close (Connect (J));
+      end loop;
+
+      Server_Push.Unregister_Clients (Push);
+   end Client_Process;
 
    -----------
    -- Image --
@@ -45,5 +127,33 @@ package body Sp_Pck is
    begin
       return Format.Image (Data, Env) & End_Of_Part;
    end Image;
+
+   ------------
+   -- Output --
+   ------------
+
+   procedure Output (Data : String) is
+      Ignore_Sample : constant String := "--AWS.Push.Boundary_";
+      use Ada.Strings;
+      First : Positive := Data'First;
+      Index : Natural;
+   begin
+      loop
+         Index := Fixed.Index (Data (First .. Data'Last), CRLF);
+         if Index = 0 then
+            Put (Data (First .. Data'Last));
+            exit;
+         else
+            if Index - First < Ignore_Sample'Length
+              or else Data (First .. First + Ignore_Sample'Length - 1)
+                       /= Ignore_Sample
+            then
+               Put_Line (Data (First .. Index - 1));
+            end if;
+            First := Index + CRLF'Length;
+         end if;
+         exit when First > Data'Last;
+      end loop;
+   end Output;
 
 end Sp_Pck;
