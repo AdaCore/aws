cb5e806bbfa8e0b2f620e5bf1f6c771194ac9537
Pascal Obry <obry@adacore.com>
2004-05-22 15:51:24 +0000

    Initial revision.

---
+++ regtests/afile.adb
@@ -0,0 +1,158 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2004                            --
+--                               ACT-Europe                                 --
+--                                                                          --
+--  Authors: Dmitriy Anisimkov - Pascal Obry                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+--  Test file as attachment
+
+with Ada.Text_IO;
+
+with AWS.Client;
+with AWS.Messages;
+with AWS.MIME;
+with AWS.Response;
+with AWS.Resources.Streams.Disk;
+with AWS.Server;
+with AWS.Status;
+
+with Afile_Strm;
+
+procedure Afile is
+
+   use Ada;
+   use AWS;
+   use Afile_Strm;
+
+   WS   : Server.HTTP;
+   Size : Natural := 0;
+
+   function CB (Request : in Status.Data) return Response.Data is
+      URI : constant String := Status.URI (Request);
+   begin
+      if URI = "/first" then
+         return Response.File
+           (MIME.Application_Octet_Stream, "afile.o");
+
+      elsif URI = "/second" then
+         return Response.File
+           (MIME.Application_Octet_Stream, "afile.o",
+            Disposition => Response.Attachment);
+
+      elsif URI = "/third" then
+         return Response.File
+           (MIME.Application_Octet_Stream, "afile.o",
+            User_Filename => "you_got_this.o");
+
+      elsif URI = "/fourth" then
+         return Response.File
+           (MIME.Application_Octet_Stream, "afile.o",
+            Disposition   => Response.Attachment,
+            User_Filename => "you_got_this.o");
+
+      elsif URI = "/fifth" then
+         Resources.Streams.Disk.Open (Strm, "afile.o");
+         return Response.Stream (MIME.Application_Octet_Stream, Strm'Access);
+
+      elsif URI = "/sixth" then
+         Resources.Streams.Disk.Open (Strm, "afile.o");
+         return Response.Stream
+           (MIME.Application_Octet_Stream, Strm'Access,
+            Disposition => Response.Attachment);
+
+      elsif URI = "/seventh" then
+         Resources.Streams.Disk.Open (Strm, "afile.o");
+         return Response.Stream
+           (MIME.Application_Octet_Stream, Strm'Access,
+            User_Filename => "a_stream.o");
+
+      elsif URI = "/eighth" then
+         Resources.Streams.Disk.Open (Strm, "afile.o");
+         return Response.Stream
+           (MIME.Application_Octet_Stream, Strm'Access,
+            Disposition   => Response.Attachment,
+            User_Filename => "a_stream.o");
+
+      elsif URI = "/nineth" then
+         return Response.File
+           (MIME.Application_Octet_Stream, "afile.o",
+            Disposition => Response.None);
+
+      elsif URI = "/tenth" then
+         Resources.Streams.Disk.Open (Strm, "afile.o");
+         return Response.Stream
+           (MIME.Application_Octet_Stream, Strm'Access,
+            Disposition => Response.None);
+      else
+         return Response.Build (MIME.Text_HTML, "URI not supported");
+      end if;
+   end CB;
+
+   procedure Call_It (Res : in String) is
+      R : Response.Data;
+   begin
+      R := Client.Get ("http://localhost:1276/" & Res);
+
+      Text_IO.Put (Res);
+      Text_IO.Set_Col (9);
+      Text_IO.Put_Line
+        ("= " & Response.Header (R, Messages.Content_Disposition_Token, 1));
+
+      declare
+         S : constant Natural := Response.Content_Length (R);
+      begin
+         if Size = 0 then
+            Size := S;
+         elsif Size /= S then
+            Text_IO.Put_Line ("Error : Not same size.");
+         end if;
+      end;
+   end Call_It;
+
+begin
+   Server.Start
+     (WS, "afile", CB'Unrestricted_Access, Port => 1276, Max_Connection => 5);
+   Text_IO.Put_Line ("started"); Ada.Text_IO.Flush;
+
+   delay 1.0;
+
+   Call_It ("first");
+   Call_It ("second");
+   Call_It ("third");
+   Call_It ("fourth");
+   Call_It ("fifth");
+   Call_It ("sixth");
+   Call_It ("seventh");
+   Call_It ("eighth");
+   Call_It ("nineth");
+   Call_It ("tenth");
+
+   Server.Shutdown (WS);
+   Text_IO.Put_Line ("shutdown");
+end Afile;
