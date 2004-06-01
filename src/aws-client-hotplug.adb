9442acbeae5d5e18c2fcf5e85a8083c03142acbd
Pascal Obry <obry@adacore.com>
2004-06-01 13:51:31 +0000

    Initial revision.

---
+++ src/aws-client-hotplug.adb
@@ -0,0 +1,145 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2004                            --
+--                                ACT-Europe                                --
+--                                                                          --
+--  Authors: Dmitriy Anisimkov - Pascal Obry                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+with MD5;
+
+with AWS.Communication.Client;
+with AWS.Digest;
+with AWS.Messages;
+with AWS.Response;
+with AWS.Server.Hotplug;
+with AWS.URL;
+
+package body AWS.Client.Hotplug is
+
+   --------------
+   -- Register --
+   --------------
+
+   function Register
+     (Name     : in String;
+      Password : in String;
+      Server   : in String;
+      Regexp   : in String;
+      URL      : in String)
+      return Response.Data
+   is
+      use type Messages.Status_Code;
+      Srv      : AWS.URL.Object;
+      Response : AWS.Response.Data;
+      Digest   : AWS.Digest.Digest_String;
+   begin
+      Srv := AWS.URL.Parse (Server);
+
+      --  Ask the server for a Nonce string
+
+      Response := AWS.Communication.Client.Send_Message
+        (AWS.URL.Host (Srv), AWS.URL.Port (Srv),
+         AWS.Server.Hotplug.Request_Nonce_Message,
+         AWS.Communication.Parameters (Name));
+
+      if AWS.Response.Status_Code (Response) /= Messages.S200 then
+         return Response;
+      end if;
+
+      --  Send registration with a digest encoded using the server Nonce
+
+      declare
+         Nonce : constant String
+           := AWS.Response.Message_Body (Response);
+      begin
+         Digest := AWS.Digest.Create
+           (Name, "hotplug",
+            MD5.Digest (Name & ":hotplug:" & Password),
+            Nonce, "hotplug", Regexp);
+
+         return AWS.Communication.Client.Send_Message
+           (AWS.URL.Host (Srv), AWS.URL.Port (Srv),
+            AWS.Server.Hotplug.Register_Message,
+            AWS.Communication.Parameters (Name, Digest, Regexp, URL));
+      end;
+   exception
+      when AWS.URL.URL_Error =>
+         return AWS.Response.Acknowledge (Messages.S400, "Server URL error");
+   end Register;
+
+   ----------------
+   -- Unregister --
+   ----------------
+
+   function Unregister
+     (Name     : in String;
+      Password : in String;
+      Server   : in String;
+      Regexp   : in String)
+      return Response.Data
+   is
+      use type Messages.Status_Code;
+      Srv      : AWS.URL.Object;
+      Response : AWS.Response.Data;
+      Digest   : AWS.Digest.Digest_String;
+   begin
+      Srv := AWS.URL.Parse (Server);
+
+      --  Ask the server for a Nonce string
+
+      Response := AWS.Communication.Client.Send_Message
+        (AWS.URL.Host (Srv), AWS.URL.Port (Srv),
+         AWS.Server.Hotplug.Request_Nonce_Message,
+         AWS.Communication.Parameters (Name));
+
+      if AWS.Response.Status_Code (Response) /= Messages.S200 then
+         return Response;
+      end if;
+
+      --  Send unregister message with a digest encoded using the server Nonce
+
+      declare
+         Nonce : constant String
+           := AWS.Response.Message_Body (Response);
+      begin
+         Digest := AWS.Digest.Create
+           (Name, "hotplug",
+            MD5.Digest (Name & ":hotplug:" & Password),
+            Nonce, "hotplug", Regexp);
+
+         return AWS.Communication.Client.Send_Message
+           (AWS.URL.Host (Srv), AWS.URL.Port (Srv),
+            AWS.Server.Hotplug.Unregister_Message,
+            AWS.Communication.Parameters (Name, Digest, Regexp));
+      end;
+   exception
+      when AWS.URL.URL_Error =>
+         return AWS.Response.Acknowledge (Messages.S400, "Server URL error");
+   end Unregister;
+
+end AWS.Client.Hotplug;
---
+++ src/aws-client-hotplug.ads
@@ -0,0 +1,64 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2004                            --
+--                                ACT-Europe                                --
+--                                                                          --
+--  Authors: Dmitriy Anisimkov - Pascal Obry                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+with AWS.Response;
+
+package AWS.Client.Hotplug is
+
+   --  Below are two routines to register/unregister hotplug modules into
+   --  server. Note that such server must be configured to accept hotplug
+   --  modules. Password parameter is the clear text paswword, it will be sent
+   --  encoded. An authorization entry for module Name with Password (and the
+   --  given URL host for registration) must be found in the server's
+   --  authorization file. See AWS.Server.Hotplug.Activate.
+
+   function Register
+     (Name     : in String;
+      Password : in String;
+      Server   : in String;
+      Regexp   : in String;
+      URL      : in String)
+      return Response.Data;
+   --  Register hotplug module Name into Server with address URL to respond to
+   --  requests matching Regexp. Server must be a valid URL, http://host:port.
+   --  If port is not specified the default HTTP port is used.
+
+   function Unregister
+     (Name     : in String;
+      Password : in String;
+      Server   : in String;
+      Regexp   : in String)
+      return Response.Data;
+   --  Unregister hotplug module Name responding to Regexp requests from
+   --  Server. See comment above about Password.
+
+end AWS.Client.Hotplug;
