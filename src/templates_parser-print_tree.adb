8cebc51e52b4bae5e7d8aa22c4956012def45c89
Pascal Obry <obry@adacore.com>
2004-06-14 17:27:00 +0000

    (Print_Tree): Output the include file parameters.

--- src/templates_parser-print_tree.adb
+++ src/templates_parser-print_tree.adb
@@ -102,6 +102,19 @@ begin
       when Include_Stmt =>
          Text_IO.Put_Line ("[INCLUDE_STMT] "
                            & To_String (T.File.Info.Filename));
+
+         declare
+            use type Data.Tree;
+         begin
+            for K in T.I_Params'Range loop
+               exit when T.I_Params (K) = null;
+               Print_Indent (Level + 2);
+               Text_IO.Put ("$" & Image (K) & " = ");
+               Data.Print_Tree (T.I_Params (K));
+               Text_IO.New_Line;
+            end loop;
+         end;
+
          Print_Tree (T.File.Info, Level + 1);
          Print_Tree (T.Next, Level);
    end case;
