0ec9c89efef6bccd16f1b80a7ce6943a23dd3985
Pascal Obry <obry@adacore.com>
2004-07-23 19:52:45 +0000

    Use definitions from AWS.OS_Lib.Definitions. Add comments. Fix header.

--- win32/poll.adb
+++ win32/poll.adb
@@ -2,7 +2,7 @@
 --                              Ada Web Server                              --
 --                                                                          --
 --                            Copyright (C) 2004                            --
---                                ACT-Europe                                 --
+--                                ACT-Europe                                --
 --                                                                          --
 --  Authors: Dmitriy Anisimkov - Pascal Obry                                --
 --                                                                          --
@@ -30,16 +30,18 @@
 
 --  $Id$
 
-with AWS.Net.Sets.Thin;
 with System.Address_To_Access_Conversions;
+
+with AWS.Net.Sets.Thin;
 
 function Poll
   (Fds     : in System.Address;
-   Nfds    : in C.unsigned_long;
+   Nfds    : in AWS.OS_Lib.Definitions.nfds_t;
    Timeout : in C.int)
    return C.int
 is
    use AWS.Net.Sets;
+   use AWS.OS_Lib;
 
    use type Thin.Events_Type;
    use type C.int;
@@ -54,7 +56,9 @@ is
    end record;
    pragma Convention (C, Timeval);
 
-   type FD_Array is array (C.unsigned_long range 1 .. Nfds) of C.int;
+   subtype Nfds_Range is C.unsigned_long range 1 .. C.unsigned_long (Nfds);
+
+   type FD_Array is array (Nfds_Range) of C.int;
    pragma Convention (C, FD_Array);
 
    type Poll_Array is array (FD_Array'Range) of Thin.Pollfd;
@@ -95,7 +99,6 @@ is
 
    FD_Events : Thin.Events_Type;
    Rs        : C.int;
-   Good      : Boolean;
 
    ------------
    -- FD_SET --
@@ -112,20 +115,24 @@ begin
       return Failure;
    end if;
 
+   --  Setup (convert data from Poll to Select layout)
+
    Timeout_V.tv_sec  := C.long (Timeout) / 1000;
    Timeout_V.tv_usec := C.long (Timeout) mod 1000;
 
-   for J in 1 .. Nfds loop
+   for J in Nfds_Range loop
       FD_Events := Poll_Ptr (J).Events;
 
-      if (FD_Events and (Thin.POLLIN or Thin.POLLPRI)) /= 0 then
-         FD_SET (Poll_Ptr (J).FD, Rfds);
-      elsif (FD_Events and Thin.POLLOUT) /= 0 then
-         FD_SET (Poll_Ptr (J).FD, Wfds);
+      if (FD_Events and (Definitions.POLLIN or Definitions.POLLPRI)) /= 0 then
+         FD_SET (C.int (Poll_Ptr (J).FD), Rfds);
+      elsif (FD_Events and Definitions.POLLOUT) /= 0 then
+         FD_SET (C.int (Poll_Ptr (J).FD), Wfds);
       end if;
 
-      FD_SET (Poll_Ptr (J).FD, Efds);
+      FD_SET (C.int (Poll_Ptr (J).FD), Efds);
    end loop;
+
+   --  Call Win32 Select
 
    if Timeout < 0 then
       Rs := C_Select
@@ -135,29 +142,28 @@ begin
         (0, Rfds'Address, Wfds'Address, Efds'Address, Timeout_V'Address);
    end if;
 
+   --  Build result (convert back from Select to Poll layout)
+
    if Rs > 0 then
       Rs := 0;
 
-      for J in 1 .. Nfds loop
-         Good := False;
+      for J in Nfds_Range loop
          Poll_Ptr (J).REvents := 0;
 
-         if FD_ISSET (Poll_Ptr (J).FD, Rfds'Address) /= 0 then
-            Good := True;
-            Poll_Ptr (J).REvents := Poll_Ptr (J).REvents or Thin.POLLIN;
+         if FD_ISSET (C.int (Poll_Ptr (J).FD), Rfds'Address) /= 0 then
+            Poll_Ptr (J).REvents := Poll_Ptr (J).REvents or Definitions.POLLIN;
+            Rs := Rs + 1;
          end if;
 
-         if FD_ISSET (Poll_Ptr (J).FD, Wfds'Address) /= 0 then
-            Good := True;
-            Poll_Ptr (J).REvents := Poll_Ptr (J).REvents or Thin.POLLOUT;
+         if FD_ISSET (C.int (Poll_Ptr (J).FD), Wfds'Address) /= 0 then
+            Poll_Ptr (J).REvents
+              := Poll_Ptr (J).REvents or Definitions.POLLOUT;
+            Rs := Rs + 1;
          end if;
 
-         if FD_ISSET (Poll_Ptr (J).FD, Efds'Address) /= 0 then
-            Good := True;
-            Poll_Ptr (J).REvents := Poll_Ptr (J).REvents or Thin.POLLERR;
-         end if;
-
-         if Good then
+         if FD_ISSET (C.int (Poll_Ptr (J).FD), Efds'Address) /= 0 then
+            Poll_Ptr (J).REvents
+              := Poll_Ptr (J).REvents or Definitions.POLLERR;
             Rs := Rs + 1;
          end if;
       end loop;
