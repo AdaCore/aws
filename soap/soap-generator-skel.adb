ef123aa035001ad7423a8a4bbdf99a328d5a9256
Pascal Obry <obry@adacore.com>
2004-07-27 09:36:21 +0000

    (Start_Service): Generate a pragma Style_Checks off for the body. Needed to avoid warnings for procedure being not in alphabetical order.

--- soap/soap-generator-skel.adb
+++ soap/soap-generator-skel.adb
@@ -617,6 +617,8 @@ package body Skel is
       Text_IO.Put_Line (Skel_Adb, "   pragma Warnings (Off);");
       Text_IO.Put_Line (Skel_Adb, "   --  Suppress wrong warnings generated"
                           & " by GNAT (fixed in 3.17)");
+      Text_IO.New_Line (Skel_Adb);
+      Text_IO.Put_Line (Skel_Adb, "   pragma Style_Checks (Off);");
    end Start_Service;
 
 end Skel;
