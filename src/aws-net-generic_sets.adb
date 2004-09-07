56cdb1e26681863e796addaac3a279a0c66bd623
Dmitriy Anisimkov <anisimkov@ada-ru.org>
2004-09-07 05:19:04 +0000

    New generic package.

---
+++ src/aws-net-generic_sets.adb
@@ -0,0 +1,383 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2004                            --
+--                                ACT-Europe                                --
+--                                                                          --
+--  Authors: Dmitriy Anisimkov - Pascal Obry                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+with Ada.Exceptions;
+with Ada.Unchecked_Deallocation;
+
+with AWS.Net.Thin;
+with AWS.OS_Lib.Definitions;
+
+package body AWS.Net.Generic_Sets is
+
+   type Poll_Set_Type is array (Positive range <>) of Thin.Pollfd;
+   pragma Pack (Poll_Set_Type);
+
+   procedure Free is
+     new Ada.Unchecked_Deallocation (Poll_Set_Type, Poll_Set_Access);
+
+   procedure Free is
+     new Ada.Unchecked_Deallocation (Socket_Array, Socket_Array_Access);
+
+   procedure Free is
+     new Ada.Unchecked_Deallocation (Socket_Type'Class, Socket_Access);
+      --  We could not use AWS.Net.Free because Socket_Set_Type did not
+      --  allocate internal socket data.
+
+   procedure Check_Range (Set : in Socket_Set_Type; Index : in Positive);
+   pragma Inline (Check_Range);
+   --  Raise Constraint_Error if Index not in Set range.
+
+   procedure Add_Private
+     (Set    : in out Socket_Set_Type;
+      Socket : in     Socket_Access;
+      Mode   : in     Waiting_Mode);
+   --  Add Socket into Set
+
+   ---------
+   -- Add --
+   ---------
+
+   procedure Add
+     (Set    : in out Socket_Set_Type;
+      Socket : in     Socket_Type'Class;
+      Mode   : in     Waiting_Mode) is
+   begin
+      Add_Private (Set, new Socket_Type'Class'(Socket), Mode);
+      Set.Set (Set.Last).Allocated := True;
+   end Add;
+
+   procedure Add
+     (Set    : in out Socket_Set_Type;
+      Socket : in     Socket_Access;
+      Mode   : in     Waiting_Mode) is
+   begin
+      Add_Private (Set, Socket, Mode);
+      Set.Set (Set.Last).Allocated := False;
+   end Add;
+
+   -----------------
+   -- Add_Private --
+   -----------------
+
+   procedure Add_Private
+     (Set    : in out Socket_Set_Type;
+      Socket : in     Socket_Access;
+      Mode   : in     Waiting_Mode)
+   is
+      use type Thin.Events_Type;
+   begin
+      if Set.Poll = null then
+         if Set.Last /= 0 then
+            raise Constraint_Error;
+         end if;
+
+         if Set.Set /= null then
+            raise Constraint_Error;
+         end if;
+
+         --  Allocate only few elements in array first, because this package
+         --  often would be used for wait just one socket.
+
+         Set.Poll := new Poll_Set_Type (1 .. 4);
+         Set.Set  := new Socket_Array (Set.Poll'Range);
+
+      elsif Set.Last >= Set.Poll'Length then
+         declare
+            Prev_Set  : Socket_Array_Access := Set.Set;
+            Prev_Poll : Poll_Set_Access     := Set.Poll;
+            Increment : Positive;
+
+         begin
+            if Set.Last < 256 then
+               Increment := Set.Last;
+            else
+               Increment := 256;
+            end if;
+
+            Set.Poll := new Poll_Set_Type (1 .. Set.Last + Increment);
+            Set.Set  := new Socket_Array (Set.Poll'Range);
+
+            Set.Poll (Prev_Poll'Range) := Prev_Poll.all;
+            Set.Set  (Prev_Set'Range)  := Prev_Set.all;
+
+            Free (Prev_Set);
+            Free (Prev_Poll);
+         end;
+      end if;
+
+      Set.Last := Set.Last + 1;
+
+      Set.Set (Set.Last).Socket := Socket;
+      Set.Poll (Set.Last).FD := Thin.FD_Type (Get_FD (Socket.all));
+
+      case Mode is
+         when Input  =>
+            Set.Poll (Set.Last).Events := OS_Lib.Definitions.POLLIN;
+         when Output =>
+            Set.Poll (Set.Last).Events := OS_Lib.Definitions.POLLOUT;
+         when Both   =>
+            Set.Poll (Set.Last).Events
+              := OS_Lib.Definitions.POLLIN + OS_Lib.Definitions.POLLOUT;
+      end case;
+   end Add_Private;
+
+   -----------------
+   -- Check_Range --
+   -----------------
+
+   procedure Check_Range (Set : in Socket_Set_Type; Index : in Positive) is
+   begin
+      if Index > Set.Last then
+         raise Constraint_Error;
+      end if;
+   end Check_Range;
+
+   ----------------
+   -- Could_Read --
+   ----------------
+
+   function Could_Read
+     (Set   : in Socket_Set_Type;
+      Index : in Positive)
+      return Boolean
+   is
+      use AWS.OS_Lib.Definitions;
+   begin
+      Check_Range (Set, Index);
+
+      return (Set.Poll (Index).REvents and (POLLIN or POLLPRI)) /= 0;
+   end Could_Read;
+
+   -----------------
+   -- Could_Write --
+   -----------------
+
+   function Could_Write
+     (Set   : in Socket_Set_Type;
+      Index : in Positive)
+      return Boolean
+   is
+      use AWS.OS_Lib.Definitions;
+   begin
+      Check_Range (Set, Index);
+
+      return (Set.Poll (Index).REvents and POLLOUT) /= 0;
+   end Could_Write;
+
+   -----------
+   -- Count --
+   -----------
+
+   function Count (Set : in Socket_Set_Type) return Natural is
+   begin
+      return Set.Last;
+   end Count;
+
+   --------------
+   -- Finalize --
+   --------------
+
+   procedure Finalize (Set : in out Socket_Set_Type) is
+   begin
+      Reset (Set);
+      Free (Set.Set);
+      Free (Set.Poll);
+   end Finalize;
+
+   --------------
+   -- Get_Data --
+   --------------
+
+   function Get_Data
+     (Set   : in Socket_Set_Type;
+      Index : in Positive)
+      return Data_Type is
+   begin
+      Check_Range (Set, Index);
+
+      return Set.Set (Index).Data;
+   end Get_Data;
+
+   ----------------
+   -- Get_Socket --
+   ----------------
+
+   function Get_Socket
+     (Set   : in Socket_Set_Type;
+      Index : in Positive)
+      return Socket_Type'Class is
+   begin
+      Check_Range (Set, Index);
+
+      return Set.Set (Index).Socket.all;
+   end Get_Socket;
+
+   --------------
+   -- In_Range --
+   --------------
+
+   function In_Range
+     (Set   : in Socket_Set_Type;
+      Index : in Positive)
+      return Boolean is
+   begin
+      return Index <= Set.Last;
+   end In_Range;
+
+   --------------
+   -- Is_Error --
+   --------------
+
+   function Is_Error
+     (Set   : in Socket_Set_Type;
+      Index : in Positive)
+      return Boolean
+   is
+      use AWS.OS_Lib.Definitions;
+   begin
+      Check_Range (Set, Index);
+
+      return (Set.Poll (Set.Last).REvents
+              and (POLLERR or POLLHUP or POLLNVAL)) /= 0;
+   end Is_Error;
+
+   ----------
+   -- Next --
+   ----------
+
+   procedure Next
+     (Set   : in     Socket_Set_Type;
+      Index : in out Positive)
+   is
+      use type Thin.Events_Type;
+   begin
+      loop
+         exit when Index > Set.Last
+           or else Set.Poll (Index).REvents /= 0;
+
+         Index := Index + 1;
+      end loop;
+   end Next;
+
+   -------------------
+   -- Remove_Socket --
+   -------------------
+
+   procedure Remove_Socket
+     (Set   : in out Socket_Set_Type;
+      Index : in     Positive) is
+   begin
+      Check_Range (Set, Index);
+
+      if Set.Set (Index).Allocated then
+         Generic_Sets.Free (Set.Set (Index).Socket);
+      end if;
+
+      Set.Set (Index)  := Set.Set (Set.Last);
+      Set.Poll (Index) := Set.Poll (Set.Last);
+
+      Set.Last := Set.Last - 1;
+   end Remove_Socket;
+
+   -----------
+   -- Reset --
+   -----------
+
+   procedure Reset (Set : in out Socket_Set_Type) is
+   begin
+      for K in 1 .. Set.Last loop
+         if Set.Set (K).Allocated then
+            Generic_Sets.Free (Set.Set (K).Socket);
+         end if;
+      end loop;
+
+      Set.Last := 0;
+   end Reset;
+
+   --------------
+   -- Set_Data --
+   --------------
+
+   procedure Set_Data
+     (Set   : in out Socket_Set_Type;
+      Index : in     Positive;
+      Data  : in     Data_Type) is
+   begin
+      Check_Range (Set, Index);
+
+      Set.Set (Index).Data := Data;
+   end Set_Data;
+
+   ----------
+   -- Wait --
+   ----------
+
+   procedure Wait
+     (Set     : in out Socket_Set_Type;
+      Timeout : in     Duration;
+      Count   :    out Natural)
+   is
+      use type Thin.Timeout_Type;
+
+      Result       : Integer;
+      Poll_Timeout : Thin.Timeout_Type;
+   begin
+      if Timeout >= Duration (Thin.Timeout_Type'Last / 1_000) then
+         Poll_Timeout := Thin.Timeout_Type'Last;
+      else
+         Poll_Timeout := Thin.Timeout_Type (Timeout * 1_000);
+      end if;
+
+      Result := Integer
+        (Thin.Poll
+           (FDS     => Set.Poll (Set.Poll'First)'Address,
+            Nfds    => Thin.nfds_t (Set.Last),
+            Timeout => Poll_Timeout));
+
+      if Result < 0 then
+         Ada.Exceptions.Raise_Exception
+           (Socket_Error'Identity, "Poll error code" & Integer'Image (Errno));
+      end if;
+
+      Count := Result;
+   end Wait;
+
+   procedure Wait
+     (Set     : in out Socket_Set_Type;
+      Timeout : in     Duration)
+   is
+      Dummy : Natural;
+   begin
+      Wait (Set, Timeout, Dummy);
+   end Wait;
+
+end AWS.Net.Generic_Sets;
---
+++ src/aws-net-generic_sets.ads
@@ -0,0 +1,168 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2004                            --
+--                                ACT-Europe                                --
+--                                                                          --
+--  Authors: Dmitriy Anisimkov - Pascal Obry                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+--  Waiting on group of sockets for input/output availability.
+
+with Ada.Finalization;
+
+generic
+   type Data_Type is private;
+package AWS.Net.Generic_Sets is
+
+   type Waiting_Mode is (Input, Output, Both);
+
+   type Socket_Set_Type is limited private;
+
+   procedure Add
+     (Set    : in out Socket_Set_Type;
+      Socket : in     Socket_Type'Class;
+      Mode   : in     Waiting_Mode);
+   --  Add socket to the set. Socket can be retreived from the set using
+   --  Get_Socket.
+
+   procedure Add
+     (Set    : in out Socket_Set_Type;
+      Socket : in     Socket_Access;
+      Mode   : in     Waiting_Mode);
+   --  Add socket to the set.
+
+   function Count (Set : in Socket_Set_Type) return Natural;
+   pragma Inline (Count);
+   --  Returns the number of sockets in the Set.
+
+   procedure Wait
+     (Set     : in out Socket_Set_Type;
+      Timeout : in     Duration);
+   pragma Inline (Wait);
+   --  Wait for a socket in the set to be ready for input or output operation.
+   --  Raises Socket_Error if an error occurs.
+
+   procedure Wait
+     (Set     : in out Socket_Set_Type;
+      Timeout : in     Duration;
+      Count   :    out Natural);
+   --  Wait for a socket in the set to be ready for input or output operation.
+   --  Raises Socket_Error if an error occurs. Count would return number of
+   --  activated sockets.
+
+   function Could_Read
+     (Set   : in Socket_Set_Type;
+      Index : in Positive)
+      return Boolean;
+   pragma Inline (Could_Read);
+   --  Return True if data could be read from socket and socket was in Input
+   --  or Both waiting mode.
+
+   function Could_Write
+     (Set   : in Socket_Set_Type;
+      Index : in Positive)
+      return Boolean;
+   pragma Inline (Could_Write);
+   --  Return True if data could be written to socket and socket was in Output
+   --  or Both waiting mode.
+
+   function Is_Error
+     (Set   : in Socket_Set_Type;
+      Index : in Positive)
+      return Boolean;
+   pragma Inline (Is_Error);
+   --  Return True if any error occured with socket while waiting.
+
+   function In_Range
+     (Set   : in Socket_Set_Type;
+      Index : in Positive)
+      return Boolean;
+   pragma Inline (In_Range);
+   --  Return True if Index is in socket set range.
+
+   function Get_Socket
+     (Set   : in Socket_Set_Type;
+      Index : in Positive)
+      return Socket_Type'Class;
+   pragma Inline (Get_Socket);
+   --  Return socket from the Index position or raise Constraint_Error
+   --  if index is more than the number of sockets in set.
+
+   function Get_Data
+     (Set   : in Socket_Set_Type;
+      Index : in Positive)
+      return Data_Type;
+   pragma Inline (Get_Data);
+
+   procedure Set_Data
+     (Set   : in out Socket_Set_Type;
+      Index : in     Positive;
+      Data  : in     Data_Type);
+   pragma Inline (Set_Data);
+
+   procedure Remove_Socket (Set : in out Socket_Set_Type; Index : in Positive);
+   --  Delete socket from Index position from the Set. If the Index is not
+   --  last position in the set, last socket would be placed instead of
+   --  deleted one.
+
+   procedure Next (Set : in Socket_Set_Type; Index : in out Positive);
+   --  Looking for active socket starting from Index and return Index of the
+   --  found active socket. After search use function Get_Socket_State to be
+   --  shure that active socket is found in the Set.
+
+   procedure Reset (Set : in out Socket_Set_Type);
+   --  Clear the set for another use
+
+private
+
+   type Poll_Set_Type;
+
+   type Poll_Set_Access is access all Poll_Set_Type;
+
+   type Socket_Record is record
+      Socket    : Socket_Access;
+      Allocated : Boolean;
+      --  Set to True if socket was allocated internally in the set (it is the
+      --  case when using the Add with Socket_Type'Class parameter).
+      --  Needed to control free on delete.
+
+      Data : Data_Type;
+   end record;
+
+   type Socket_Array is array (Positive range <>) of Socket_Record;
+
+   type Socket_Array_Access is access all Socket_Array;
+
+   type Socket_Set_Type is new Ada.Finalization.Limited_Controlled with record
+      Poll : Poll_Set_Access;
+      Set  : Socket_Array_Access;
+      Last : Natural := 0;
+   end record;
+
+   procedure Finalize (Set : in out Socket_Set_Type);
+
+end AWS.Net.Generic_Sets;
