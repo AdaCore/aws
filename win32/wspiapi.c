9629750e139204e0627c87a1457dff4d6c87aceb
Dmitriy Anisimkov <anisimkov@ada-ru.org>
2004-09-16 05:54:43 +0000

    Need to compile wspiapi.h to object file.

---
+++ win32/wspiapi.c
@@ -0,0 +1,9 @@
+//
+//  Dynamic calls to protocol independent API functions if available, or
+//  emulate it. This file would not be necessary after we would not support
+//  Windows 2000 in AWS.
+//
+//  $Id$
+
+#include <ws2tcpip.h>
+#include "wspiapi.h"
