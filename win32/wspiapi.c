454d570737a726b6397784cbad008096d4164de5
Dmitriy Anisimkov <anisimkov@ada-ru.org>
2004-09-21 15:54:43 +0000

    (WSPIAPI_init): New routine.

--- win32/wspiapi.c
+++ win32/wspiapi.c
@@ -7,3 +7,23 @@
 
 #include <ws2tcpip.h>
 #include "wspiapi.h"
+
+int WSPIAPI_init ()
+{
+   //  This routine should be called in Ada elaboration process to initialize
+   //  internal wspiapi.h static data and make subsequent calls thread safety.
+
+   struct addrinfo Hints;
+   struct addrinfo *Addr;
+   int rc;
+
+   memset(&Hints, 0, sizeof(Hints));
+   Hints.ai_family   = PF_UNSPEC;
+   Hints.ai_socktype = SOCK_STREAM;
+   Hints.ai_flags    = AI_PASSIVE;
+
+   rc = WspiapiGetAddrInfo (NULL, "5001", &Hints, &Addr);
+   WspiapiFreeAddrInfo (Addr);
+
+   return rc; 
+}
