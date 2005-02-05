763ec4a37a47a37a1427efe6e8b241f7f90c1744
Pascal Obry <obry@adacore.com>
2005-02-05 08:05:58 +0000

    (Version): Set to 2.1a. Update copyright notice.

--- src/aws.ads
+++ src/aws.ads
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                         Copyright (C) 2000-2004                          --
+--                         Copyright (C) 2000-2005                          --
 --                                ACT-Europe                                --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
@@ -32,7 +32,7 @@ package AWS is
 
    pragma Pure;
 
-   Version      : constant String := "2.1w";
+   Version      : constant String := "2.1a";
 
    HTTP_Version : constant String := "HTTP/1.1";
 
