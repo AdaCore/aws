06f326694fbd0b9ab25b5802d2f0d096cfc127bd
Pascal Obry <obry@adacore.com>
2005-02-16 11:23:41 +0000

    Initial revision.

---
+++ config/templates_parser-configuration__aws.ads
@@ -0,0 +1,54 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2005                            --
+--                                 AdaCore                                  --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+with Ada.Calendar;
+
+with AWS.OS_Lib;
+
+package Templates_Parser.Configuration is
+
+   subtype Time_Stamp is Ada.Calendar.Time;
+
+   function "="
+     (T1, T2 : in Time_Stamp)
+      return Boolean
+      renames Ada.Calendar."=";
+
+   function Is_Regular_File
+     (Filename : in String)
+      return Boolean
+      renames AWS.OS_Lib.Is_Regular_File;
+
+   function File_Time_Stamp
+     (Filename : in String)
+      return Time_Stamp
+      renames AWS.OS_Lib.File_Time_Stamp;
+
+end Templates_Parser.Configuration;
