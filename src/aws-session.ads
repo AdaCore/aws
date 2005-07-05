1d57541916889314e4d33ba58776da8aaa4c24d4
Dmitriy Anisimkov <anisimkov@ada-ru.org>
2005-07-05 08:47:28 +0000

    Update copyright years range.

--- src/aws-session.ads
+++ src/aws-session.ads
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                         Copyright (C) 2000-2004                          --
+--                         Copyright (C) 2000-2005                          --
 --                                ACT-Europe                                --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
