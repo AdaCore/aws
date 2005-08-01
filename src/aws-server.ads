71ec2a259ca10ef293dfb9a699e67f5278edc348
Pascal Obry <obry@adacore.com>
2005-08-01 17:34:05 +0000

    Minor reformatting.

--- src/aws-server.ads
+++ src/aws-server.ads
@@ -182,7 +182,7 @@ private
       Log    : in out AWS.Log.Object;
       Error  : in     Exceptions.Data;
       Answer : in out Response.Data);
-   --  Default unexpected exception handler.
+   --  Default unexpected exception handler
 
    procedure Socket_Taken (Flag : in Boolean);
    --  Mark socket of the cureent line of the current server taken
