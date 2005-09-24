5c219ba3ce15b50524353b0531fa45f50f2c05d2
Pascal Obry <obry@adacore.com>
2005-09-24 14:16:11 +0000

    (Console_Process): New class. (ada2wsdl): New procedure. (wsdl2aws): Idem. This brings support for AWS SOAP toolset (ada2wsdl and wsdl2aws) to GPS. Using those files it is possible to generate a WSDL document from a spec and the stubs/skeletons to build a SOAP client or SOAP server directly from GPS. Continued work for E923-010.

--- gps/aws.py
+++ gps/aws.py
@@ -12,6 +12,29 @@ def locate_exec_on_path (prog):
         if os.path.isfile(file) or os.path.isfile(file+".exe"):
             return file
 
+class Console_Process (GPS.Console, GPS.Process):
+    out_file = ""
+
+    def on_output (self, unmatched, matched):
+        self.write (unmatched + matched)
+
+    def on_input (self, input):
+        self.send (input)
+
+    def on_exit (self, status, remaining_output):
+	if status == 0 and self.out_file != "":
+	   GPS.Editor.edit (self.out_file, force=True)
+
+    def __init__ (self, ofile, process, args=""):
+	self.out_file = ofile
+        GPS.Console.__init__ (self, process, \
+                              on_input=Console_Process.on_input)
+        GPS.Process.__init__ (self, process + ' ' + args, ".+", \
+			      on_exit=Console_Process.on_exit, \
+                              on_match=Console_Process.on_output)
+
+# For API documentation support
+
 def gnat_root ():
     gnatls = locate_exec_on_path ("gnatls")
     return gnatls[:-10]
@@ -33,3 +56,31 @@ def open_file (filename):
         GPS.EditorBuffer.get(file)
     else:
 	GPS.Console ("Messages").write ("File " + bfile + " not found.")
+
+# For SOAP support
+
+def ada2wsdl (api):
+    toolexe=locate_exec_on_path("ada2wsdl");
+    if toolexe == "":
+        GPS.Console ("Messages").write ("Error: ada2wsdl tool not found.");
+    else:
+        ofile=os.path.splitext(api)[0]+'.wsdl'
+    	idirs=[];
+    	proj=GPS.current_context().project();
+    	for dir in proj.source_dirs():
+            idirs = idirs + ["-I", dir];
+	opts = GPS.Project.get_tool_switches_as_list (proj, "Ada2WSDL") \
+	    + ["-o", ofile] + idirs + [api];
+        fopts=" ".join(opts); fopts=string.replace(fopts, "\\", "/");
+        Console_Process (ofile, toolexe, fopts);
+
+def wsdl2aws (wsdl):
+    toolexe=locate_exec_on_path("wsdl2aws");
+    if toolexe == "":
+        GPS.Console ("Messages").write ("Error: wsdl2aws tool not found.");
+    else:
+        proj=GPS.current_context().project();
+        opts = GPS.Project.get_tool_switches_as_list (proj, "WSDL2AWS") \
+	         + [wsdl];
+        fopts=" ".join(opts);
+    	Console_Process ("", toolexe, fopts);
