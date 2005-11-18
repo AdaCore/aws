d5ef57e25778ae0b9f479df55c7f048addc6c057
Pascal Obry <obry@adacore.com>
2005-11-18 15:00:00 +0000

    Initial revision. For the Ajax demo.

---
+++ demos/web_elements_containers.adb
@@ -0,0 +1,65 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2004                            --
+--                                ACT-Europe                                --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+with AI302.Containers.Indefinite_Vectors;
+
+with AWS.Templates;
+
+package body Web_Elements_Containers is
+
+   use AWS.Templates;
+
+   package String_Vectors is
+     new AI302.Containers.Indefinite_Vectors (Positive, String);
+
+   Groups : String_Vectors.Vector;
+
+   ---------------
+   -- Add_Group --
+   ---------------
+
+   procedure Add_Group (Name : in String) is
+   begin
+      String_Vectors.Append (Groups, Name);
+   end Add_Group;
+
+   ----------------
+   -- Get_Groups --
+   ----------------
+
+   function Get_Groups return AWS.Templates.Vector_Tag is
+      Result : Vector_Tag;
+   begin
+      for K in 1 .. String_Vectors.Length (Groups) loop
+         Result := Result & String_Vectors.Element (Groups, Positive (K));
+      end loop;
+
+      return Result;
+   end Get_Groups;
+
+end Web_Elements_Containers;
