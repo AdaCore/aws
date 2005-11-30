5fc0f91283c2320950274d5f543c05c9517c5d3e
Pascal Obry <obry@adacore.com>
2005-11-30 11:39:24 +0000

    Initial revision. Work for EB29-007.

---
+++ regtests/test_wsdl7.adb
@@ -0,0 +1,47 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                            Copyright (C) 2005                            --
+--                                 AdaCore                                  --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+--  $Id$
+
+--  ~ MAIN [SOAP]
+
+--  SOAP/WSDL test
+
+with Ada.Text_IO;
+
+--  Just make sure that the Ada generated names are consistent accross
+--  all files.
+
+with Ada_Name.Client;
+with Ada_Name.Server;
+with Ada_Name.Types;
+
+procedure Test_WSDL7 is
+begin
+   Ada.Text_IO.Put_Line ("Compiled and run fine");
+end Test_WSDL7;
