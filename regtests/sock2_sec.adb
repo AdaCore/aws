21ad0985502ba24aa9ae71f0ea3fedcef0abfa95
Dmitriy Anisimkov <anisimkov@ada-ru.org>
2006-02-24 14:41:16 +0000

    Update copyright years.

--- regtests/sock2_sec.adb
+++ regtests/sock2_sec.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                         Copyright (C) 2000-2004                          --
+--                         Copyright (C) 2000-2006                          --
 --                                ACT-Europe                                --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
