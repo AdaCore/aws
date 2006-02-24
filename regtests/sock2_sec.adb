d1115ea0a22e37599ba58904c9b0b80b897b7c74
Pascal Obry <obry@adacore.com>
2006-02-24 16:43:16 +0000

    Update copyright notice.

--- regtests/sock2_sec.adb
+++ regtests/sock2_sec.adb
@@ -2,7 +2,7 @@
 --                              Ada Web Server                              --
 --                                                                          --
 --                         Copyright (C) 2000-2006                          --
---                                ACT-Europe                                --
+--                                 AdaCore                                  --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --
