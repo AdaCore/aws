db41ead8bface32af99c32a0c033597870efbc6f
Dmitriy Anisimkov <anisimkov@ada-ru.org>
2006-04-22 09:20:45 +0000

    A bit simplification.

--- workspace/tp.adb
+++ workspace/tp.adb
@@ -1,69 +1,62 @@
+with Ada.Exceptions;
 with Ada.Text_IO;
 with Templates_Parser;
-with Ada.Exceptions;
-with Ada.Command_Line;
 
 procedure TP is
-   use Templates_Parser;
+   Cached : constant Boolean := False;
 
-   T1 : Tag;
-   T2 : Tag;
+   task type Secondary is
+      entry Start;
+   end Secondary;
 
-   procedure Show_Tag (T : Tag);
-   task type Secondary;
+   procedure Parse (P1 : Positive);
 
-   procedure Show_Tag (T : Tag) is
-      use Ada.Text_IO;
+   -----------
+   -- Parse --
+   -----------
+
+   procedure Parse (P1 : Positive) is
+      use Templates_Parser;
+
+      Translations : Translate_Table (1 .. 1);
+      T : Tag;
    begin
-      for J in 1 .. Size (T) loop
-         Put (Item (T, J) & ' ');
-      end loop;
+      T := +P1;
+      Translations (1) := Assoc ("P1", T);
 
-      New_Line;
-   end Show_Tag;
+      declare
+         Result : constant String
+           := Parse ("file.tmplt", Translations, Cached);
+      begin
+         if P1 rem 1000 = 0 then
+            Ada.Text_IO.Put_Line (Result);
+         end if;
+      end;
+   end Parse;
 
    ---------------
    -- Secondary --
    ---------------
 
    task body Secondary is
-      use Ada.Command_Line;
-      use Ada.Text_IO;
-      Translations : Translate_Table (1 .. Argument_Count / 2);
-      T : Tag;
-
-      Len2 : Natural;
-      Len1 : Natural := 0;
    begin
-      for J in Translations'Range loop
-         T := +Argument (J * 2);
-         Translations (J) := Assoc (Argument (J * 2 - 1), T);
-      end loop;
+      accept Start;
 
-      loop
-         declare
-            Result : constant String := Parse ("file.tmplt", Translations, False);
-         begin
-            Len2 := Result'Length;
-
-            --  if Len2 /= Len1 then
-               Put_Line (Result);
-            --  end if;
-
-            Len1 := Len2;
-         end;
+      for K in Positive'Range loop
+         Parse (K);
       end loop;
 
    exception
       when E : others =>
-         Put_Line (Ada.Exceptions.Exception_Information (E));
+         Ada.Text_IO.Put_Line (Ada.Exceptions.Exception_Information (E));
    end Secondary;
 
    TS : array (1 .. 2) of Secondary;
 
 begin
-   T1 := +"One";
-   T2 := T1 & "Two";
-   Show_Tag (T1);
-   Show_Tag (T2);
+   Parse (1);
+
+   for J in TS'Range loop
+      TS (J).Start;
+   end loop;
 end TP;
