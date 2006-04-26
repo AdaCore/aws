58be08e3297aa7bfff407b0bc4b0d8ca072fd9ba
Pascal Obry <obry@adacore.com>
2006-04-26 14:04:05 +0000

    (Version): Set to 2.2.0 to follow the GNU standard numbering X.Y.Z. Work for F424-039.

--- src/aws.ads
+++ src/aws.ads
@@ -30,7 +30,7 @@ package AWS is
 
    pragma Pure;
 
-   Version      : constant String := "2.2a";
+   Version      : constant String := "2.2.0";
 
    HTTP_Version : constant String := "HTTP/1.1";
 
