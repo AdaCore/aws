0c3e0a38fa60ce5980832ea71986a11033184838
Dmitriy Anisimkov <anisimkov@ada-ru.org>
2007-10-23 10:10:42 +0000

    How many sockets could be in the socket set.

---
+++ workspace/max_poll_size.adb
@@ -0,0 +1,20 @@
+with Ada.Text_IO;
+with AWS.Net.Sets;
+
+procedure Max_Poll_Size is
+   use AWS.Net;
+   Set : Sets.Socket_Set_Type;
+   Port : constant Positive := 8088;
+   Server, Client, Peer : Socket_Type'Class := Socket (False);
+begin
+   Bind (Server, Port);
+   Listen (Server);
+   loop
+      Connect (Client, "localhost", Port);
+      Accept_Socket (Server, Peer);
+      Sets.Add (Set, Peer, Sets.Output);
+      Sets.Add (Set, Client, Sets.Input);
+      Sets.Wait (Set, 0.001);
+      Ada.Text_IO.Put_Line (Sets.Count (Set)'Img);
+   end loop;
+end Max_Poll_Size;
