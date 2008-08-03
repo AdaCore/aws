3d68660dfa14fdabfa65194457022f7424f18aa1
Pascal Obry <obry@adacore.com>
2008-08-03 11:28:07 +0000

    Move SockOver_Sec tests to new test framework.
    Work for H627-016.

--- regtests/0018_sockover_sec/sockover_sec.adb
+++ regtests/0018_sockover_sec/sockover_sec.adb
@@ -1,8 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                            Copyright (C) 2006                            --
---                                ACT-Europe                                --
+--                     Copyright (C) 2006-2008, AdaCore                     --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --
@@ -26,9 +25,7 @@
 --  covered by the  GNU Public License.                                     --
 ------------------------------------------------------------------------------
 
---  ~ MAIN [SSL]
-
---  Regression test for output buffer overflow.
+--  Regression test for output buffer overflow
 
 with SockOver_Proc;
 
