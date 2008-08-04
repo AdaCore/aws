690968cae493c4c524fd70a6b58b3bf297db00d3
Pascal Obry <obry@adacore.com>
2008-08-04 17:48:31 +0000

    Move Check_Mem tests to new test framework.
    Work for H627-016.

---
+++ regtests/0043_check_mem/test.py
@@ -0,0 +1,34 @@
+from test_support import *
+
+#  Build driver
+build('check_mem')
+
+#  Run driver (2 loops)
+run('check_mem', ['2'], output_file='check_mem.res1')
+exec_cmd('gnatmem', ['3', '-i', 'gmem.out', './check_mem'],
+         output_file='check_mem.run1')
+
+#  Run driver (30 loops)
+run('check_mem', ['30'], output_file='check_mem.res2')
+exec_cmd('gnatmem', ['3', '-i', 'gmem.out', './check_mem'],
+         output_file='check_mem.run2')
+
+#  Now check that final water-mark for run1 and run2 is equal
+r1 = open ('check_mem.run1').readlines()
+r2 = open ('check_mem.run2').readlines()
+
+fr1 = ""
+fr2 = ""
+
+for item in r1:
+    if item[0:8] == "   Final":
+        fr1 = item;
+for item in r2:
+    if item[0:8] == "   Final":
+        fr2 = item;
+
+if fr1 != fr2:
+    logging.error(fr1 + "!=" + fr2)
+    sys.exit(1)
+else:
+    sys.exit(0)
