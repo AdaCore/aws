fc6da0ecf700a5431c42bc3a218fe1b118b9668d
Pascal Obry <obry@adacore.com>
2008-08-04 17:30:45 +0000

    Move Afile_Sec tests to new test framework.
    Work for H627-016.

--- regtests/0033_afile_sec/afile_sec.adb
+++ regtests/0033_afile_sec/afile_sec.adb
@@ -1,8 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                            Copyright (C) 2004                            --
---                               ACT-Europe                                 --
+--                     Copyright (C) 2004-2008, AdaCore                     --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --
@@ -27,8 +26,6 @@
 ------------------------------------------------------------------------------
 
 --  Test file as attachment
-
---  ~ MAIN [SSL]
 
 with AFile_Pack;
 
