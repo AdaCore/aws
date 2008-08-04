e0c181399c3e6e72f1d3fea45948fad197ef6ca6
Pascal Obry <obry@adacore.com>
2008-08-04 17:49:57 +0000

    Move HLoad tests to new test framework.
    Work for H627-016.

--- regtests/0059_hload/hload_pack.ads
+++ regtests/0059_hload/hload_pack.ads
@@ -1,8 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                            Copyright (C) 2004                            --
---                                ACT-Europe                                --
+--                     Copyright (C) 2004-2008, AdaCore                     --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --
@@ -26,7 +25,7 @@
 --  covered by the  GNU Public License.                                     --
 ------------------------------------------------------------------------------
 
---  Test for heavy loading.
+--  Test for heavy loading
 
 package HLoad_Pack is
 
