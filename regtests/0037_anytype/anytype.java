100d1b04937eccf1c2a20ef730761e15cc7042de
Pascal Obry <obry@adacore.com>
2008-08-04 17:42:43 +0000

    Move AnyType tests to new test framework.
    Work for H627-016.

--- regtests/0037_anytype/anytype.java
+++ regtests/0037_anytype/anytype.java
@@ -1,32 +1,29 @@
 //--------------------------------------------------------------------------
 //                              Ada Web Server
 //
-//                         Copyright (C) 2004-2005
-//                                 AdaCore
-//                                                                          
-//  This library is free software; you can redistribute it and/or modify    
-//  it under the terms of the GNU General Public License as published by    
-//  the Free Software Foundation; either version 2 of the License, or (at   
-//  your option) any later version.                                         
+//                     Copyright (C) 2004-2008, AdaCore
 //
-//  This library is distributed in the hope that it will be useful, but     
-//  WITHOUT ANY WARRANTY; without even the implied warranty of              
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       
-//  General Public License for more details.                                
+//  This library is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or (at
+//  your option) any later version.
 //
-//  You should have received a copy of the GNU General Public License       
-//  along with this library; if not, write to the Free Software Foundation, 
-//  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          
+//  This library is distributed in the hope that it will be useful, but
+//  WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  General Public License for more details.
 //
-//  As a special exception, if other files instantiate generics from this   
-//  unit, or you link this unit with other files to produce an executable,  
-//  this  unit  does not  by itself cause  the resulting executable to be   
-//  covered by the GNU General Public License. This exception does not      
-//  however invalidate any other reasons why the executable file  might be  
-//  covered by the  GNU Public License.                                     
+//  You should have received a copy of the GNU General Public License
+//  along with this library; if not, write to the Free Software Foundation,
+//  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+//  As a special exception, if other files instantiate generics from this
+//  unit, or you link this unit with other files to produce an executable,
+//  this  unit  does not  by itself cause  the resulting executable to be
+//  covered by the GNU General Public License. This exception does not
+//  however invalidate any other reasons why the executable file  might be
+//  covered by the  GNU Public License.
 //--------------------------------------------------------------------------
-//
-// $Id$
 //
 // This program can be used to test the AWS SOAP/WSDL implementation using the
 // Axis application server based on Tomcat.
@@ -67,10 +64,10 @@ public class anytype {
 
         // Make a service
         Anytype_Service service = new Anytype_ServiceLocator();
- 
+
         // Now use the service to get a stub which implements the SDI.
         Anytype_PortType port = service.getanytype_Port();
- 
+
         // Make the actual call
         // System.out.println (port.plus (12));
 
