ed898757a34a8f08b3afd6a9d17c7010920915b1
Pascal Obry <obry@adacore.com>
2008-08-05 11:18:14 +0000

    Move TRes tests to new test framework.
    Work for H627-016.

---
+++ regtests/0094_tres/test.py
@@ -0,0 +1,60 @@
+from test_support import *
+
+#  Create the resources
+
+f=open('file1.html', 'w')
+f.write('line 1, file 1\n')
+f.write('line 2, file 1\n')
+f.close()
+
+f=open('file2.html', 'w')
+f.write('line 1, file 2\n')
+f.write('line 2, file 2\n')
+f.write('line 3, file 2\n')
+f.close()
+
+f=open('file3.html', 'w')
+f.write('line 1, file 3\n')
+f.write('line 2, file 3\n')
+f.close()
+
+f=open('file.tmplt', 'w')
+f.write('line 1 : @_TAG1_@\n')
+f.write('line 2 : @_TAG2_@\n')
+f.write('@@TABLE@@\n')
+f.write('line 3.1 : @_TAG_V_@\n')
+f.write('@@SECTION@@\n')
+f.write('line 3.2 : @_TAG_V_@\n')
+f.write('@@END_TABLE@@\n')
+f.write('@@IF@@ @_COND_@\n')
+f.write('   ok\n')
+f.write('@@ELSE@@\n')
+f.write('   nok\n')
+f.write('@@END_IF@@\n')
+f.close()
+
+exec_cmd('awsres',
+         ['-q', '-r', 'tresres', 'file1.html',
+          'file2.html', 'file.tmplt'])
+
+build('tres');
+
+run('tres', output_file='tres.run1')
+diff('test.out1', 'tres.run1')
+
+#  Change file on disk
+
+f=open('file1.html', 'a')
+f.write('line 3, file 1\n')
+f.close()
+
+f=open('file2.html', 'a')
+f.write('line 4, file 2\n')
+f.close()
+
+f=open('file3.html', 'a')
+f.write('line 3, file 3\n')
+f.close()
+
+run('tres', output_file='tres.run2')
+diff('test.out2', 'tres.run2')
--- regtests/0094_tres/tres.adb
+++ regtests/0094_tres/tres.adb
@@ -1,8 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                         Copyright (C) 2000-2004                          --
---                                ACT-Europe                                --
+--                     Copyright (C) 2000-2008, AdaCore                     --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --
@@ -26,9 +25,7 @@
 --  covered by the  GNU Public License.                                     --
 ------------------------------------------------------------------------------
 
---  ~ MAIN [STD]
-
---  Test for the embedded resource files.
+--  Test for the embedded resource files
 
 with Ada.Command_Line;
 with Ada.Text_IO;
