0060479386ef9e07ebb3acb54a44371bf2f11198
Pascal Obry <obry@adacore.com>
2008-08-06 17:29:31 +0000

    Move SPG_Sec tests to new test framework.
    Work for H627-016.

--- regtests/0109_spg_sec/spg_sec.adb
+++ regtests/0109_spg_sec/spg_sec.adb
@@ -1,8 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                            Copyright (C) 2006                            --
---                                ACT-Europe                                --
+--                     Copyright (C) 2006-2008, AdaCore                     --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --
@@ -26,9 +25,7 @@
 --  covered by the  GNU Public License.                                     --
 ------------------------------------------------------------------------------
 
---  ~ MAIN [SSL]
-
---  Server push regression test.
+--  Server push regression test
 
 with Get_Free_Port;
 with Sp_Pack;
