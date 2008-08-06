92b38cb25ad76be211eb82343d54176de1ba551c
Pascal Obry <obry@adacore.com>
2008-08-06 17:31:51 +0000

    Move Test_WSDL6 tests to new test framework.
    Work for H627-016.

--- regtests/0137_test_wsdl6/test_wsdl6.adb
+++ regtests/0137_test_wsdl6/test_wsdl6.adb
@@ -1,8 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                         Copyright (C) 2005-2008                          --
---                                 AdaCore                                  --
+--                     Copyright (C) 2005-2008, AdaCore                     --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --
@@ -25,8 +24,6 @@
 --  however invalidate any other reasons why the executable file  might be  --
 --  covered by the  GNU Public License.                                     --
 ------------------------------------------------------------------------------
-
---  ~ MAIN [XMLADA]
 
 with Ada.Text_IO;
 
