3beec397aec71671479743ff11945631b6cdbf11
Olivier Ramonat <ramonat@adacore.com>
2008-08-20 13:20:36 +0000

    Be sure that SHUT_RD and SHUT_WR are defined on Windows

--- config/src/check_config.c
+++ config/src/check_config.c
@@ -106,7 +106,17 @@ main (int argc, char *argv[])
 #endif
 
 #ifdef _WIN32
-  #define SHUT_RDWR   SD_BOTH
+
+  #if !defined SHUT_RD && defined SD_RECEIVE
+  #define SHUT_RD SD_RECEIVE
+  #endif
+  #if !defined SHUT_WR && defined SD_SEND
+  #define SHUT_WR SD_SEND
+  #endif
+  #if !defined SHUT_RDWR && defined SD_BOTH
+  #define SHUT_RDWR SD_BOTH
+  #endif
+
   #define ETIMEDOUT   WSAETIMEDOUT
   #define ENOTCONN    WSAENOTCONN
   #define EWOULDBLOCK WSAEWOULDBLOCK
