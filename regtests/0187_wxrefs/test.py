d18af6c103fab43bf30e0d049929cd4ca83cb5a1
Dmitriy Anisimkov <anisimkov@ada-ru.org>
2008-08-27 15:16:17 +0000

    Set file list directly because GNAT extend wx_* without order.

--- regtests/0187_wxrefs/test.py
+++ regtests/0187_wxrefs/test.py
@@ -1,18 +1,19 @@
 from test_support import *
 
-exec_cmd('webxref', ['wx_*'], output_file='wxrefs.res1')
+files=['wx_block.thtml', 'wx_page.html', 'wx_reply.txml', 'wx_style.css']
+exec_cmd('webxref', files, output_file='wxrefs.res1')
 diff('wxrefs.out1', 'wxrefs.res1')
 
-exec_cmd('webxref', ['-u', 'wx_*'], output_file='wxrefs.res2')
+exec_cmd('webxref', ['-u'] + files, output_file='wxrefs.res2')
 diff('wxrefs.out2', 'wxrefs.res2')
 
-exec_cmd('webxref', ['-d', 'wx_*'], output_file='wxrefs.res3')
+exec_cmd('webxref', ['-d'] + files, output_file='wxrefs.res3')
 diff('wxrefs.out3', 'wxrefs.res3')
 
-exec_cmd('webxref', ['-I', '-C', '-pi', 'id', 'wx_*'],
+exec_cmd('webxref', ['-I', '-C', '-pi', 'id'] + files,
          output_file='wxrefs.res4', ignore_error=True)
 diff('wxrefs.out4', 'wxrefs.res4')
 
-exec_cmd('webxref', ['-I', '-C', '-pi', 'file_based', 'wx_*'],
+exec_cmd('webxref', ['-I', '-C', '-pi', 'file_based'] + files,
          output_file='wxrefs.res5', ignore_error=True)
 diff('wxrefs.out5', 'wxrefs.res5')
