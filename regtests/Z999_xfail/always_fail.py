30bc7cb184aa82b208236842ecd877024ae1e006
Olivier Ramonat <ramonat@adacore.com>
2008-12-04 10:43:17 +0000

    Generate a diff for Z999_xfail test

--- regtests/Z999_xfail/always_fail.py
+++ regtests/Z999_xfail/always_fail.py
@@ -1,2 +1,11 @@
 import sys
+import time
+
+from test_support import *
+
+output = open('test.res', 'w')
+output.write(time.strftime("%y-%m-%d %H:%M", time.localtime()))
+output.close()
+diff()
+
 sys.exit(1)
