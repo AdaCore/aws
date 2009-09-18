c77e34673a0bcda37f9528a56364f1f849e98f63
Dmitriy Anisimkov <anisimkov@ada-ru.org>
2009-09-18 07:01:14 +0000

    Change copyright notice.

--- config/src/test_uconv.adb
+++ config/src/test_uconv.adb
@@ -1,8 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                         Copyright (C) 2000-2001                          --
---                                ACT-Europe                                --
+--                     Copyright (C) 2000-2009, AdaCore                     --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --
