720d19c724b6210f4e961fffde7602aa48984528
Pascal Obry <obry@adacore.com>
2009-12-29 21:59:50 +0000

    Remove obsolescent initialization code not needed anymore.
    Code clean-up in socket support.

--- config/src/aws-net-std__gnat.adb
+++ config/src/aws-net-std__gnat.adb
@@ -712,11 +712,4 @@ package body AWS.Net.Std is
       end if;
    end Shutdown;
 
-begin
-   pragma Warnings (Off);
-   --  The call to Sockets.Initialize has been obsoleted. We keep this call
-   --  for compatibility with older compilers.
-   --  ??? This call should be removed when GNAT version 6.2 and GPL 2009
-   --  will  be out.
-   Sockets.Initialize;
 end AWS.Net.Std;
