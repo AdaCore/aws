fefdb37ff79a6d6fb6b3a6e118ac554d4c574499
Dmitriy Anisimkov <anisimkov@ada-ru.org>
2010-01-13 03:59:40 +0000

    Post only numeric in AWS for now.

--- config/setup/check_config.c
+++ config/setup/check_config.c
@@ -1,29 +1,29 @@
-/***************************************************************************/
-/*                             Ada Web Server                              */
-/*                                                                         */
-/*                    Copyright (C) 2000-2010, AdaCore                     */
-/*                                                                         */
-/* This library is free software; you can redistribute it and/or modify    */
-/* it under the terms of the GNU General Public License as published by    */
-/* the Free Software Foundation; either version 2 of the License, or (at   */
-/* your option) any later version.                                         */
-/*                                                                         */
-/* This library is distributed in the hope that it will be useful, but     */
-/* WITHOUT ANY WARRANTY; without even the implied warranty of              */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       */
-/* General Public License for more details.                                */
-/*                                                                         */
-/* You should have received a copy of the GNU General Public License       */
-/* along with this library; if not, write to the Free Software Foundation, */
-/* Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          */
-/*                                                                         */
-/* As a special exception, if other files instantiate generics from this   */
-/* unit, or you link this unit with other files to produce an executable,  */
-/* this  unit  does not  by itself cause  the resulting executable to be   */
-/* covered by the GNU General Public License. This exception does not      */
-/* however invalidate any other reasons why the executable file  might be  */
-/* covered by the  GNU Public License.                                     */
-/***************************************************************************/
+/****************************************************************************/
+/*                              Ada Web Server                              */
+/*
+--                     Copyright (C) 2000-2010, AdaCore                     --
+--                                                                          */
+/*  This library is free software; you can redistribute it and/or modify    */
+/*  it under the terms of the GNU General Public License as published by    */
+/*  the Free Software Foundation; either version 2 of the License, or (at   */
+/*  your option) any later version.                                         */
+/*                                                                          */
+/*  This library is distributed in the hope that it will be useful, but     */
+/*  WITHOUT ANY WARRANTY; without even the implied warranty of              */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       */
+/*  General Public License for more details.                                */
+/*                                                                          */
+/*  You should have received a copy of the GNU General Public License       */
+/*  along with this library; if not, write to the Free Software Foundation, */
+/*  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          */
+/*                                                                          */
+/*  As a special exception, if other files instantiate generics from this   */
+/*  unit, or you link this unit with other files to produce an executable,  */
+/*  this  unit  does not  by itself cause  the resulting executable to be   */
+/*  covered by the GNU General Public License. This exception does not      */
+/*  however invalidate any other reasons why the executable file  might be  */
+/*  covered by the  GNU Public License.                                     */
+/****************************************************************************/
 
 /* This program is used to get some OS specific settings */
 
@@ -275,6 +275,7 @@ main (int argc, char *argv[])
   P ("   AI_PASSIVE     : constant := %d;\n", AI_PASSIVE);
   P ("   AI_CANONNAME   : constant := %d;\n", AI_CANONNAME);
   P ("   AI_NUMERICHOST : constant := %d;\n", AI_NUMERICHOST);
+  P ("   AI_NUMERICSERV : constant := %d;\n", AI_NUMERICSERV);
   P ("   EAI_SYSTEM     : constant := %d;\n\n", EAI_SYSTEM);
 
   /* getaddrname flags */
--- config/src/aws-net-std__ipv6.adb
+++ config/src/aws-net-std__ipv6.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                     Copyright (C) 2004-2009, AdaCore                     --
+--                     Copyright (C) 2004-2010, AdaCore                     --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --
@@ -356,6 +356,7 @@ package body AWS.Net.Std is
    is
       package CS renames Interfaces.C.Strings;
       use type C.int;
+      use type C.unsigned;
       use type OS_Lib.Addr_Info_Access;
 
       C_Node : aliased C.char_array := C.To_C (Host);
@@ -367,7 +368,8 @@ package body AWS.Net.Std is
                  (ai_family    => OS_Lib.PF_UNSPEC,
                   ai_socktype  => OS_Lib.SOCK_STREAM,
                   ai_protocol  => OS_Lib.IPPROTO_IP,
-                  ai_flags     => Flags,
+                  ai_flags     =>
+                    C.int (C.unsigned (Flags) or OS_Lib.AI_NUMERICSERV),
                   ai_addrlen   => 0,
                   ai_canonname => CS.Null_Ptr,
                   ai_addr      => System.Null_Address,
