dfb337bf5ca17fb2b9cc95be0ed58ce3619ac09a
Pascal Obry <obry@adacore.com>
2010-04-01 17:04:30 +0000

    Allow more delay for the timeout check.
    These tests are unreliable where run under heavy loaded
    machines.

--- regtests/0129_test_soap_timeout/test_soap_timeout.adb
+++ regtests/0129_test_soap_timeout/test_soap_timeout.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                     Copyright (C) 2008-2009, AdaCore                     --
+--                     Copyright (C) 2008-2010, AdaCore                     --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --
@@ -59,7 +59,7 @@ procedure Test_SOAP_Timeout is
       use type Calendar.Time;
       Elaps : constant Duration := Stop - Start - D;
    begin
-      if abs (Elaps) < 0.4 then
+      if abs (Elaps) < 1.0 then
          Text_IO.Put_Line ("OK:" & Message);
       else
          Text_IO.Put_Line ("NOK:" & Message & ", " & Duration'Image (Elaps));
