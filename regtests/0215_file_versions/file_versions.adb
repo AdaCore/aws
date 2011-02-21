AdaCore/aws

commit 546d2fa8a73804fd6e59196645edc406e8359ad7

Do not ressort on exception being raised for open files.
Rework the way files are opened by checking which versions
are available (plain, gzip). The code is cleaner and avoid
some allocations/deallocations.
--
Preparating work for K219-001.

src/core/aws-resources-files.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                     Copyright (C) 2002-2009, AdaCore                     --
+--                     Copyright (C) 2002-2011, AdaCore                     --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --
@@ -27,7 +27,6 @@
 
 with Ada.Directories;
 with Ada.IO_Exceptions;
-with Ada.Unchecked_Deallocation;
 
 with AWS.Resources.Streams.Disk;
 with AWS.Resources.Streams.ZLib;
@@ -120,58 +119,64 @@ package body AWS.Resources.Files is
    is
       use type AWS.Resources.Streams.Stream_Access;
 
-      Stream : AWS.Resources.Streams.Stream_Access;
+      File_Kind : constant Resources.File_Instance := Resources.Exist (Name);
+      Stream    : AWS.Resources.Streams.Stream_Access;
 
-      procedure Open_File (Name : String; Last : Boolean);
+      procedure Open_File (Name : String);
 
       ---------------
       -- Open_File --
       ---------------
 
-      procedure Open_File (Name : String; Last : Boolean) is
-         procedure Free is
-           new Ada.Unchecked_Deallocation
-             (Streams.Stream_Type'Class, Streams.Stream_Access);
+      procedure Open_File (Name : String) is
       begin
          Stream := new AWS.Resources.Streams.Disk.Stream_Type;
 
          AWS.Resources.Streams.Disk.Open
            (AWS.Resources.Streams.Disk.Stream_Type (Stream.all), Name, Form);
-      exception
-         when Ada.IO_Exceptions.Name_Error =>
-            Free (Stream);
-
-            if Last then
-               raise;
-            end if;
       end Open_File;
 
    begin
       if Is_GZip (Name) then
          --  Don't try to open file Name & ".gz.gz"
 
-         GZip := False;
+         case File_Kind is
+            when Both | Resources.GZip =>
+               GZip := False;
+               Open_File (Name);
 
-         Open_File (Name, True);
+            when Plain | None =>
+               raise IO_Exceptions.Name_Error;
+         end case;
 
       elsif GZip then
-         Open_File (Name & ".gz", False);
 
-         if Stream = null then
-            Open_File (Name, True);
+         case File_Kind is
+            when Both | Resources.GZip =>
+               Open_File (Name & ".gz");
 
-            GZip := False;
-         end if;
+            when Plain =>
+               Open_File (Name);
+               GZip := False;
+
+            when None =>
+               raise IO_Exceptions.Name_Error;
+         end case;
 
       else
-         Open_File (Name, False);
 
-         if Stream = null then
-            Open_File (Name & ".gz", True);
+         case File_Kind is
+            when Both | Plain =>
+               Open_File (Name);
 
-            Stream := Streams.ZLib.Inflate_Create
-                        (Stream, Header => ZLib.GZip);
-         end if;
+            when Resources.GZip =>
+               Open_File (Name & ".gz");
+               Stream := Streams.ZLib.Inflate_Create
+                 (Stream, Header => ZLib.GZip);
+
+            when None =>
+               raise IO_Exceptions.Name_Error;
+         end case;
       end if;
 
       Streams.Create (File, Stream);

commit d148a5c5b985fbf8cb78aa551596c1af2ff8a4ba

Fix how file existence is checked.
The Resources.Files.Exist and Resources.Embedded.Exist routines
were not properly covering all cases.

This fix a regression introduced in 546d2fa.
Add a corresponding regression test.
--
Continued work for K219-001.

MANIFEST
@@ -1539,6 +1539,15 @@ regtests/0214_stream_not_found/stream_not_found.adb
 regtests/0214_stream_not_found/stream_not_found.gpr
 regtests/0214_stream_not_found/test.out
 regtests/0214_stream_not_found/test.py
+regtests/0215_file_versions/data2.txt
+regtests/0215_file_versions/data3.txt
+regtests/0215_file_versions/data3.txt.gz
+regtests/0215_file_versions/data4.txt.gz
+regtests/0215_file_versions/file_versions.adb
+regtests/0215_file_versions/file_versions.gpr
+regtests/0215_file_versions/hello.txt.gz
+regtests/0215_file_versions/test.out
+regtests/0215_file_versions/test.py
 regtests/README
 regtests/aws.gpr
 regtests/common/get_free_port.adb

regtests/0215_file_versions/file_versions.adb
@@ -1,0 +1,94 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                       Copyright (C) 2011, AdaCore                        --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+with Ada.Streams;
+with Ada.Text_IO;
+
+with AWS.Client;
+with AWS.MIME;
+with AWS.Resources;
+with AWS.Response;
+with AWS.Server;
+with AWS.Status;
+with AWS.Translator;
+with AWS.Utils;
+
+with ZLib;
+
+with Get_Free_Port;
+
+procedure File_Versions is
+
+   use Ada;
+   use Ada.Streams;
+   use AWS;
+
+   WS : Server.HTTP;
+
+   function CB (Request : Status.Data) return Response.Data is
+   begin
+      return Response.File (MIME.Text_HTML, "hello.txt.gz");
+   end CB;
+
+   procedure Exist (Filename : String) is
+   begin
+      Text_IO.Put ("> " & Filename);
+      Text_IO.Set_Col (20);
+      Text_IO.Put_Line
+        (Resources.File_Instance'Image (Resources.Exist (Filename)));
+   end Exist;
+
+   Port : Natural := 1234;
+   R    : Response.Data;
+
+begin
+   Get_Free_Port (Port);
+
+   Server.Start
+     (WS, "file_versions",
+      CB'Unrestricted_Access, Port => Port, Max_Connection => 1);
+   Text_IO.Put_Line ("started"); Ada.Text_IO.Flush;
+
+   R := Client.Get ("http://localhost:" & Utils.Image (Port));
+
+   Text_IO.Put_Line
+     ("R : "
+      & Translator.To_String
+        (Translator.Decompress (Response.Message_Body (R), ZLib.GZip).all));
+
+   Server.Shutdown (WS);
+   Text_IO.Put_Line ("shutdown");
+
+   Exist ("data1.txt");
+   Exist ("data1.txt.gz");
+   Exist ("data2.txt");
+   Exist ("data2.txt.gz");
+   Exist ("data3.txt");
+   Exist ("data3.txt.gz");
+   Exist ("data4.txt");
+   Exist ("data4.txt.gz");
+end File_Versions;

regtests/0215_file_versions/file_versions.gpr
@@ -1,0 +1,33 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                       Copyright (C) 2011, AdaCore                        --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
+with "aws";
+
+project File_Versions is
+   for Source_Dirs use (".", "../common");
+   for Main use ("file_versions.adb");
+end File_Versions;

regtests/0215_file_versions/test.out
@@ -1,0 +1,12 @@
+started
+R : Hello World
+
+shutdown
+> data1.txt        NONE
+> data1.txt.gz     NONE
+> data2.txt        PLAIN
+> data2.txt.gz     PLAIN
+> data3.txt        BOTH
+> data3.txt.gz     BOTH
+> data4.txt        GZIP
+> data4.txt.gz     GZIP

regtests/0215_file_versions/test.py
@@ -1,0 +1,3 @@
+from test_support import *
+
+build_and_run('file_versions');

src/core/aws-resources-embedded.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                     Copyright (C) 2002-2009, AdaCore                     --
+--                     Copyright (C) 2002-2011, AdaCore                     --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --
@@ -83,19 +83,24 @@ package body AWS.Resources.Embedded is
    -----------
 
    function Exist (Name : String) return File_Instance is
+      VP, VG : Boolean := False;
    begin
-      if not Is_GZip (Name)
-        and then Res_Files.Contains (Files_Table, Name & GZip_Ext)
-      then
-         if Res_Files.Contains (Files_Table, Name) then
-            return Both;
-         else
-            return GZip;
-         end if;
+      if Is_GZip (Name) then
+         VG := Res_Files.Contains (Files_Table, Name);
+         VP := Res_Files.Contains
+           (Files_Table,
+            (Name (Name'First .. Name'Last - GZip_Ext'Length)));
+      else
+         VP := Res_Files.Contains (Files_Table, Name);
+         VG := Res_Files.Contains (Files_Table, Name & GZip_Ext);
+      end if;
 
-      elsif Res_Files.Contains (Files_Table, Name) then
+      if VG and then VP then
+         return Both;
+      elsif VG then
+         return GZip;
+      elsif VP then
          return Plain;
-
       else
          return None;
       end if;

src/core/aws-resources-files.adb
@@ -43,16 +43,23 @@ package body AWS.Resources.Files is
    -----------
 
    function Exist (Name : String) return File_Instance is
+      VP, VG : Boolean := False;
    begin
-      if not Is_GZip (Name)
-        and then Utils.Is_Regular_File (Name & GZip_Ext)
-      then
-         if Utils.Is_Regular_File (Name) then
-            return Both;
-         else
-            return GZip;
-         end if;
-      elsif Utils.Is_Regular_File (Name) then
+      if Is_GZip (Name) then
+         VG := Utils.Is_Regular_File (Name);
+         VP := Utils.Is_Regular_File
+           (Name (Name'First .. Name'Last - GZip_Ext'Length));
+
+      else
+         VP := Utils.Is_Regular_File (Name);
+         VG := Utils.Is_Regular_File (Name & GZip_Ext);
+      end if;
+
+      if VG and then VP then
+         return Both;
+      elsif VG then
+         return GZip;
+      elsif VP then
          return Plain;
       else
          return None;