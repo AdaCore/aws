dee65e73136701c52f5d24751ce00031d0a1677f
Pascal Obry <obry@adacore.com>
2011-03-02 17:03:05 +0100

    Update 0066_upload3 test to pass on VxWorks.
    Part of J817-008.

--- regtests/0066_upload3/upload3.adb
+++ regtests/0066_upload3/upload3.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                     Copyright (C) 2003-2010, AdaCore                     --
+--                     Copyright (C) 2003-2011, AdaCore                     --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --
@@ -187,7 +187,7 @@ begin
    Server.Started;
 
    Request
-     ("http://localhost:" & Utils.Image (Port) & "/upload", "test.out");
+     ("http://localhost:" & Utils.Image (Port) & "/upload", "file2.txt");
 
    Server.Stopped;
 
