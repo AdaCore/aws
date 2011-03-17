5a29eff0f433eab4424c14492a8d9135530bcdff
Pascal Obry <obry@adacore.com>
2011-03-17 21:30:59 +0100

    Update 0078_partial test to pass on VxWorks.
    Part of J817-008.

--- regtests/0078_partial/partial.adb
+++ regtests/0078_partial/partial.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                     Copyright (C) 2005-2010, AdaCore                     --
+--                     Copyright (C) 2005-2011, AdaCore                     --
 --                                                                          --
 --  This library is free software; you can redistribute it and/or modify    --
 --  it under the terms of the GNU General Public License as published by    --
@@ -51,7 +51,7 @@ procedure Partial is
    use AWS;
    use GNAT;
 
-   Filename : constant String := "partial.adb";
+   Filename : constant String := "partial.txt";
 
    function CB (Request : Status.Data) return Response.Data;
 
