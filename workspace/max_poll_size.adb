fe67ca8270f15706aad645d50ae08cb26f176862
Dmitriy Anisimkov <anisimkov@ada-ru.org>
2012-03-18 19:02:15 +0700

    Add timeout test

--- workspace/max_poll_size.adb
+++ workspace/max_poll_size.adb
@@ -27,10 +27,13 @@
 --  covered by the  GNU Public License.                                     --
 ------------------------------------------------------------------------------
 
+with Ada.Real_Time;
 with Ada.Exceptions;
 with Ada.Streams;
 with Ada.Text_IO;
 with AWS.Net.Sets;
+with AWS.Net.Std;
+with AWS.OS_Lib;
 
 procedure Max_Poll_Size is
    use AWS.Net;
@@ -45,9 +48,108 @@ procedure Max_Poll_Size is
    Data                 : constant Stream_Element_Array :=
      (1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19, 20, 21, 22);
 
+   Set_Size : constant := 4096;
+   type FD_Set_Type is array (0 .. Set_Size - 1) of Boolean;
+   pragma Pack (FD_Set_Type);
+   for FD_Set_Type'Size use Set_Size;
+
+   FDS : FD_Set_Type;
+
+   task Reader_Task is
+      entry Read (Socket : Socket_Access);
+      entry Ready;
+      entry Was_Timeout (TO : out Duration);
+   end Reader_Task;
+
+   procedure Print_FDS is
+      Count : Positive := 1;
+      Last  : Boolean  := FDS (FDS'First);
+
+      procedure Flush is
+         Img : constant array (Boolean) of Character :=
+           (False => '-', True => '#');
+      begin
+         if Count > 1 then
+            Put (Count'Img);
+         end if;
+
+         Put (Img (Last));
+      end Flush;
+
+   begin
+      for J in FDS'First + 1 .. FDS'Last loop
+         if FDS (J) = Last then
+            Count := Count + 1;
+         else
+            Flush;
+            Count := 1;
+            Last := FDS (J);
+         end if;
+      end loop;
+
+      Flush;
+
+      New_Line;
+   end Print_FDS;
+
+   procedure Read_Data (Idx : Sets.Socket_Index; Mark : String) is
+   begin
+      if Sets.Get_Socket (Set, Idx).Receive /= Data then
+         Put_Line ("Wrong data " & Mark);
+      end if;
+   end Read_Data;
+
+   -----------------
+   -- Reader_Task --
+   -----------------
+
+   task body Reader_Task is
+      Socket : Socket_Access;
+   begin
+      loop
+         accept Read (Socket : Socket_Access) do
+            Reader_Task.Socket := Read.Socket;
+         end Read;
+
+         declare
+            use Ada.Real_Time;
+            Stamp  : constant Time := Clock;
+            Span   : Duration;
+         begin
+            if Socket.Receive /= Data then
+               Put_Line ("Wrond data on timeout test");
+            end if;
+
+            accept Ready;
+         exception
+            when E : Socket_Error =>
+               if Is_Timeout (E) then
+                  Span := To_Duration (Clock - Stamp);
+
+                  accept Was_Timeout (TO : out Duration) do
+                     TO := Span;
+                  end Was_Timeout;
+               else
+                  raise;
+               end if;
+         end;
+      end loop;
+   exception
+      when E : others =>
+         Ada.Text_IO.Put_Line (Ada.Exceptions.Exception_Information (E));
+   end Reader_Task;
+
 begin
    Server.Bind (0);
    Server.Listen;
+
+   AWS.OS_Lib.FD_ZERO (FDS'Address);
+
+   Print_FDS;
+
+   for J in FDS'Range loop
+      FDS (J) := False;
+   end loop;
 
    loop
       begin
@@ -68,6 +170,58 @@ begin
 
    Put_Line (Sets.Count (Set)'Img);
 
+   for J in reverse 1 .. Sets.Count (Set) loop
+      declare
+         use Ada.Real_Time;
+         Timeout  : constant Duration := 1.0;
+         TO       : Duration;
+         Peer_Idx : constant Sets.Socket_Index := J - 1 + J rem 2 * 2;
+         Socket   : aliased Socket_Type'Class := Sets.Get_Socket (Set, J);
+      begin
+         for K in 1 .. Sets.Count (Set) loop
+            --  Write into all sockets except the peer one
+            if K /= Peer_Idx then
+               Sets.Get_Socket (Set, K).Send (Data);
+            end if;
+         end loop;
+
+         Socket.Set_Timeout (Timeout);
+
+         Reader_Task.Read (Socket'Unchecked_Access);
+
+         select
+            Reader_Task.Was_Timeout (TO);
+            Put_Line ("Too short timeout" & J'Img & TO'Img);
+         or delay Timeout / 4;
+            Sets.Get_Socket (Set, Peer_Idx).Send (Data);
+            Put_Line ("OK" & J'Img);
+
+            select
+               Reader_Task.Ready;
+            or delay Timeout;
+               Put_Line ("Unexpected delay");
+            end select;
+         end select;
+
+         for K in 1 .. Sets.Count (Set) loop
+            --  Read from all sockets except the tested
+            if K /= J then
+               Read_Data (K, "3");
+            end if;
+         end loop;
+
+      exception
+         when E : others =>
+            Put_Line (Ada.Exceptions.Exception_Information (E));
+            abort Reader_Task;
+            exit;
+      end;
+   end loop;
+
+   abort Reader_Task;
+
+   Put_Line ("Timeout test complete");
+
    for J in 1 .. Sets.Count (Set) loop
       Sets.Get_Socket (Set, J - 1 + J rem 2 * 2).Send (Data);
 
@@ -78,15 +232,13 @@ begin
       end if;
 
       if Sets.Is_Read_Ready (Set, J) then
-         if Sets.Get_Socket (Set, J).Receive /= Data then
-            Put_Line ("Wrong data");
-         end if;
+         Read_Data (J, "1");
       else
          Put_Line ("Wrong ready state");
       end if;
    end loop;
 
-   Put_Line ("One socket active test done");
+   Put_Line ("One socket active test complete");
 
    for J in 1 .. Sets.Count (Set) / 2 loop
       Sets.Get_Socket (Set, J * 2).Send (Data);
@@ -103,10 +255,10 @@ begin
          Put_Line ("Wrong Ready state");
       end if;
 
-      if J rem 2 = 1 and then Sets.Get_Socket (Set, J).Receive /= Data then
-         Put_Line ("Wrong Data");
+      if J rem 2 = 1 then
+         Read_Data (J, "2");
       end if;
    end loop;
 
-   Put_Line ("Half sockets active test done");
+   Put_Line ("Half sockets active test complete");
 end Max_Poll_Size;
