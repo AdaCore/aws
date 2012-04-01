85029b9521746e890ba6dfa6cfd9f76c5ec5446c
Pascal Obry <obry@adacore.com>
2012-04-01 16:17:23 +0200

    Fix posix implementation of poll.
    The previous implementation was wrongly expecting that the fd of
    value n would be stored into the nth bucket of the fd set. The fd of
    value one is in fact stored into the lower bit of the first word (more
    precisely the first fd_mask) of the set.
    
    Fixes L329-055.

---
+++ src/poll/os_support.c
@@ -0,0 +1,56 @@
+/***************************************************************************
+ *                             Ada Web Server                              *
+ *                                                                         *
+ *                    Copyright (C) 2009-2012, AdaCore                     *
+ *                                                                         *
+ * This library is free software;  you can redistribute it and/or modify   *
+ * it under terms of the  GNU General Public License  as published by the  *
+ * Free Software  Foundation;  either version 3,  or (at your  option) any *
+ * later version. This library is distributed in the hope that it will be  *
+ * useful, but WITHOUT ANY WARRANTY;  without even the implied warranty of *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                    *
+ *                                                                         *
+ * As a special exception under Section 7 of GPL version 3, you are        *
+ * granted additional permissions described in the GCC Runtime Library     *
+ * Exception, version 3.1, as published by the Free Software Foundation.   *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License and   *
+ * a copy of the GCC Runtime Library Exception along with this program;    *
+ * see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see   *
+ * <http://www.gnu.org/licenses/>.                                         *
+ *                                                                         *
+ * As a special exception, if other files instantiate generics from this   *
+ * unit, or you link this unit with other files to produce an executable,  *
+ * this  unit  does not  by itself cause  the resulting executable to be   *
+ * covered by the GNU General Public License. This exception does not      *
+ * however invalidate any other reasons why the executable file  might be  *
+ * covered by the  GNU Public License.                                     *
+ ***************************************************************************/
+
+/* Wrapper around some C macros */
+
+#if defined(__vxworks)
+#include <vxWorks.h>
+#else
+#include <sys/select.h>
+#endif
+
+#define SOCKET int
+
+int
+__aws_is_socket_in_set (SOCKET socket, fd_set *set)
+{
+  return FD_ISSET (socket, set);
+}
+
+void
+__aws_set_socket_in_set (SOCKET socket, fd_set *set)
+{
+  FD_SET (socket, set);
+}
+
+void
+__aws_clear_socket_set (fd_set *set)
+{
+  FD_ZERO (set);
+}
