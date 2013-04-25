3b88288a7ab6df0639033e6a5c41f90ac5a0797e
Pascal Obry <obry@adacore.com>
2013-04-25 11:53:22 +0200

    Relax a bit the timeout constraint.
    This should help having the tests pass fine and more regularly.

--- regtests/0028_ssto/s_sto_proc.adb
+++ regtests/0028_ssto/s_sto_proc.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                     Copyright (C) 2004-2012, AdaCore                     --
+--                     Copyright (C) 2004-2013, AdaCore                     --
 --                                                                          --
 --  This is free software;  you can redistribute it  and/or modify it       --
 --  under terms of the  GNU General Public License as published  by the     --
@@ -51,7 +51,7 @@ procedure S_STO_Proc (Security : Boolean) is
    procedure Check_Timeout (Span : Ada.Real_Time.Time_Span) is
       D : constant Duration := Ada.Real_Time.To_Duration (Span);
    begin
-      if not (D in 0.99 .. 1.1) then
+      if not (D in 0.98 .. 1.2) then
          Text_IO.Put_Line ("wrong timeout" & D'Img);
       end if;
    end Check_Timeout;
