ca1330fa8998fb91bb9159ea83f6e8a4574aafde
Pascal Obry <obry@adacore.com>
2013-12-21 19:11:47 +0100

    Properly tag SSL.Thin (GNU/TLS version) as written in Ada 2012.
    Continued work for MA30-042.

--- config/ssl/ssl-thin__gnutls.ads
+++ config/ssl/ssl-thin__gnutls.ads
@@ -27,6 +27,8 @@
 --  covered by the  GNU Public License.                                     --
 ------------------------------------------------------------------------------
 
+pragma Ada_2012;
+
 --  Thin binding to GNU/TLS
 
 with Interfaces.C.Strings;
