3c7e18c2b7f5631131f689b4c006a4bec4992727
Dmitriy Anisimkov <anisimkov@ada-ru.org>
2014-06-18 17:07:46 +0700

    Add application/json MIME

--- src/core/aws-mime.ads
+++ src/core/aws-mime.ads
@@ -81,6 +81,7 @@ package AWS.MIME is
    Application_Powerpoint      : constant String := "application/powerpoint";
    Application_Rtf             : constant String := "application/rtf";
    Application_XML             : constant String := "application/xml";
+   Application_JSON            : constant String := "application/json";
    Application_X_Compress      : constant String := "application/x-compress";
    Application_X_GTar          : constant String := "application/x-gtar";
    Application_X_GZip          : constant String := "application/x-gzip";
