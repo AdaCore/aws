0dd811c86f2bd33226e6fd4a94db80f90c490755
Pascal Obry <obry@adacore.com>
2014-07-27 23:26:12 +0200

    Initialize record's field (minor code change).

--- src/core/aws-server.ads
+++ src/core/aws-server.ads
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                     Copyright (C) 2000-2013, AdaCore                     --
+--                     Copyright (C) 2000-2014, AdaCore                     --
 --                                                                          --
 --  This library is free software;  you can redistribute it and/or modify   --
 --  it under terms of the  GNU General Public License  as published by the  --
@@ -505,7 +505,7 @@ private
       Server     : HTTP_Access;
       Line       : Positive;
       Stat       : Status.Data;
-      Expect_100 : Boolean;
+      Expect_100 : Boolean := False;
       Skip_Log   : Boolean := False;
       Log_Data   : AWS.Log.Fields_Table;
    end record;
