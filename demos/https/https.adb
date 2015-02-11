4401144e90491252066a9745de574c2fa790e007
Pascal Obry <obry@adacore.com>
2015-02-11 20:05:40 +0100

    Add HTTPS demo.
    This demo ways to select a specific protocol like TLS 1.2 and enforce
    it as the only allowed protocol.
    
    Motivated by O202-018.

---
+++ demos/https/https.adb
@@ -0,0 +1,72 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                       Copyright (C) 2015, AdaCore                        --
+--                                                                          --
+--  This is free software;  you can redistribute it  and/or modify it       --
+--  under terms of the  GNU General Public License as published  by the     --
+--  Free Software  Foundation;  either version 3,  or (at your option) any  --
+--  later version.  This software is distributed in the hope  that it will  --
+--  be useful, but WITHOUT ANY WARRANTY;  without even the implied warranty --
+--  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU     --
+--  General Public License for  more details.                               --
+--                                                                          --
+--  You should have  received  a copy of the GNU General  Public  License   --
+--  distributed  with  this  software;   see  file COPYING3.  If not, go    --
+--  to http://www.gnu.org/licenses for a complete copy of the license.      --
+------------------------------------------------------------------------------
+
+--  Demos using TLS 1.2 only
+
+with Ada.Command_Line;
+with Ada.Text_IO;
+
+with AWS.Config;
+with AWS.Net.SSL;
+with AWS.Server;
+
+with HTTPS_CB;
+
+procedure HTTPS is
+
+   use Ada;
+   use AWS;
+
+   WS  : Server.HTTP;
+   SSL : Net.SSL.Config;
+
+begin
+   Text_IO.Put_Line ("Call me on port 4433, press Q to exit");
+   Text_IO.New_Line;
+
+   --  Priorities    : for GNUTLS
+   --  Security_Mode : for OpenSSL
+   --
+   --  TLS 1.1 only:
+   --     Priorities           => "NORMAL:-VERS-TLS-ALL:+VERS-TLS1.1",
+   --     Security_Mode        => Net.SSL.TLSv1_1);
+   --
+   --  TLS 1.2 only:
+   --     Priorities           => "NORMAL:-VERS-TLS-ALL:+VERS-TLS1.2",
+   --     Security_Mode        => Net.SSL.TLSv1_2);
+
+   Net.SSL.Initialize
+     (SSL,
+      Certificate_Filename => "aws-server.crt",
+      Key_Filename         => "aws-server.key",
+      Priorities           => "NORMAL:-VERS-TLS-ALL:+VERS-TLS1.1",
+      Security_Mode        => Net.SSL.TLSv1_2);
+
+   Server.Set_SSL_Config (WS, SSL);
+
+   Server.Start
+     (WS, "HTTPS",
+      Max_Connection => 5,
+      Security       => True,
+      Port           => 4433,
+      Callback       => HTTPS_CB.HW_CB'Access);
+
+   Server.Wait (Server.Q_Key_Pressed);
+
+   Server.Shutdown (WS);
+end HTTPS;
---
+++ demos/https/https_cb.adb
@@ -0,0 +1,38 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                       Copyright (C) 2015, AdaCore                        --
+--                                                                          --
+--  This is free software;  you can redistribute it  and/or modify it       --
+--  under terms of the  GNU General Public License as published  by the     --
+--  Free Software  Foundation;  either version 3,  or (at your option) any  --
+--  later version.  This software is distributed in the hope  that it will  --
+--  be useful, but WITHOUT ANY WARRANTY;  without even the implied warranty --
+--  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU     --
+--  General Public License for  more details.                               --
+--                                                                          --
+--  You should have  received  a copy of the GNU General  Public  License   --
+--  distributed  with  this  software;   see  file COPYING3.  If not, go    --
+--  to http://www.gnu.org/licenses for a complete copy of the license.      --
+------------------------------------------------------------------------------
+
+with Ada.Text_IO;
+
+with AWS.MIME;
+with AWS.Response.Set;
+
+package body HTTPS_CB is
+
+   use Ada;
+
+   -----------
+   -- HW_CB --
+   -----------
+
+   function HW_CB (Request : Status.Data) return Response.Data is
+      Answer : Response.Data;
+   begin
+      return Response.Build (MIME.Text_HTML, "Hello World!");
+   end HW_CB;
+
+end HTTPS_CB;
