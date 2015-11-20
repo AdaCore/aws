b4b353027f5591dd32b77c6e519f1059559f08e2
Pascal Obry <obry@adacore.com>
2015-11-20 19:08:12 +0100

    Minor style fix.

--- src/core/aws-server-http_utils.ads
+++ src/core/aws-server-http_utils.ads
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                     Copyright (C) 2005-2014, AdaCore                     --
+--                     Copyright (C) 2005-2015, AdaCore                     --
 --                                                                          --
 --  This library is free software;  you can redistribute it and/or modify   --
 --  it under terms of the  GNU General Public License  as published by the  --
@@ -29,9 +29,9 @@
 
 with Ada.IO_Exceptions;
 
-with AWS.Status;
 with AWS.Resources;
 with AWS.Response;
+with AWS.Status;
 
 package AWS.Server.HTTP_Utils is
 
