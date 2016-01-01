e602b444641b057ff43bb21663f65fe78659a78c
Dmitriy Anisimkov <anisimko@adacore.com>
2016-01-02 00:27:05 +0600

    LibreSSL supported.
    OC02-012
    
    LibreSSL has a binary compartibility with OpenSSL, at least in the AWS
    using routines. The only difference is in the system default configuration
    parameters absense for certificate generations. This fix is adding config
    file for one failed regtest.

--- regtests/0235_crl_check/create_certs.sh
+++ regtests/0235_crl_check/create_certs.sh
@@ -21,7 +21,8 @@ openssl x509 -req -days 365 -in private-ca.csr -signkey private-ca.key -out priv
 openssl genrsa -out aws-server.key 1024
 openssl req -new -batch -subj '/C=FR/ST=Paris/O=ACME/OU=server/CN=localhost/' -key aws-server.key -out aws-server.csr
 
-openssl ca -batch -in aws-server.csr -cert private-ca.crt -keyfile private-ca.key -out aws-server.crt
+CA_BATCH="openssl ca -batch -config ca.cnf"
+$CA_BATCH -in aws-server.csr -cert private-ca.crt -keyfile private-ca.key -out aws-server.crt
 
 cat aws-server.crt aws-server.key > cert.pem
 
@@ -30,16 +31,16 @@ cat aws-server.crt aws-server.key > cert.pem
 mk_client() {
   openssl genrsa -out aws-client$1.key 1024
   openssl req -new -batch -subj "/C=FR/ST=Paris/O=ACME/OU=clients/CN=client$1/" -key aws-client$1.key -out aws-client$1.csr
-  openssl ca -batch -in aws-client$1.csr -cert private-ca.crt -keyfile private-ca.key -out aws-client$1.crt
+  $CA_BATCH -in aws-client$1.csr -cert private-ca.crt -keyfile private-ca.key -out aws-client$1.crt
   cat aws-client$1.crt aws-client$1.key > aws-client$1.pem
 }
 
 mk_client 1
 mk_client 2
 
-openssl ca -batch -gencrl -keyfile private-ca.key -cert private-ca.crt -out private-ca.crl1
+$CA_BATCH -gencrl -keyfile private-ca.key -cert private-ca.crt -out private-ca.crl1
 
 # Revoke 1st client cert
 
-openssl ca -batch -revoke aws-client1.crt -keyfile private-ca.key -cert private-ca.crt
-openssl ca -batch -gencrl -keyfile private-ca.key -cert private-ca.crt -out private-ca.crl2
+$CA_BATCH -revoke aws-client1.crt -keyfile private-ca.key -cert private-ca.crt
+$CA_BATCH -gencrl -keyfile private-ca.key -cert private-ca.crt -out private-ca.crl2
