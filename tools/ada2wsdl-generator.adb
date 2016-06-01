10201cf89355ef91c0a1dc0da6dada0b3ea9b4e8
Pascal Obry <obry@adacore.com>
2016-06-01 10:20:57 +0200

    Add Pre condition aspect for Write_Message.
    The definition node must be a routine when entering this procedure.
    
    Motivated by P504-041.

--- tools/ada2wsdl-generator.adb
+++ tools/ada2wsdl-generator.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                     Copyright (C) 2003-2015, AdaCore                     --
+--                     Copyright (C) 2003-2016, AdaCore                     --
 --                                                                          --
 --  This is free software;  you can redistribute it  and/or modify it       --
 --  under terms of the  GNU General Public License as published  by the     --
@@ -685,7 +685,8 @@ package body Ada2WSDL.Generator is
 
       procedure Write_Messages is
 
-         procedure Write_Message (R : Definition);
+         procedure Write_Message (R : Definition)
+           with Pre => R.Def_Mode = Routine;
 
          ---------------------
          -- Write_Operation --
