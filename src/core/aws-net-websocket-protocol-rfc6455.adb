48da23645b07d375ba7a5de8a108b8236b628c84
Pascal Obry <obry@adacore.com>
2017-05-24 07:23:48 +0200

    Update copyright notice.
    After merged from GitHub.

--- src/core/aws-net-websocket-protocol-rfc6455.adb
+++ src/core/aws-net-websocket-protocol-rfc6455.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                     Copyright (C) 2012-2014, AdaCore                     --
+--                     Copyright (C) 2012-2017, AdaCore                     --
 --                                                                          --
 --  This library is free software;  you can redistribute it and/or modify   --
 --  it under terms of the  GNU General Public License  as published by the  --
