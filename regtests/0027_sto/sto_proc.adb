996a270f658160a2184e45102ffcfa97ded101aa
Pascal Obry <obry@adacore.com>
2017-11-11 08:22:19 +0100

    Allow some more time for the time-out.
    On loaded machines it may happens that the time-out take a bit longer.
    
    For QB09-038.

--- regtests/0027_sto/sto_proc.adb
+++ regtests/0027_sto/sto_proc.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                     Copyright (C) 2004-2014, AdaCore                     --
+--                     Copyright (C) 2004-2018, AdaCore                     --
 --                                                                          --
 --  This library is free software;  you can redistribute it and/or modify   --
 --  it under terms of the  GNU General Public License  as published by the  --
@@ -62,7 +62,7 @@ procedure STO_Proc (Security : Boolean) is
    procedure Check_Timeout (Span : Ada.Real_Time.Time_Span) is
       D : constant Duration := Ada.Real_Time.To_Duration (Span);
    begin
-      if not (D in 0.98 .. 1.1) then
+      if not (D in 0.98 .. 1.2) then
          Text_IO.Put_Line ("wrong timeout" & D'Img);
       end if;
    end Check_Timeout;
