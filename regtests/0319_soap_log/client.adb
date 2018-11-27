fb8f9e22526fc9d11aedbea862146128cddcc36b
Pascal Obry <obry@adacore.com>
2018-11-27 19:31:33 +0100

    Add a test for log enabling when running a generated SOAP server.
    Continued work for RB21-041.

---
+++ regtests/0319_soap_log/client.adb
@@ -0,0 +1,30 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                       Copyright (C) 2018, AdaCore                        --
+--                                                                          --
+--  This is free software;  you can redistribute it  and/or modify it       --
+--  under terms of the  GNU General Public License as published  by the     --
+--  Free Software  Foundation;  either version 3,  or (at your option) any  --
+--  later version.  This software is distributed in the hope  that it will  --
+--  be useful, but WITHOUT ANY WARRANTY;  without even the implied warranty --
+--  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU     --
+--  General Public License for  more details.                               --
+--                                                                          --
+--  You should have  received  a copy of the GNU General  Public  License   --
+--  distributed  with  this  software;   see  file COPYING3.  If not, go    --
+--  to http://www.gnu.org/licenses for a complete copy of the license.      --
+------------------------------------------------------------------------------
+
+with Ada.Text_IO;
+with Data_Service.Client;
+
+procedure Client is
+   use Ada;
+   R : Integer;
+begin
+   R := Data_Service.Client.Add (1, 2);
+   Text_IO.Put_Line ("R =" & Integer'Image (R));
+   R := Data_Service.Client.Add (2, 2);
+   Text_IO.Put_Line ("R =" & Integer'Image (R));
+end Client;
