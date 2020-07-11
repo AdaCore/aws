AdaCore/aws

commit 05a2aeaf68df025a01cafb27cc1549b7fec9c566

Avoid exception when an ini file cannot be opened.
This can be because it is already opened by another process for example.
So we try to open it without sharing it and if this fails we do catch
all exceptions.

For S223-004.

src/core/aws-config-ini.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                     Copyright (C) 2000-2012, AdaCore                     --
+--                     Copyright (C) 2000-2019, AdaCore                     --
 --                                                                          --
 --  This library is free software;  you can redistribute it and/or modify   --
 --  it under terms of the  GNU General Public License  as published by the  --
@@ -138,7 +138,8 @@ package body AWS.Config.Ini is
    begin
       Text_IO.Open (Name => Filename,
                     File => File,
-                    Mode => Text_IO.In_File);
+                    Mode => Text_IO.In_File,
+                    Form => "shared=no");
       Line := 0;
 
       while not Text_IO.End_Of_File (File) loop
@@ -186,6 +187,12 @@ package body AWS.Config.Ini is
       end loop;
 
       Text_IO.Close (File);
+
+   exception
+      when others =>
+         if Text_IO.Is_Open (File) then
+            Text_IO.Close (File);
+         end if;
    end Read;
 
 end AWS.Config.Ini;

commit a93fe04e90e4067dea98b275de7aadeda808232a

Config.Ini.Read: review documentation and fix implemenation.
All exceptions where masked which was not compliant with the comments.

Now this routine properly propagates the exception Constraint_Error in case
of erroneous option name or value.

The regression was introduced in 05a2aea when fixing a possible resource
leak failing to close the file descriptor of the configuration file in
case or error.

For T707-002.

src/core/aws-config-ini.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                     Copyright (C) 2000-2019, AdaCore                     --
+--                     Copyright (C) 2000-2020, AdaCore                     --
 --                                                                          --
 --  This library is free software;  you can redistribute it and/or modify   --
 --  it under terms of the  GNU General Public License  as published by the  --
@@ -189,6 +189,13 @@ package body AWS.Config.Ini is
       Text_IO.Close (File);
 
    exception
+      when Constraint_Error =>
+         if Text_IO.Is_Open (File) then
+            Text_IO.Close (File);
+         end if;
+
+         raise;
+
       when others =>
          if Text_IO.Is_Open (File) then
             Text_IO.Close (File);

src/core/aws-config-ini.ads
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                     Copyright (C) 2000-2012, AdaCore                     --
+--                     Copyright (C) 2000-2020, AdaCore                     --
 --                                                                          --
 --  This library is free software;  you can redistribute it and/or modify   --
 --  it under terms of the  GNU General Public License  as published by the  --
@@ -41,8 +41,7 @@ package AWS.Config.Ini is
      (Config   : in out Object;
       Filename : String);
    --  Read Filename and update the configuration object with the
-   --  options read from it. Raises Ada.Text_IO.Name_Error if Filename does
-   --  not exist. Raises Constraint_Error in case of wrong any parameter name
-   --  or value.
+   --  options read from it. Raises Constraint_Error in case of wrong
+   --  any parameter name or value.
 
 end AWS.Config.Ini;