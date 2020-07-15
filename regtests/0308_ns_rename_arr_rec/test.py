bea18c6589f34e6b8083f679230661726fe912c0
Dmitriy Anisimkov <anisimko@adacore.com>
2020-07-15 10:47:43 +0600

    Fix regression test
    S415-009
    
    Put real filename to -P parameter for ada2wsdl call.

--- regtests/0308_ns_rename_arr_rec/test.py
+++ regtests/0308_ns_rename_arr_rec/test.py
@@ -1,6 +1,6 @@
 from test_support import exec_cmd, build, run
 
-exec_cmd('ada2wsdl', ['-q', '-Pnsren', '-f', '-lit', '-o',
+exec_cmd('ada2wsdl', ['-q', '-Pns_rename_arr_rec_server', '-f', '-lit', '-o',
                       'api.wsdl', 'api.ads'])
 
 exec_cmd('wsdl2aws', ['-q', '-f', '-types', 'api', 'api.wsdl'])
