0f8fd8d392f8d08d41b3b2129538fc4d30b11cf9
Dmitriy Anisimkov <anisimko@adacore.com>
2020-09-21 19:41:46 +0600

    Use Significant_Image in timeout error message
    T921-008
    
    It is much better when timeout less then 0.01 second.

--- src/core/aws-net.adb
+++ src/core/aws-net.adb
@@ -700,7 +700,7 @@ package body AWS.Net is
          Raise_Socket_Error
            (Socket,
             Wait_Event_Type'Image (Mode) & Timeout_Token
-            & Utils.Image (Socket.Timeout));
+            & Utils.Significant_Image (Socket.Timeout, 2));
          --  Can't use just Timeout in tests output becuse it can differ from
          --  stable Socket.Timeout value on Send operation.
 
