ec3a676b86adf125a662d36ac9000b5a2feeeff1
Pascal Obry <github@obry.net>
2020-10-03 10:56:23 +0200

    Update wsdl2aws-generator.adb
    Update copyright year to fix conflict.

--- tools/wsdl2aws-generator.adb
+++ tools/wsdl2aws-generator.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Ada Web Server                              --
 --                                                                          --
---                     Copyright (C) 2003-2018, AdaCore                     --
+--                     Copyright (C) 2003-2020, AdaCore                     --
 --                                                                          --
 --  This library is free software;  you can redistribute it and/or modify   --
 --  it under terms of the  GNU General Public License  as published by the  --
