ef2466707baf38d5f4ba7251bc95e4b0de08e407
Pascal Obry <obry@adacore.com>
2021-06-02 23:20:09 +0200

    Fix support of subtype as array index.
    This properly handle a subtype as array index and also a subtype of
    subtype (and any deeper level) as array index. Also we do not try
    to generate constraints for non numeric ranges as not supported in
    WSDL.
    
    Add corresponding regression test.
    
    Fixes U602-070.

---
+++ regtests/0337_subtype_array_index/signal.ads
@@ -0,0 +1,31 @@
+------------------------------------------------------------------------------
+--                              Ada Web Server                              --
+--                                                                          --
+--                      Copyright (C) 2021, AdaCore                         --
+--                                                                          --
+--  This is free software;  you can redistribute it  and/or modify it       --
+--  under terms of the  GNU General Public License as published  by the     --
+--  Free Software  Foundation;  either version 3,  or (at your option) any  --
+--  later version.  This software is distributed in the hope  that it will  --
+--  be useful, but WITHOUT ANY WARRANTY;  without even the implied warranty --
+--  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU     --
+--  General Public License for  more details.                               --
+--                                                                          --
+--  You should have  received  a copy of the GNU General  Public  License   --
+--  distributed  with  this  software;   see  file COPYING3.  If not, go    --
+--  to http://www.gnu.org/licenses for a complete copy of the license.      --
+------------------------------------------------------------------------------
+
+package Signal is
+
+   type T is (A, B);
+
+   subtype ST is T range A .. A;
+   subtype SPT is Positive range 1 .. 2;
+   type Arr1 is array (ST) of Boolean;
+   type Arr2 is array (T) of Boolean;
+   type Arr3 is array (SPT) of Boolean;
+
+   procedure Call (P : T);
+
+end Signal;
