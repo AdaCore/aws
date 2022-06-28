49451d0f67a35560bbbba2ee4a0a1f18375ade69
Dmitriy Anisimkov <anisimko@adacore.com>
2022-06-28 17:27:23 +0600

    Increase length of certificate key
    OpenSSL 1.1.1f do not want to worl with too short certificate key.

--- regtests/0043_check_mem/create_certs.sh
+++ regtests/0043_check_mem/create_certs.sh
@@ -19,6 +19,43 @@ openssl x509 -req -days 365 -in private-ca.csr -signkey private-ca.key -out priv
 # Create server cert
 
 openssl genrsa -out cert.key
-openssl req -new -batch -subj '/C=FR/ST=Ile de France/O=AWS Team/CN=localhost/emailAddress=aws@obry.net/' -key cert.key -out aws-server.csr
+openssl req -new -batch -subj '/CN=0043_check_mem/C=FR/ST=Ile de France/O=AWS Team/emailAddress=aws@obry.net/' -key cert.key -out aws-server.csr
 
 openssl ca -batch -config ca.cnf -in aws-server.csr -cert private-ca.crt -keyfile private-ca.key -out cert.pem
+
+cat cert.pem cert.key > cert.kpem
+
+cp -f cert.kpem ../0012_sock2_sec/cert.pem
+cp -f cert.kpem ../0014_sock3_sec/cert.pem
+cp -f cert.kpem ../0016_sockext_sec/cert.pem
+cp -f cert.kpem ../0018_sockover_sec/cert.pem
+cp -f cert.kpem ../0020_ssockto/cert.pem
+cp -f cert.kpem ../0028_ssto/cert.pem
+cp -f cert.kpem ../0031_accs_sec/cert.pem
+cp -f cert.kpem ../0033_afile_sec/cert.pem
+cp -f cert.kpem ../0035_append_sec/cert.pem
+cp -f cert.kpem ../0047_disp_soap_sec/cert.pem
+cp -f cert.kpem ../0060_hload_sec/cert.pem
+cp -f cert.kpem ../0062_smtp_2/cert.pem
+cp -f cert.kpem ../0069_huge_response_sec/cert.pem
+cp -f cert.kpem ../0076_nbconn_sec/cert.pem
+cp -f cert.kpem ../0080_plain2ssl/cert.pem
+cp -f cert.kpem ../0083_ssback/cert.pem
+cp -f cert.kpem ../0093_smem_sec/cert.pem
+cp -f cert.kpem ../0105_ssp/cert.pem
+cp -f cert.kpem ../0107_spair_sec/cert.pem
+cp -f cert.kpem ../0109_spg_sec/cert.pem
+cp -f cert.kpem ../0111_sslcfg/cert.pem
+cp -f cert.kpem ../0122_test_sec_soap/cert.pem
+cp -f cert.kpem ../0127_test_soap5_sec/cert.pem
+cp -f cert.kpem ../0142_tsgetparam/cert.pem
+cp -f cert.kpem ../0157_wait_sec/cert.pem
+cp -f cert.kpem ../0159_wait2_sec/cert.pem
+cp -f cert.kpem ../0172_xmlt_sec/cert.pem
+cp -f cert.kpem ../0236_snb_send/cert.pem
+cp -f cert.kpem ../0242_ssl_reuse/cert.pem
+cp -f cert.kpem ../0245_errcon/cert.pem
+cp -f cert.kpem ../0279_broken_status_line/cert.pem
+cp -f cert.kpem ../0334_alpn/cert.pem
+
+rm cert.kpem
