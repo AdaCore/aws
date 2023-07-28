db22a3dac215bb57373f228e2d699d97e7d7483b
Kévin Le Gouguec <legouguec@adacore.com>
2023-07-28 09:04:56 +0200

    regtests: Fix line-ending mixup in 0327_prog_config
    By default, Python's write() function translates "\n" to the host's native
    line-ending string.  On armhf-linux-windows64, this causes test.py to
    generate an aws.ini that contains CRLF sequences, which prog_config
    mis-parses.
    
    Make sure the config file we generate has the canonical line-endings for the
    target we are testing.
    
    TN: eng/toolchain/aws#12

--- regtests/0327_prog_config/test.py
+++ regtests/0327_prog_config/test.py
@@ -1,14 +1,28 @@
+from e3.env import Env
+
 from test_support import *
 
-with open('aws.ini', 'w') as ini_file:
-    ini_file.write("status_page	aws.ini-status_page\n")
-    ini_file.write("directory_browser_page aws.ini-directory_browser_page\n")
+def write_aws_ini(config):
+    newline = None
+    if Env().host.os.name == "windows" and Env().target.os.name == "linux":
+        # On Linux, AWS will fail to parse ini files that contain CRLF line-endings: ask
+        # Python to keep line-feeds verbatim on Windows-hosted cross-Linux testing.
+        newline = "\n"
+
+    with open("aws.ini", "w", newline=newline) as ini_file:
+        ini_file.write(config)
+
+write_aws_ini("""\
+status_page	aws.ini-status_page
+directory_browser_page	aws.ini-directory_browser_page
+""")
 
 build_and_run('prog_config')
 
-with open('aws.ini', 'w') as ini_file:
-    ini_file.write("status_page	aws.ini-status_page\n")
-    ini_file.write("directory_browser_page aws.ini-directory_browser_page\n")
-    ini_file.write("disable_program_ini true\n")
+write_aws_ini("""\
+status_page	aws.ini-status_page
+directory_browser_page	aws.ini-directory_browser_page
+disable_program_ini	true
+""")
 
 run('prog_config')
